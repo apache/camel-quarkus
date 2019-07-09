8a9a2a41a4d2c0a2ede2cd8f9fe38be197674996
Peter Palaga <ppalaga@redhat.com>
2019-07-09 20:53:56 +0200

    Fix #31 Move infinispan integration-tests out of core

--- integration-tests/infinispan/src/test/java/io/quarkus/it/camel/infinispan/CamelInfinispanITCase.java
+++ integration-tests/infinispan/src/test/java/io/quarkus/it/camel/infinispan/CamelInfinispanITCase.java
@@ -14,27 +14,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.quarkus.it.camel.core;
+package io.quarkus.it.camel.infinispan;
 
-import static org.hamcrest.Matchers.is;
+import io.quarkus.test.junit.SubstrateTest;
 
-import java.net.URI;
-
-import org.junit.jupiter.api.Test;
-
-import io.quarkus.test.common.QuarkusTestResource;
-import io.quarkus.test.junit.QuarkusTest;
-import io.restassured.RestAssured;
-
-@QuarkusTest
-@QuarkusTestResource(InfinispanServerTestResource.class)
-public class CamelInfinispanTest {
-
-    @Test
-    public void testInfinispan() throws Exception {
-        RestAssured.with().body("Hello Infinispan").post(new URI("http://localhost:8999/put")).then();
-
-        RestAssured.when().get(new URI("http://localhost:8999/get"))
-                .then().body(is("Hello Infinispan"));
-    }
+@SubstrateTest
+public class CamelInfinispanITCase extends CamelInfinispanTest {
 }
