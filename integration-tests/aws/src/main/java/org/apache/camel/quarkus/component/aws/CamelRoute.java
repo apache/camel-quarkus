bf894ff4a52b717d0d18cd54919d1d3321de6e3c
lburgazzoli <lburgazzoli@gmail.com>
2019-07-18 08:56:54 +0200

    move test packages to org.apache.camel

---
+++ integration-tests/aws/src/main/java/org/apache/camel/quarkus/component/aws/CamelRoute.java
@@ -0,0 +1,50 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.quarkus.component.aws;
+
+import org.apache.camel.builder.RouteBuilder;
+
+import io.quarkus.runtime.annotations.RegisterForReflection;
+
+@RegisterForReflection
+public class CamelRoute extends RouteBuilder {
+
+    @Override
+    public void configure() {
+        from("timer:quarkus-s3?repeatCount=1")
+                .setHeader("CamelAwsS3Key", constant("testquarkus"))
+                .setBody(constant("Quarkus is great!"))
+                .to("aws-s3://camel-kafka-connector")
+                .to("log:sf?showAll=true");
+
+        from("timer:quarkus-sqs?repeatCount=1")
+                .setBody(constant("Quarkus is great!"))
+                .to("aws-sqs://camel-1")
+                .to("log:sf?showAll=true");
+
+        from("timer:quarkus-eks?repeatCount=1")
+                .setHeader("CamelAwsEKSOperation", constant("listClusters"))
+                .to("aws-eks://cluster")
+                .to("log:sf?showAll=true");
+
+        from("timer:quarkus-sns?repeatCount=1")
+               .setBody(constant("Quarkus is great!"))
+               .to("aws-sns://topic1")
+               .to("log:sf?showAll=true");
+    }
+
+}
