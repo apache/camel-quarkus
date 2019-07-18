5ef9d37cb1b4e61ebb5e0b4086dcef1e53ab50a6
lburgazzoli <lburgazzoli@gmail.com>
2019-07-18 08:56:54 +0200

    test packages

---
+++ integration-tests/infinispan/src/test/java/org/apache/camel/quarkus/component/infinispan/InfinispanServerTestResource.java
@@ -0,0 +1,52 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.quarkus.component.infinispan;
+
+import java.util.Collections;
+import java.util.Map;
+
+import org.infinispan.configuration.cache.ConfigurationBuilder;
+import org.infinispan.configuration.global.GlobalConfigurationBuilder;
+import org.infinispan.manager.EmbeddedCacheManager;
+import org.infinispan.server.hotrod.HotRodServer;
+import org.infinispan.server.hotrod.test.HotRodTestingUtil;
+import org.infinispan.test.fwk.TestCacheManagerFactory;
+import org.infinispan.test.fwk.TestResourceTracker;
+
+import io.quarkus.test.common.QuarkusTestResourceLifecycleManager;
+
+public class InfinispanServerTestResource implements QuarkusTestResourceLifecycleManager {
+    private HotRodServer hotRodServer;
+
+    @Override
+    public Map<String, String> start() {
+        TestResourceTracker.setThreadTestName("InfinispanServer");
+        EmbeddedCacheManager ecm = TestCacheManagerFactory.createCacheManager(
+                new GlobalConfigurationBuilder().nonClusteredDefault().defaultCacheName("default"),
+                new ConfigurationBuilder());
+        // Client connects to a non default port
+        hotRodServer = HotRodTestingUtil.startHotRodServer(ecm, 11232);
+        return Collections.emptyMap();
+    }
+
+    @Override
+    public void stop() {
+        if (hotRodServer != null) {
+            hotRodServer.stop();
+        }
+    }
+}
---
+++ integration-tests/servlet/src/test/java/org/apache/camel/quarkus/component/servlet/CamelServletTest.java
@@ -0,0 +1,51 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.quarkus.component.servlet;
+
+import org.apache.camel.quarkus.component.servlet.CustomServlet;
+import org.hamcrest.core.IsEqual;
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.RestAssured;
+
+@QuarkusTest
+public class CamelServletTest {
+
+    @Test
+    public void multiplePaths() throws Throwable {
+        RestAssured.when().get("/folder-1/rest-get").then().body(IsEqual.equalTo("GET: /rest-get"));
+        RestAssured.when().get("/folder-2/rest-get").then().body(IsEqual.equalTo("GET: /rest-get"));
+        RestAssured.when().post("/folder-1/rest-post").then().body(IsEqual.equalTo("POST: /rest-post"));
+        RestAssured.when().post("/folder-2/rest-post").then().body(IsEqual.equalTo("POST: /rest-post"));
+        RestAssured.when().get("/folder-1/hello").then().body(IsEqual.equalTo("GET: /hello"));
+        RestAssured.when().get("/folder-2/hello").then().body(IsEqual.equalTo("GET: /hello"));
+    }
+
+    @Test
+    public void namedWithservletClass() throws Throwable {
+        RestAssured.when().get("/my-named-folder/custom").then()
+                .body(IsEqual.equalTo("GET: /custom"))
+                .and().header("x-servlet-class-name", CustomServlet.class.getName());
+    }
+
+    @Test
+    public void ignoredKey() throws Throwable {
+        RestAssured.when().get("/my-favorite-folder/favorite").then()
+                .body(IsEqual.equalTo("GET: /favorite"));
+    }
+}
