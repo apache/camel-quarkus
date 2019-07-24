f726d3dfb73cf9ab705b5338cc6470a56408b0f2
Peter Palaga <ppalaga@redhat.com>
2019-07-24 11:16:01 +0200

    Remove unused imports from CamelRoute in netty4-http integration test

--- integration-tests/netty4-http/src/main/java/io/quarkus/it/camel/netty4http/CamelRoute.java
+++ integration-tests/netty4-http/src/main/java/io/quarkus/it/camel/netty4http/CamelRoute.java
@@ -16,21 +16,7 @@
  */
 package io.quarkus.it.camel.netty4http;
 
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.CompletableFuture;
-
-import org.apache.camel.AsyncCallback;
-import org.apache.camel.AsyncProcessor;
-import org.apache.camel.Exchange;
-import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.component.bean.BeanProcessor;
-import org.apache.camel.support.DefaultExchange;
-
-import io.quarkus.runtime.annotations.RegisterForReflection;
 
 public class CamelRoute extends RouteBuilder {
     @Override
