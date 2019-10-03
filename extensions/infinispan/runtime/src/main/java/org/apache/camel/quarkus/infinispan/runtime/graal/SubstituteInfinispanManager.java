f97125a15ebe4f1f74aa48559157a78897f792a1
lburgazzoli <lburgazzoli@gmail.com>
2019-10-03 14:21:22 +0200

    chore(substrate): simplify name of camel-quarkus-infinispan substitutions classes

---
+++ extensions/infinispan/runtime/src/main/java/org/apache/camel/quarkus/infinispan/runtime/graal/SubstituteInfinispanManager.java
@@ -0,0 +1,36 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.quarkus.infinispan.runtime.graal;
+
+import com.oracle.svm.core.annotate.Alias;
+import com.oracle.svm.core.annotate.Substitute;
+import com.oracle.svm.core.annotate.TargetClass;
+import org.apache.camel.component.infinispan.InfinispanManager;
+import org.apache.camel.util.ObjectHelper;
+import org.infinispan.commons.api.BasicCache;
+import org.infinispan.commons.api.BasicCacheContainer;
+
+@TargetClass(InfinispanManager.class)
+final class SubstituteInfinispanManager {
+    @Alias
+    private BasicCacheContainer cacheContainer;
+
+    @Substitute
+    public <K, V> BasicCache<K, V> getCache(String cacheName) {
+        return ObjectHelper.isEmpty(cacheName) ? cacheContainer.getCache() : cacheContainer.getCache(cacheName);
+    }
+}
