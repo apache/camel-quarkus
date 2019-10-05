b746f4c25e2722021535876334383615742e18cb
lburgazzoli <lburgazzoli@gmail.com>
2019-10-05 09:16:50 +0200

    inline BeanManagerHelper in RuntimeBeanRepository #9

--- extensions/core/runtime/src/main/java/org/apache/camel/quarkus/core/RuntimeBeanRepository.java
+++ extensions/core/runtime/src/main/java/org/apache/camel/quarkus/core/RuntimeBeanRepository.java
@@ -16,9 +16,18 @@
  */
 package org.apache.camel.quarkus.core;
 
+import java.lang.annotation.Annotation;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 
+import javax.enterprise.inject.spi.Bean;
+import javax.enterprise.inject.spi.BeanManager;
+
+import io.quarkus.arc.Arc;
+import io.quarkus.arc.ArcContainer;
 import org.apache.camel.spi.BeanRepository;
 
 public final class RuntimeBeanRepository implements BeanRepository {
@@ -29,16 +38,63 @@ public final class RuntimeBeanRepository implements BeanRepository {
 
     @Override
     public <T> T lookupByNameAndType(String name, Class<T> type) {
-        return BeanManagerHelper.getReferenceByName(name, type).orElse(null);
+        final ArcContainer container = Arc.container();
+        final BeanManager manager = container.beanManager();
+
+        return getReferenceByName(manager, name, type).orElse(null);
     }
 
     @Override
     public <T> Map<String, T> findByTypeWithName(Class<T> type) {
-        return BeanManagerHelper.getReferencesByTypeWithName(type);
+        return getReferencesByTypeWithName(type);
     }
 
     @Override
     public <T> Set<T> findByType(Class<T> type) {
-        return BeanManagerHelper.getReferencesByType(type);
+        final ArcContainer container = Arc.container();
+        final BeanManager manager = container.beanManager();
+
+        return getReferencesByType(manager, type);
+    }
+
+    private static <T> Map<String, T> getReferencesByTypeWithName(Class<T> type, Annotation... qualifiers) {
+        final ArcContainer container = Arc.container();
+        final BeanManager manager = container.beanManager();
+
+        return getReferencesByTypeWithName(manager, type, qualifiers);
+    }
+
+    private static <T> Set<T> getReferencesByType(BeanManager manager, Class<T> type, Annotation... qualifiers) {
+        Set<T> answer = new HashSet<>();
+
+        for (Bean<?> bean: manager.getBeans(type, qualifiers)) {
+            T ref = getReference(manager, type, bean);
+            if (ref != null) {
+                answer.add(ref);
+            }
+        }
+
+        return answer;
+    }
+
+    private static <T> Optional<T> getReferenceByName(BeanManager manager, String name, Class<T> type) {
+        return Optional.ofNullable(manager.resolve(manager.getBeans(name))).map(bean -> getReference(manager, type, bean));
+    }
+
+    private static <T> T getReference(BeanManager manager, Class<T> type, Bean<?> bean) {
+        return type.cast(manager.getReference(bean, type, manager.createCreationalContext(bean)));
+    }
+
+    private static <T> Map<String, T> getReferencesByTypeWithName(BeanManager manager, Class<T> type, Annotation... qualifiers) {
+        Map<String, T> answer = new HashMap<>();
+
+        for (Bean<?> bean: manager.getBeans(type, qualifiers)) {
+            T ref = getReference(manager, type, bean);
+            if (ref != null) {
+                answer.put(bean.getName(), ref);
+            }
+        }
+
+        return answer;
     }
 }
