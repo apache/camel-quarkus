dd55cadd6effdc3a4d8b659c765f55c22065341a
Carlos Esteban Feria Vila <carlosthe19916@gmail.com>
2019-10-07 12:17:12 +0200

    change tests

--- integration-tests/csv/src/main/java/org/apache/camel/quarkus/component/csv/it/CsvRouteBuilder.java
+++ integration-tests/csv/src/main/java/org/apache/camel/quarkus/component/csv/it/CsvRouteBuilder.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.camel.quarkus.component.carlos.it;
+package org.apache.camel.quarkus.component.csv.it;
 
 import org.apache.camel.builder.RouteBuilder;
 
