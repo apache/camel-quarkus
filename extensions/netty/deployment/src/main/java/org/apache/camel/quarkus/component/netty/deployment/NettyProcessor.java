61867773dbccee557390d96e5286b144e5e79cff
dhartford <dhartford@changehealthcare.com>
2019-10-30 22:39:53 -0400

    Initial camel-quarkus-netty support

---
+++ extensions/netty/deployment/src/main/java/org/apache/camel/quarkus/component/netty/deployment/NettyProcessor.java
@@ -0,0 +1,62 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.quarkus.component.netty.deployment;
+
+import java.util.Arrays;
+import java.util.List;
+
+import io.quarkus.deployment.annotations.BuildProducer;
+import io.quarkus.deployment.annotations.BuildStep;
+import io.quarkus.deployment.builditem.FeatureBuildItem;
+import io.quarkus.deployment.builditem.substrate.ReflectiveClassBuildItem;
+
+
+import org.apache.camel.component.netty.NettyConfiguration;
+import org.apache.camel.component.netty.NettyEndpoint;
+import org.apache.camel.component.netty.NettyServerBootstrapConfiguration;
+import org.apache.camel.spi.UriParam;
+import org.apache.camel.spi.UriParams;
+
+
+
+class NettyProcessor {
+
+    private static final String FEATURE = "camel-netty";
+
+    private static final List<Class<?>> NETTY_REFLECTIVE_CLASSES = Arrays.asList(
+        NettyConfiguration.class,
+        NettyServerBootstrapConfiguration.class,
+        NettyEndpoint.class,
+        UriParam.class,
+        UriParams.class
+    );
+
+    @BuildStep
+    FeatureBuildItem feature() {
+        return new FeatureBuildItem(FEATURE);
+    }
+
+    @BuildStep
+    void registerForReflection(BuildProducer<ReflectiveClassBuildItem> reflectiveClass) {
+        for (Class<?> type : NETTY_REFLECTIVE_CLASSES) {
+            reflectiveClass.produce(
+                    new ReflectiveClassBuildItem(true, true, type)
+            );
+        }
+    }
+
+}
