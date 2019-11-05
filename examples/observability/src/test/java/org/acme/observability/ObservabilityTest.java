7af2bf0284cc3fdff8197b8cc03ce03f40399e11
Guillaume Nodet <gnodet@gmail.com>
2019-11-05 11:10:28 +0100

    Examples should import the BOM instead of inheriting it as users would do

--- examples/observability/src/test/java/org/acme/observability/ObservabilityTest.java
+++ examples/observability/src/test/java/org/acme/observability/ObservabilityTest.java
@@ -16,37 +16,34 @@ public class ObservabilityTest {
     public void metrics() {
         // Verify a expected Camel metric is available
         given()
-            .when().accept(ContentType.JSON)
-            .get("/metrics/application")
-            .then()
-            .statusCode(200)
-            .body(
-                "'camel.context.status;camelContext=camel-quarkus-observability'", is(1)
-            );
+                .when().accept(ContentType.JSON)
+                .get("/metrics/application")
+                .then()
+                .statusCode(200)
+                .body(
+                        "'camel.context.status;camelContext=camel-quarkus-observability'", is(1));
     }
 
     @Test
     public void health() {
         // Verify liveness
         given()
-            .when().accept(ContentType.JSON)
-            .get("/health/live")
-            .then()
-            .statusCode(200)
-            .body("status", Matchers.is("UP"),
-                  "checks.name", containsInAnyOrder("camel-liveness-checks", "camel"),
-                  "checks.data.custom-liveness-check", containsInAnyOrder(null, "UP")
-            );
+                .when().accept(ContentType.JSON)
+                .get("/health/live")
+                .then()
+                .statusCode(200)
+                .body("status", Matchers.is("UP"),
+                        "checks.name", containsInAnyOrder("camel-liveness-checks", "camel"),
+                        "checks.data.custom-liveness-check", containsInAnyOrder(null, "UP"));
 
         // Verify readiness
         given()
-            .when().accept(ContentType.JSON)
-            .get("/health/ready")
-            .then()
-            .statusCode(200)
-            .body("status", Matchers.is("UP"),
-                    "checks.name", containsInAnyOrder("camel-readiness-checks", "camel", "Uptime readiness check"),
-                    "checks.data.custom-readiness-check", containsInAnyOrder(null, "UP")
-            );
+                .when().accept(ContentType.JSON)
+                .get("/health/ready")
+                .then()
+                .statusCode(200)
+                .body("status", Matchers.is("UP"),
+                        "checks.name", containsInAnyOrder("camel-readiness-checks", "camel", "Uptime readiness check"),
+                        "checks.data.custom-readiness-check", containsInAnyOrder(null, "UP"));
     }
 }
--- examples/rest-json/src/test/java/org/acme/rest/json/RestJsonTest.java
+++ examples/rest-json/src/test/java/org/acme/rest/json/RestJsonTest.java
@@ -34,39 +34,37 @@ public class RestJsonTest {
 
         /* Assert the initial fruits are there */
         given()
-          .when().get("/fruits")
-          .then()
-             .statusCode(200)
-             .body(
-                "$.size()", is(2),
-                "name", containsInAnyOrder("Apple", "Pineapple"),
-                "description", containsInAnyOrder("Winter fruit", "Tropical fruit")
-             );
+                .when().get("/fruits")
+                .then()
+                .statusCode(200)
+                .body(
+                        "$.size()", is(2),
+                        "name", containsInAnyOrder("Apple", "Pineapple"),
+                        "description", containsInAnyOrder("Winter fruit", "Tropical fruit"));
 
         /* Add a new fruit */
         given()
-            .body("{\"name\": \"Pear\", \"description\": \"Winter fruit\"}")
-            .header("Content-Type", "application/json")
-        .when()
-            .post("/fruits")
-        .then()
-            .statusCode(200)
-            .body(
-                "$.size()", is(3),
-                "name", containsInAnyOrder("Apple", "Pineapple", "Pear"),
-                "description", containsInAnyOrder("Winter fruit", "Tropical fruit", "Winter fruit")
-            );
+                .body("{\"name\": \"Pear\", \"description\": \"Winter fruit\"}")
+                .header("Content-Type", "application/json")
+                .when()
+                .post("/fruits")
+                .then()
+                .statusCode(200)
+                .body(
+                        "$.size()", is(3),
+                        "name", containsInAnyOrder("Apple", "Pineapple", "Pear"),
+                        "description", containsInAnyOrder("Winter fruit", "Tropical fruit", "Winter fruit"));
     }
 
     @Test
     public void legumes() {
         given()
-          .when().get("/legumes")
-          .then()
-             .statusCode(200)
-             .body("$.size()", is(2),
-                     "name", containsInAnyOrder("Carrot", "Zucchini"),
-                     "description", containsInAnyOrder("Root vegetable, usually orange", "Summer squash"));
+                .when().get("/legumes")
+                .then()
+                .statusCode(200)
+                .body("$.size()", is(2),
+                        "name", containsInAnyOrder("Carrot", "Zucchini"),
+                        "description", containsInAnyOrder("Root vegetable, usually orange", "Summer squash"));
     }
 
 }
