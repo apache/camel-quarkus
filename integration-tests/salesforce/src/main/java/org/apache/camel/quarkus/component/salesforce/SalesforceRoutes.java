32d9a5cf512a0d091172e4dea7a0ca80ef265a0e
lburgazzoli <lburgazzoli@gmail.com>
2020-02-18 11:10:11 +0100

    it-cleanup(salesforce): rename

--- integration-tests/salesforce/src/main/java/org/apache/camel/quarkus/component/salesforce/SalesforceRoutes.java
+++ integration-tests/salesforce/src/main/java/org/apache/camel/quarkus/component/salesforce/SalesforceRoutes.java
@@ -16,29 +16,19 @@
  */
 package org.apache.camel.quarkus.component.salesforce;
 
-import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Inject;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
+import io.quarkus.runtime.annotations.RegisterForReflection;
+import org.apache.camel.builder.RouteBuilder;
 
-import org.apache.camel.CamelContext;
-import org.apache.camel.ProducerTemplate;
+@RegisterForReflection
+public class SalesforceRoutes extends RouteBuilder {
 
-@Path("/test")
-@ApplicationScoped
-public class CamelServlet {
-    @Inject
-    CamelContext context;
+    @Override
+    public void configure() {
+        from("direct:case")
+                .autoStartup(false)
+                .setHeader("sObjectName").constant("Case")
+                .to("salesforce:getSObject?rawPayload=true")
+                .to("log:sf?showAll=true");
 
-    @Path("/case/{id}")
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public Object getCase(@PathParam("id") String id) {
-        ProducerTemplate template = context.createProducerTemplate();
-
-        return template.requestBody("direct:case", id);
     }
 }
