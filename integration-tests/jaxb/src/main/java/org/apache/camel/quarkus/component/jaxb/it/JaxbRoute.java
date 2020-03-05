79b58b61093f39f3e67d36c1539a7af555099d61
mmelko <mmelko@redhat.com>
2020-03-05 15:31:30 +0100

    add camel-jaxb into extensions

---
+++ integration-tests/jaxb/src/main/java/org/apache/camel/quarkus/component/jaxb/it/JaxbRoute.java
@@ -0,0 +1,51 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.quarkus.component.jaxb.it;
+
+import javax.xml.bind.JAXBContext;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.converter.jaxb.JaxbDataFormat;
+import org.apache.camel.quarkus.component.jaxb.it.model.Person;
+
+public class JaxbRoute extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+        JaxbDataFormat jaxb = new JaxbDataFormat("org.apache.camel.quarkus.component.jaxb.it.model");
+
+        JaxbDataFormat xml = new JaxbDataFormat();
+        JAXBContext context = JAXBContext.newInstance(Person.class);
+        xml.setContext(context);
+
+        JaxbDataFormat jaxbFromScheme = new JaxbDataFormat();
+        jaxbFromScheme.setSchema("classpath:person.xsd");
+
+        from("direct:unmarshal")
+                .unmarshal(jaxb);
+
+        from("direct:unmarshal-2")
+                .unmarshal(xml);
+
+        from("direct:marshal")
+                .marshal(jaxbFromScheme);
+
+        from("direct:marshal-2")
+                .marshal(xml);
+
+    }
+}
---
+++ integration-tests/jaxb/src/main/java/org/apache/camel/quarkus/component/jaxb/it/model/Person.java
@@ -0,0 +1,116 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.quarkus.component.jaxb.it.model;
+
+import javax.xml.bind.annotation.*;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = { "firstName", "lastName", "age" })
+@XmlRootElement(name = "person")
+public class Person {
+
+    @XmlElement(required = true)
+    protected String firstName = "John";
+    @XmlElement(required = true)
+    protected String lastName = "Doe";
+    @XmlElement(required = true, type = Integer.class, nillable = true)
+    protected Integer age = 33;
+
+    /**
+     * Gets the value of the firstName property.
+     *
+     * @return possible object is
+     *         {@link String }
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * Sets the value of the firstName property.
+     *
+     * @param value allowed object is
+     *              {@link String }
+     */
+    public void setFirstName(String value) {
+        this.firstName = value;
+    }
+
+    /**
+     * Gets the value of the lastName property.
+     *
+     * @return possible object is
+     *         {@link String }
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * Sets the value of the lastName property.
+     *
+     * @param value allowed object is
+     *              {@link String }
+     */
+    public void setLastName(String value) {
+        this.lastName = value;
+    }
+
+    /**
+     * Gets the value of the age property.
+     *
+     * @return possible object is
+     *         {@link Integer }
+     */
+    public Integer getAge() {
+        return age;
+    }
+
+    /**
+     * Sets the value of the age property.
+     *
+     * @param value allowed object is
+     *              {@link Integer }
+     */
+    public void setAge(Integer value) {
+        this.age = value;
+    }
+
+    public Person withFirstName(String value) {
+        setFirstName(value);
+        return this;
+    }
+
+    public Person withLastName(String value) {
+        setLastName(value);
+        return this;
+    }
+
+    public Person withAge(Integer value) {
+        setAge(value);
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
