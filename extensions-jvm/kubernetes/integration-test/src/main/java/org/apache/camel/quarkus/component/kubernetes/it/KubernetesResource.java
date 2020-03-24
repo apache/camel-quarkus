1ea9564256fb4a1648370fd7da41c51808ef9e18
James Netherton <jamesnetherton@gmail.com>
2020-03-24 10:07:44 +0000

    Add Kubernetes extension (JVM only)
    Fixes #303

---
+++ extensions-jvm/kubernetes/integration-test/src/main/java/org/apache/camel/quarkus/component/kubernetes/it/KubernetesResource.java
@@ -0,0 +1,105 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.quarkus.component.kubernetes.it;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import io.fabric8.kubernetes.api.model.Container;
+import io.fabric8.kubernetes.api.model.Pod;
+import io.fabric8.kubernetes.api.model.PodSpec;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.component.kubernetes.KubernetesConstants;
+
+@Path("/kubernetes")
+@ApplicationScoped
+public class KubernetesResource {
+
+    @Inject
+    ProducerTemplate producerTemplate;
+
+    @Path("/pod/{namespace}/{podName}")
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String readPod(@PathParam("namespace") String namespace, @PathParam("podName") String podName) {
+        Map<String, Object> headers = new HashMap<>();
+        headers.put(KubernetesConstants.KUBERNETES_NAMESPACE_NAME, namespace);
+        headers.put(KubernetesConstants.KUBERNETES_POD_NAME, podName);
+        final Pod pod = producerTemplate.requestBodyAndHeaders(
+                "kubernetes-pods:///?kubernetesClient=#kubernetesClient&operation=getPod", null, headers, Pod.class);
+        return pod.getMetadata().getName();
+    }
+
+    @Path("/pod/{namespace}/{podName}")
+    @POST
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response createPod(@PathParam("namespace") String namespace, @PathParam("podName") String podName) throws Exception {
+        Map<String, Object> headers = new HashMap<>();
+        headers.put(KubernetesConstants.KUBERNETES_NAMESPACE_NAME, namespace);
+        headers.put(KubernetesConstants.KUBERNETES_POD_NAME, podName);
+        headers.put(KubernetesConstants.KUBERNETES_POD_SPEC, createPodSpec(podName));
+        producerTemplate.requestBodyAndHeaders("kubernetes-pods:///?kubernetesClient=#kubernetesClient&operation=createPod",
+                null,
+                headers, Pod.class);
+        return Response
+                .created(new URI("https://camel.apache.org/"))
+                .build();
+    }
+
+    @Path("/pod/{namespace}/{podName}")
+    @DELETE
+    public Response deletePod(@PathParam("namespace") String namespace, @PathParam("podName") String podName) {
+        Map<String, Object> headers = new HashMap<>();
+        headers.put(KubernetesConstants.KUBERNETES_NAMESPACE_NAME, namespace);
+        headers.put(KubernetesConstants.KUBERNETES_POD_NAME, podName);
+        producerTemplate.requestBodyAndHeaders("kubernetes-pods:///?kubernetesClient=#kubernetesClient&operation=deletePod",
+                null,
+                headers);
+        return Response
+                .status(Response.Status.NO_CONTENT)
+                .build();
+    }
+
+    private PodSpec createPodSpec(String containerName) {
+        PodSpec podSpec = new PodSpec();
+
+        Container container = new Container();
+        container.setImage("docker.io/busybox:latest");
+        container.setName(containerName);
+
+        List<Container> containers = new ArrayList<>();
+        containers.add(container);
+
+        podSpec.setContainers(containers);
+
+        return podSpec;
+    }
+}
---
+++ extensions-jvm/kubernetes/integration-test/src/test/java/org/apache/camel/quarkus/component/kubernetes/it/KubernetesTest.java
@@ -0,0 +1,89 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.quarkus.component.kubernetes.it;
+
+import io.fabric8.kubernetes.api.model.Container;
+import io.fabric8.kubernetes.api.model.Pod;
+import io.fabric8.kubernetes.api.model.PodBuilder;
+import io.fabric8.kubernetes.client.server.mock.KubernetesMockServer;
+import io.quarkus.test.common.QuarkusTestResource;
+import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.kubernetes.client.KubernetesMockServerTestResource;
+import io.quarkus.test.kubernetes.client.MockServer;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.Matchers.is;
+
+@QuarkusTest
+@QuarkusTestResource(KubernetesMockServerTestResource.class)
+public class KubernetesTest {
+
+    @MockServer
+    private KubernetesMockServer mockServer;
+
+    @Test
+    public void testKubernetesComponent() {
+        Container container = new Container();
+        container.setImage("docker.io/busybox:latest");
+        container.setName("camel-pod");
+
+        Pod pod = new PodBuilder()
+                .withNewMetadata()
+                .withName("camel-pod")
+                .withNamespace("test")
+                .and()
+                .withNewSpec()
+                .withContainers(container)
+                .and()
+                .build();
+
+        mockServer.expect()
+                .post()
+                .withPath("/api/v1/namespaces/test/pods")
+                .andReturn(201, pod)
+                .once();
+
+        mockServer.expect()
+                .get()
+                .withPath("/api/v1/namespaces/test/pods/camel-pod")
+                .andReturn(200, pod)
+                .always();
+
+        mockServer.expect()
+                .delete()
+                .withPath("/api/v1/namespaces/test/pods/camel-pod")
+                .andReturn(200, "{}")
+                .once();
+
+        RestAssured.when()
+                .post("/kubernetes/pod/test/camel-pod")
+                .then()
+                .statusCode(201);
+
+        RestAssured.when()
+                .get("/kubernetes/pod/test/camel-pod")
+                .then()
+                .statusCode(200)
+                .body(is("camel-pod"));
+
+        RestAssured.when()
+                .delete("/kubernetes/pod/test/camel-pod")
+                .then()
+                .statusCode(204);
+    }
+}
