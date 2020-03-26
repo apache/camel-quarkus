7c9f9f468670ed2e1d44e21912d04e5509fe4a17
James Netherton <jamesnetherton@gmail.com>
2020-03-26 17:50:06 +0000

    Add GitHub extension
    Fixes #811

---
+++ extensions/github/deployment/src/main/java/org/apache/camel/quarkus/component/github/deployment/GithubProcessor.java
@@ -0,0 +1,59 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.quarkus.component.github.deployment;
+
+import java.io.Serializable;
+
+import io.quarkus.deployment.annotations.BuildProducer;
+import io.quarkus.deployment.annotations.BuildStep;
+import io.quarkus.deployment.builditem.AdditionalApplicationArchiveMarkerBuildItem;
+import io.quarkus.deployment.builditem.CombinedIndexBuildItem;
+import io.quarkus.deployment.builditem.ExtensionSslNativeSupportBuildItem;
+import io.quarkus.deployment.builditem.FeatureBuildItem;
+import io.quarkus.deployment.builditem.nativeimage.ReflectiveClassBuildItem;
+import org.jboss.jandex.DotName;
+import org.jboss.jandex.IndexView;
+
+class GithubProcessor {
+
+    private static final String FEATURE = "camel-github";
+
+    @BuildStep
+    FeatureBuildItem feature() {
+        return new FeatureBuildItem(FEATURE);
+    }
+
+    @BuildStep
+    ExtensionSslNativeSupportBuildItem activateSslNativeSupport() {
+        return new ExtensionSslNativeSupportBuildItem(FEATURE);
+    }
+
+    @BuildStep
+    AdditionalApplicationArchiveMarkerBuildItem egitArchiveMarker() {
+        return new AdditionalApplicationArchiveMarkerBuildItem("org/eclipse/egit/github");
+    }
+
+    @BuildStep
+    void registerForReflection(BuildProducer<ReflectiveClassBuildItem> reflectiveClass, CombinedIndexBuildItem combinedIndex) {
+        IndexView index = combinedIndex.getIndex();
+        index.getKnownDirectImplementors(DotName.createSimple(Serializable.class.getName()))
+                .stream()
+                .filter(classInfo -> classInfo.name().prefix().toString().equals("org.eclipse.egit.github.core"))
+                .map(className -> new ReflectiveClassBuildItem(false, true, className.name().toString()))
+                .forEach(reflectiveClass::produce);
+    }
+}
