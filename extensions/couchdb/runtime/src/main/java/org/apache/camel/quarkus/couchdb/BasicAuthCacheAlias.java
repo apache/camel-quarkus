e6b741f6a6364bd0305218b39ac8814811312cf2
aldettinger <aldettinger@gmail.com>
2020-03-27 09:16:10 +0100

    Added native support for Couchdb extension in producer mode #989

---
+++ extensions/couchdb/runtime/src/main/java/org/apache/camel/quarkus/couchdb/BasicAuthCacheAlias.java
@@ -0,0 +1,88 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.quarkus.couchdb;
+
+import java.util.Map;
+
+import com.oracle.svm.core.annotate.Alias;
+import com.oracle.svm.core.annotate.Substitute;
+import com.oracle.svm.core.annotate.TargetClass;
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScheme;
+import org.apache.http.client.AuthCache;
+import org.apache.http.conn.SchemePortResolver;
+import org.apache.http.util.Args;
+
+/**
+ * <code>BasicAuthCacheAlias</code> is an alias of <code>org.apache.http.impl.client.BasicAuthCache</code>
+ * that work in native mode by skipping AuthScheme serialization.
+ */
+@TargetClass(className = "org.apache.http.impl.client.BasicAuthCache")
+public final class BasicAuthCacheAlias implements AuthCache {
+
+    @Alias
+    private Map<HttpHost, AuthScheme> map;
+    @Alias
+    private SchemePortResolver schemePortResolver;
+
+    @Alias
+    public BasicAuthCacheAlias(final SchemePortResolver schemePortResolver) {
+    }
+
+    @Alias
+    public BasicAuthCacheAlias() {
+    }
+
+    @Alias
+    protected HttpHost getKey(final HttpHost host) {
+        return null;
+    }
+
+    @Substitute
+    @Override
+    public void put(final HttpHost host, final AuthScheme authScheme) {
+        Args.notNull(host, "HTTP host");
+        if (authScheme == null) {
+            return;
+        }
+        this.map.put(getKey(host), authScheme);
+    }
+
+    @Substitute
+    @Override
+    public AuthScheme get(final HttpHost host) {
+        Args.notNull(host, "HTTP host");
+        return this.map.get(getKey(host));
+    }
+
+    @Alias
+    @Override
+    public void remove(final HttpHost host) {
+    }
+
+    @Alias
+    @Override
+    public void clear() {
+    }
+
+    @Alias
+    @Override
+    public String toString() {
+        return null;
+    }
+
+}
---
+++ integration-tests/couchdb/src/main/java/org/apache/camel/quarkus/component/couchdb/it/CouchdbResource.java
@@ -0,0 +1,105 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.quarkus.component.couchdb.it;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.component.couchdb.CouchDbConstants;
+import org.apache.camel.component.couchdb.CouchDbOperations;
+import org.jboss.logging.Logger;
+import org.lightcouch.NoDocumentException;
+
+@Path("/couchdb")
+@ApplicationScoped
+public class CouchdbResource {
+
+    private static final String baseUri = "couchdb:http://{{camel.couchdb.test.server.authority}}/database";
+    private static final Logger LOG = Logger.getLogger(CouchdbResource.class);
+
+    @Inject
+    ProducerTemplate producerTemplate;
+
+    @Path("/create")
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public CouchdbTestDocument create(CouchdbTestDocument document) {
+        LOG.info("Invoking create");
+        Exchange createExchange = producerTemplate.request(baseUri + "?createDatabase=true",
+                e -> e.getMessage().setBody(document.toJsonObject()));
+        document.setId(createExchange.getMessage().getHeader(CouchDbConstants.HEADER_DOC_ID, String.class));
+        document.setRevision(createExchange.getMessage().getHeader(CouchDbConstants.HEADER_DOC_REV, String.class));
+        return document;
+    }
+
+    @Path("/get")
+    @GET
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public String get(CouchdbTestDocument document) {
+        LOG.info("Invoking get");
+        Exchange getExchange = producerTemplate.request(baseUri, e -> {
+            e.getMessage().setBody(document.toJsonObject());
+            e.getMessage().setHeader(CouchDbConstants.HEADER_METHOD, CouchDbOperations.GET);
+            e.getMessage().setHeader(CouchDbConstants.HEADER_DOC_ID, document.getId());
+        });
+        if (getExchange.getException(NoDocumentException.class) != null) {
+            return null;
+        } else {
+            return getExchange.getMessage().getBody(String.class);
+        }
+    }
+
+    @Path("/update")
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public CouchdbTestDocument update(CouchdbTestDocument document) {
+        LOG.info("Invoking update");
+        Exchange updateExchange = producerTemplate.request(baseUri, e -> {
+            e.getMessage().setBody(document.toJsonObject());
+        });
+        document.setId(updateExchange.getMessage().getHeader(CouchDbConstants.HEADER_DOC_ID, String.class));
+        document.setRevision(updateExchange.getMessage().getHeader(CouchDbConstants.HEADER_DOC_REV, String.class));
+        return document;
+    }
+
+    @Path("/delete")
+    @DELETE
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public CouchdbTestDocument delete(CouchdbTestDocument document) {
+        LOG.info("Invoking delete");
+        Exchange deleteExchange = producerTemplate.request(baseUri, e -> {
+            e.getMessage().setBody(document.toJsonObject());
+            e.getMessage().setHeader(CouchDbConstants.HEADER_METHOD, CouchDbOperations.DELETE);
+        });
+        document.setId(deleteExchange.getMessage().getHeader(CouchDbConstants.HEADER_DOC_ID, String.class));
+        document.setRevision(deleteExchange.getMessage().getHeader(CouchDbConstants.HEADER_DOC_REV, String.class));
+        return document;
+    }
+}
