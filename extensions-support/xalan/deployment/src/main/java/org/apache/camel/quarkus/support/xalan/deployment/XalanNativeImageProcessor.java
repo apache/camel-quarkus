91ed0408cec949f7117b63b04527593b4e805705
Peter Palaga <ppalaga@redhat.com>
2020-04-03 11:00:00 +0200

    Revisit #1017 Be even less intrusive when registering our TransformerFactory

--- extensions-support/xalan/deployment/src/main/java/org/apache/camel/quarkus/support/xalan/deployment/XalanNativeImageProcessor.java
+++ extensions-support/xalan/deployment/src/main/java/org/apache/camel/quarkus/support/xalan/deployment/XalanNativeImageProcessor.java
@@ -16,18 +16,31 @@
  */
 package org.apache.camel.quarkus.support.xalan.deployment;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.List;
 
 import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
-import io.quarkus.deployment.builditem.SystemPropertyBuildItem;
+import io.quarkus.deployment.builditem.GeneratedNativeImageClassBuildItem;
+import io.quarkus.deployment.builditem.GeneratedResourceBuildItem;
 import io.quarkus.deployment.builditem.nativeimage.NativeImageResourceBuildItem;
 import io.quarkus.deployment.builditem.nativeimage.NativeImageResourceBundleBuildItem;
 import io.quarkus.deployment.builditem.nativeimage.ReflectiveClassBuildItem;
-import io.quarkus.deployment.builditem.nativeimage.ServiceProviderBuildItem;
+import io.quarkus.gizmo.CatchBlockCreator;
+import io.quarkus.gizmo.ClassCreator;
+import io.quarkus.gizmo.ClassOutput;
+import io.quarkus.gizmo.MethodCreator;
+import io.quarkus.gizmo.TryBlock;
+import org.apache.camel.quarkus.core.graal.ResourceUtils;
+import org.apache.camel.quarkus.support.xalan.XalanTransformerFactory;
+import org.graalvm.nativeimage.hosted.Feature;
+
+import static io.quarkus.gizmo.MethodDescriptor.ofMethod;
 
 class XalanNativeImageProcessor {
+    private static final String TRANSFORMER_FACTORY_SERVICE_FILE_PATH = "META-INF/services/javax.xml.transform.TransformerFactory";
+
     @BuildStep
     ReflectiveClassBuildItem reflectiveClasses() {
         return new ReflectiveClassBuildItem(
@@ -58,26 +71,46 @@ class XalanNativeImageProcessor {
 
     @BuildStep
     void installTransformerFactory(
-            CamelXalanBuildTimeConfig config,
-            BuildProducer<SystemPropertyBuildItem> properties,
-            BuildProducer<ServiceProviderBuildItem> serviceProviders,
-            BuildProducer<NativeImageResourceBuildItem> nativeResources) {
+            BuildProducer<GeneratedNativeImageClassBuildItem> nativeImageClass,
+            BuildProducer<GeneratedResourceBuildItem> generatedResources) {
 
-        config.transformerFactory
-                .ifPresent(val -> properties.produce(
-                        /*
-                         * If we do not do this, the service provider defined in xalan.jar's
-                         * META-INF/services/javax.xml.transform.TransformerFactory
-                         * wins over our factory on Java 11+ native
-                         * I any case, the user has an option to pass his preferred factory instead of ours
-                         */
-                        new SystemPropertyBuildItem("javax.xml.transform.TransformerFactory", val)));
+        final String serviceProviderFileContent = XalanTransformerFactory.class.getName() + "\n";
 
-        serviceProviders.produce(
-                new ServiceProviderBuildItem("javax.xml.transform.TransformerFactory",
-                        "org.apache.camel.quarkus.support.xalan.XalanTransformerFactory"));
+        /* This is primarily for the JVM mode */
+        generatedResources.produce(
+                new GeneratedResourceBuildItem(TRANSFORMER_FACTORY_SERVICE_FILE_PATH,
+                        serviceProviderFileContent.getBytes(StandardCharsets.UTF_8)));
 
-        nativeResources.produce(new NativeImageResourceBuildItem("META-INF/services/javax.xml.transform.TransformerFactory"));
+        /* A low level way to embed only our service file in the native image.
+         * There are at least two META-INF/services/javax.xml.transform.TransformerFactory files
+         * in the class path: ours and the one from xalan.jar. As of GraalVM 19.3.1-java8, 19.3.1-java11,
+         * 20.0.0-java8 and 20.0.0-java11, there is no way to ensure that ServiceProviderBuildItem
+         * or NativeImageResourceBuildItem will pick the service file preferred by us.
+         * We are thus forced to use this low level mechanism to ensure that.
+         */
+        final ClassCreator file = new ClassCreator(new ClassOutput() {
+            @Override
+            public void write(String s, byte[] bytes) {
+                nativeImageClass.produce(new GeneratedNativeImageClassBuildItem(s, bytes));
+            }
+        }, getClass().getName() + "AutoFeature", null,
+                Object.class.getName(), Feature.class.getName());
+        file.addAnnotation("com.oracle.svm.core.annotate.AutomaticFeature");
+        final MethodCreator beforeAn = file.getMethodCreator("beforeAnalysis", "V",
+                Feature.BeforeAnalysisAccess.class.getName());
+        final TryBlock overallCatch = beforeAn.tryBlock();
+        overallCatch.invokeStaticMethod(
+                ofMethod(ResourceUtils.class, "registerResources", void.class,
+                        String.class, String.class),
+                overallCatch.load(TRANSFORMER_FACTORY_SERVICE_FILE_PATH),
+                overallCatch.load(serviceProviderFileContent));
+
+        final CatchBlockCreator print = overallCatch.addCatch(Throwable.class);
+        print.invokeVirtualMethod(ofMethod(Throwable.class, "printStackTrace", void.class), print.getCaughtException());
+
+        beforeAn.returnValue(null);
+        file.close();
+
     }
 
 }
