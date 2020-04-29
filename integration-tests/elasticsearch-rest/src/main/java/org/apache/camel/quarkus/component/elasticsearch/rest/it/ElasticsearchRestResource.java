62d2b10b24802a008419a8fcded4defe11db8c4c
James Netherton <jamesnetherton@gmail.com>
2020-04-29 13:12:23 +0100

    Add native support for Elasticsearch REST
    Fixes #896

---
+++ integration-tests/elasticsearch-rest/src/main/java/org/apache/camel/quarkus/component/elasticsearch/rest/it/ElasticsearchRestResource.java
@@ -0,0 +1,99 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.quarkus.component.elasticsearch.rest.it;
+
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.PATCH;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.component.elasticsearch.ElasticsearchConstants;
+import org.elasticsearch.action.get.GetResponse;
+
+@Path("/elasticsearch-rest")
+@ApplicationScoped
+public class ElasticsearchRestResource {
+
+    @Inject
+    ProducerTemplate producerTemplate;
+
+    @Path("/get")
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response getData(@QueryParam("indexId") String indexId) throws Exception {
+        GetResponse response = producerTemplate
+                .requestBody("elasticsearch-rest://elasticsearch?operation=GetById&indexName=test", indexId, GetResponse.class);
+
+        if (response.getSource() == null) {
+            return Response.status(404).build();
+        }
+        return Response.ok().entity(response.getSource().get("test-key")).build();
+    }
+
+    @Path("/index")
+    @POST
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response indexData(String indexValue) throws Exception {
+        Map<String, String> data = createIndexedData(indexValue);
+        String indexId = producerTemplate.requestBody("elasticsearch-rest://elasticsearch?operation=Index&indexName=test", data,
+                String.class);
+        return Response
+                .created(new URI("https://camel.apache.org/"))
+                .entity(indexId)
+                .build();
+    }
+
+    @Path("/update")
+    @PATCH
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response updateData(@QueryParam("indexId") String indexId, String indexValue) throws Exception {
+        Map<String, String> data = createIndexedData(indexValue);
+        Map<String, Object> headers = new HashMap<>();
+        headers.put(ElasticsearchConstants.PARAM_INDEX_ID, indexId);
+
+        producerTemplate.requestBodyAndHeaders("elasticsearch-rest://elasticsearch?operation=Update&indexName=test", data,
+                headers);
+        return Response.ok().build();
+    }
+
+    @Path("/delete")
+    @DELETE
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response deleteData(@QueryParam("indexId") String indexId) throws Exception {
+        producerTemplate.requestBody("elasticsearch-rest://elasticsearch?operation=Delete&indexName=test", indexId);
+        return Response.noContent().build();
+    }
+
+    private Map<String, String> createIndexedData(String indexValue) {
+        Map<String, String> map = new HashMap<>();
+        map.put("test-key", indexValue);
+        return map;
+    }
+
+}
---
+++ integration-tests/elasticsearch-rest/src/test/java/org/apache/camel/quarkus/component/elasticsearch/rest/it/ElasticSearchTestResource.java
@@ -0,0 +1,70 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.quarkus.component.elasticsearch.rest.it;
+
+import java.util.Map;
+
+import io.quarkus.test.common.QuarkusTestResourceLifecycleManager;
+import org.apache.camel.util.CollectionHelper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.utility.TestcontainersConfiguration;
+
+public class ElasticSearchTestResource implements QuarkusTestResourceLifecycleManager {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ElasticSearchTestResource.class);
+    private static final String ELASTICSEARCH_IMAGE = "elasticsearch:7.6.1";
+    private static final int ELASTICSEARCH_PORT = 9200;
+
+    private GenericContainer container;
+
+    @Override
+    public Map<String, String> start() {
+        LOGGER.info(TestcontainersConfiguration.getInstance().toString());
+
+        try {
+            container = new GenericContainer(ELASTICSEARCH_IMAGE)
+                    .withExposedPorts(ELASTICSEARCH_PORT)
+                    .withLogConsumer(new Slf4jLogConsumer(LOGGER))
+                    .withEnv("discovery.type", "single-node")
+                    .waitingFor(Wait.forListeningPort());
+
+            container.start();
+
+            return CollectionHelper.mapOf(
+                    "camel.component.elasticsearch-rest.host-addresses",
+                    String.format("localhost:%s", container.getMappedPort(ELASTICSEARCH_PORT)));
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void stop() {
+        try {
+            if (container != null) {
+                container.stop();
+            }
+        } catch (Exception e) {
+            // Ignored
+        }
+    }
+}
