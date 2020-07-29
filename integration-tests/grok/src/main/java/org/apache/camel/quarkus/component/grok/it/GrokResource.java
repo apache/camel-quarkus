7a5a5b7312f39578d066031793b497c7a680340c
aldettinger <aldettinger@gmail.com>
2020-07-29 09:46:09 +0200

    Added grok support #1466

---
+++ integration-tests/grok/src/main/java/org/apache/camel/quarkus/component/grok/it/GrokResource.java
@@ -0,0 +1,136 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.quarkus.component.grok.it;
+
+import java.util.Map;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+import org.apache.camel.CamelExecutionException;
+import org.apache.camel.ProducerTemplate;
+import org.jboss.logging.Logger;
+
+@Path("/grok")
+@ApplicationScoped
+public class GrokResource {
+
+    private static final Logger LOG = Logger.getLogger(GrokResource.class);
+
+    @Inject
+    ProducerTemplate template;
+
+    @Path("/log")
+    @GET
+    public String log(String input) {
+        LOG.infof("Calling log with %s", input);
+        return template.requestBody("direct:log", input, String.class);
+    }
+
+    @Path("/fooBar")
+    @GET
+    public String fooBar(String input) {
+        LOG.infof("Calling fooBar with %s", input);
+        return template.requestBody("direct:fooBar", input, String.class);
+    }
+
+    @Path("/ip")
+    @GET
+    public String ip(String input) {
+        LOG.infof("Calling ip with %s", input);
+        return template.requestBody("direct:ip", input, String.class);
+    }
+
+    @Path("/qs")
+    @GET
+    public String qs(String input) {
+        LOG.infof("Calling qs with %s", input);
+        return template.requestBody("direct:qs", input, String.class);
+    }
+
+    @Path("/uuid")
+    @GET
+    public String uuid(String input) {
+        LOG.infof("Calling uuid with %s", input);
+        return template.requestBody("direct:uuid", input, String.class);
+    }
+
+    @Path("/mac")
+    @GET
+    public String mac(String input) {
+        LOG.infof("Calling mac with %s", input);
+        return template.requestBody("direct:mac", input, String.class);
+    }
+
+    @Path("/path")
+    @GET
+    public String path(String input) {
+        LOG.infof("Calling path with %s", input);
+        return template.requestBody("direct:path", input, String.class);
+    }
+
+    @Path("/uri")
+    @GET
+    public String uri(String input) {
+        LOG.infof("Calling uri with %s", input);
+        return template.requestBody("direct:uri", input, String.class);
+    }
+
+    @Path("/num")
+    @GET
+    public String num(String input) {
+        LOG.infof("Calling num with %s", input);
+        return template.requestBody("direct:num", input, String.class);
+    }
+
+    @Path("/timestamp")
+    @GET
+    public String timestamp(String input) {
+        LOG.infof("Calling timestamp with %s", input);
+        return template.requestBody("direct:timestamp", input, String.class);
+    }
+
+    @Path("/flatten")
+    @GET
+    public String flatten(String input) {
+        LOG.infof("Calling flatten with %s", input);
+        try {
+            template.requestBody("direct:flatten", input, String.class);
+        } catch (CamelExecutionException cex) {
+            return cex.getCause().getClass().getName();
+        }
+        return null;
+    }
+
+    @Path("/namedOnly")
+    @GET
+    public String namedOnly(String input) {
+        LOG.infof("Calling namedOnly with %s", input);
+        Map<?, ?> r = template.requestBody("direct:namedOnly", input, Map.class);
+        return String.format("%s-%s+%s", r.containsKey("URIPROTO"), r.containsKey("URIHOST"), r.containsKey("URIPATHPARAM"));
+    }
+
+    @Path("/singleMatchPerLine")
+    @GET
+    public String singleMatchPerLine(String input) {
+        LOG.infof("Calling singleMatchPerLine with %s", input);
+        return template.requestBody("direct:singleMatchPerLine", input, String.class);
+    }
+
+}
---
+++ integration-tests/grok/src/main/java/org/apache/camel/quarkus/component/grok/it/GrokRoute.java
@@ -0,0 +1,58 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.quarkus.component.grok.it;
+
+import javax.enterprise.context.ApplicationScoped;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.grok.GrokDataFormat;
+import org.apache.camel.component.grok.GrokPattern;
+import org.apache.camel.spi.DataFormat;
+
+@ApplicationScoped
+public class GrokRoute extends RouteBuilder {
+
+    @Override
+    public void configure() {
+
+        bindToRegistry("myCustomPatternBean", new GrokPattern("FOOBAR", "foo|bar"));
+        GrokPattern myAnotherCustomPatternBean = new GrokPattern("FOOBAR_WITH_PREFIX_AND_SUFFIX", "-- %{FOOBAR}+ --");
+        bindToRegistry("myAnotherCustomPatternBean", myAnotherCustomPatternBean);
+
+        from("direct:log").unmarshal().grok("%{COMMONAPACHELOG}").setBody(simple("ip: ${body[4][clientip]}"));
+        from("direct:fooBar").unmarshal().grok("%{FOOBAR_WITH_PREFIX_AND_SUFFIX:fooBar}").setBody(simple("${body[fooBar]}"));
+        from("direct:ip").unmarshal().grok("%{IP:ip}").setBody(simple("${body[0][ip]} -> ${body[3][ip]}"));
+
+        from("direct:qs").unmarshal().grok("%{QS:qs}").setBody(simple("${body[qs]}"));
+        from("direct:uuid").unmarshal().grok("%{UUID:uuid}").setBody(simple("${body[uuid]}"));
+        from("direct:mac").unmarshal().grok("%{MAC:mac}").setBody(simple("${body[mac]}"));
+        from("direct:path").unmarshal().grok("%{PATH:path}").setBody(simple("${body[path]}"));
+        from("direct:uri").unmarshal().grok("%{URI:uri}").setBody(simple("${body[uri]}"));
+        from("direct:num").unmarshal().grok("%{NUMBER:num}").setBody(simple("${body[num]}"));
+        from("direct:timestamp").unmarshal().grok("%{TIMESTAMP_ISO8601:timestamp}").setBody(simple("${body[timestamp]}"));
+
+        DataFormat flattenDf = new GrokDataFormat("%{INT:i} %{INT:i}").setFlattened(true);
+        from("direct:flatten").unmarshal(flattenDf);
+
+        DataFormat namedOnlyDf = new GrokDataFormat("%{URI:website}").setNamedOnly(true);
+        from("direct:namedOnly").unmarshal(namedOnlyDf);
+
+        DataFormat singleMatchPerLineDf = new GrokDataFormat("%{INT:i}").setAllowMultipleMatchesPerLine(false);
+        from("direct:singleMatchPerLine").unmarshal(singleMatchPerLineDf).setBody(simple("${body[0][i]}-${body[1][i]}"));
+    }
+
+}
