d21369049a303dadf2406082fb93867b3e2f0f20
James Netherton <jamesnetherton@gmail.com>
2020-08-07 09:17:14 +0100

    Remove remaining camel UriParams reflective class registrations
    Fixes #1108

--- extensions/google-drive/deployment/src/main/java/org/apache/camel/quarkus/component/google/drive/deployment/GoogleDriveProcessor.java
+++ extensions/google-drive/deployment/src/main/java/org/apache/camel/quarkus/component/google/drive/deployment/GoogleDriveProcessor.java
@@ -16,19 +16,10 @@
  */
 package org.apache.camel.quarkus.component.google.drive.deployment;
 
-import java.util.Collection;
-
 import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.builditem.AdditionalApplicationArchiveMarkerBuildItem;
-import io.quarkus.deployment.builditem.CombinedIndexBuildItem;
 import io.quarkus.deployment.builditem.FeatureBuildItem;
-import io.quarkus.deployment.builditem.nativeimage.ReflectiveClassBuildItem;
-import org.apache.camel.quarkus.core.deployment.spi.UnbannedReflectiveBuildItem;
-import org.jboss.jandex.AnnotationInstance;
-import org.jboss.jandex.AnnotationTarget;
-import org.jboss.jandex.DotName;
-import org.jboss.jandex.IndexView;
 
 class GoogleDriveProcessor {
 
@@ -42,25 +33,5 @@ class GoogleDriveProcessor {
     @BuildStep
     void applicationArchiveMarkers(BuildProducer<AdditionalApplicationArchiveMarkerBuildItem> applicationArchiveMarker) {
         applicationArchiveMarker.produce(new AdditionalApplicationArchiveMarkerBuildItem("com/google/api/services/drive"));
-    }
-
-    @BuildStep
-    void registerForReflection(BuildProducer<ReflectiveClassBuildItem> reflectiveClass,
-            BuildProducer<UnbannedReflectiveBuildItem> unbannedClass, CombinedIndexBuildItem combinedIndex) {
-        IndexView index = combinedIndex.getIndex();
-
-        // Google drive component configuration class reflection
-        Collection<AnnotationInstance> uriParams = index
-                .getAnnotations(DotName.createSimple("org.apache.camel.spi.UriParams"));
-
-        String[] googleDriveConfigClasses = uriParams.stream()
-                .map(annotation -> annotation.target())
-                .filter(annotationTarget -> annotationTarget.kind().equals(AnnotationTarget.Kind.CLASS))
-                .map(annotationTarget -> annotationTarget.asClass().name().toString())
-                .filter(className -> className.startsWith("org.apache.camel.component.google.drive"))
-                .toArray(String[]::new);
-
-        reflectiveClass.produce(new ReflectiveClassBuildItem(true, true, googleDriveConfigClasses));
-        unbannedClass.produce(new UnbannedReflectiveBuildItem(googleDriveConfigClasses));
     }
 }
