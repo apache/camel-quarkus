b4500f5cda71158be54da7d5a02ac174233f5675
aldettinger <aldettinger@gmail.com>
2020-09-02 10:41:38 +0200

    Added nats native support fixes #1578

---
+++ integration-tests/nats/src/test/java/org/apache/camel/quarkus/component/nats/it/NatsTest.java
@@ -0,0 +1,120 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.quarkus.component.nats.it;
+
+import java.util.concurrent.TimeUnit;
+
+import io.quarkus.test.common.QuarkusTestResource;
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.Header;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.awaitility.Awaitility.await;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@QuarkusTestResource(NatsTestResource.class)
+@QuarkusTest
+class NatsTest {
+
+    @Test
+    void basicAuthProduceConsumeRoundTripShouldSucceed() {
+        Header header = new Header("sendToEndpointUri", "natsBasicAuth:test");
+        given().when().header(header).body("basic-auth-msg").post("/nats/send").then().statusCode(204);
+
+        await().atMost(10L, TimeUnit.SECONDS).until(() -> {
+            return given().get("/nats/messages/basic-auth").path("size()").equals(1);
+        });
+
+        String[] messages = given().get("/nats/messages/basic-auth").then().statusCode(200).extract().as(String[].class);
+        assertEquals(1, messages.length);
+        assertEquals("basic-auth-msg", messages[0]);
+    }
+
+    @Test
+    void noAuthProduceConsumeRoundTripShouldSucceed() {
+        Header header = new Header("sendToEndpointUri", "natsNoAuth:test");
+        given().when().header(header).body("no-auth-msg").post("/nats/send").then().statusCode(204);
+
+        await().atMost(10L, TimeUnit.SECONDS).until(() -> {
+            return given().get("/nats/messages/no-auth").path("size()").equals(1);
+        });
+
+        String[] messages = given().get("/nats/messages/no-auth").then().statusCode(200).extract().as(String[].class);
+        assertEquals(1, messages.length);
+        assertEquals("no-auth-msg", messages[0]);
+    }
+
+    @Test
+    void tokenAuthProduceConsumeRoundTripShouldSucceed() {
+        Header header = new Header("sendToEndpointUri", "natsTokenAuth:test");
+        given().when().header(header).body("token-auth-msg").post("/nats/send").then().statusCode(204);
+
+        await().atMost(10L, TimeUnit.SECONDS).until(() -> {
+            return given().get("/nats/messages/token-auth").path("size()").equals(1);
+        });
+
+        String[] messages = given().get("/nats/messages/token-auth").then().statusCode(200).extract().as(String[].class);
+        assertEquals(1, messages.length);
+        assertEquals("token-auth-msg", messages[0]);
+    }
+
+    @Test
+    void consumeMaxMessagesShouldRetainFirstTwoMessages() {
+        Header header = new Header("sendToEndpointUri", "natsNoAuth:max");
+        for (int msgNumber = 1; msgNumber <= 10; msgNumber++) {
+            given().when().header(header).body("msg " + msgNumber).post("/nats/send").then().statusCode(204);
+        }
+
+        await().atMost(10L, TimeUnit.SECONDS).until(() -> {
+            return given().get("/nats/messages/2-msg-max").path("size()").equals(2);
+        });
+
+        String[] messages = given().get("/nats/messages/2-msg-max").then().statusCode(200).extract().as(String[].class);
+        assertEquals(2, messages.length);
+        assertEquals("msg 1", messages[0]);
+        assertEquals("msg 2", messages[1]);
+    }
+
+    @Test
+    void consumeMaxQueueMessagesShouldRetainRightNumberOfMessages() {
+        Header header = new Header("sendToEndpointUri", "natsNoAuth:qmax");
+        for (int msgNumber = 1; msgNumber <= 20; msgNumber++) {
+            given().when().header(header).body("qmsg " + msgNumber).post("/nats/send").then().statusCode(204);
+        }
+
+        await().atMost(10L, TimeUnit.SECONDS).until(() -> {
+            return given().get("/nats/messages/3-qmsg-max").path("size()").equals(3)
+                    && given().get("/nats/messages/8-qmsg-max").path("size()").equals(8);
+        });
+    }
+
+    @Test
+    void requestReplyShouldSucceed() {
+        Header header = new Header("sendToEndpointUri", "natsNoAuth:request-reply?replySubject=reply");
+        given().when().header(header).body("Request").post("/nats/send").then().statusCode(204);
+
+        await().atMost(10L, TimeUnit.SECONDS).until(() -> {
+            return given().get("/nats/messages/reply").path("size()").equals(1);
+        });
+
+        String[] messages = given().get("/nats/messages/reply").then().statusCode(200).extract().as(String[].class);
+        assertEquals(1, messages.length);
+        assertEquals("Request => Reply", messages[0]);
+    }
+
+}
---
+++ integration-tests/nats/src/test/java/org/apache/camel/quarkus/component/nats/it/NatsTestResource.java
@@ -0,0 +1,97 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.quarkus.component.nats.it;
+
+import java.util.Map;
+
+import org.apache.camel.quarkus.testcontainers.ContainerResourceLifecycleManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.utility.TestcontainersConfiguration;
+
+import static org.apache.camel.quarkus.component.nats.it.NatsConfiguration.NATS_BROKER_URL_BASIC_AUTH_CONFIG_KEY;
+import static org.apache.camel.quarkus.component.nats.it.NatsConfiguration.NATS_BROKER_URL_NO_AUTH_CONFIG_KEY;
+import static org.apache.camel.quarkus.component.nats.it.NatsConfiguration.NATS_BROKER_URL_TOKEN_AUTH_CONFIG_KEY;
+import static org.apache.camel.util.CollectionHelper.mapOf;
+
+public class NatsTestResource implements ContainerResourceLifecycleManager {
+
+    private static final Logger LOG = LoggerFactory.getLogger(NatsTestResource.class);
+    private static final String BASIC_AUTH_USERNAME = "admin";
+    private static final String BASIC_AUTH_PASSWORD = "password";
+    private static final String NATS_IMAGE = "nats:2.1.4";
+    private static final int NATS_SERVER_PORT = 4222;
+    private static final String TOKEN_AUTH_TOKEN = "!admin23456";
+
+    private GenericContainer basicAuthContainer, noAuthContainer, tokenAuthContainer;
+
+    @Override
+    public Map<String, String> start() {
+        LOG.info(TestcontainersConfiguration.getInstance().toString());
+
+        // Start the container needed for the basic authentication test
+        basicAuthContainer = new GenericContainer(NATS_IMAGE).withExposedPorts(NATS_SERVER_PORT)
+                .withCommand("-DV", "--user", BASIC_AUTH_USERNAME, "--pass", BASIC_AUTH_PASSWORD)
+                .waitingFor(Wait.forLogMessage(".*Server is ready.*", 1));
+        basicAuthContainer.start();
+        String basicAuthIp = basicAuthContainer.getContainerIpAddress();
+        Integer basicAuthPort = basicAuthContainer.getMappedPort(NATS_SERVER_PORT);
+        String basicAuthAuthority = BASIC_AUTH_USERNAME + ":" + BASIC_AUTH_PASSWORD;
+        String basicAuthBrokerUrl = String.format("%s@%s:%d", basicAuthAuthority, basicAuthIp, basicAuthPort);
+
+        // Start the container needed for tests without authentication
+        noAuthContainer = new GenericContainer(NATS_IMAGE).withExposedPorts(NATS_SERVER_PORT)
+                .waitingFor(Wait.forLogMessage(".*Listening for route connections.*", 1));
+        noAuthContainer.start();
+        String noAuthIp = noAuthContainer.getContainerIpAddress();
+        Integer noAuthPort = noAuthContainer.getMappedPort(NATS_SERVER_PORT);
+        String noAuthBrokerUrl = String.format("%s:%s", noAuthIp, noAuthPort);
+
+        // Start the container needed for the token authentication test
+        tokenAuthContainer = new GenericContainer(NATS_IMAGE).withExposedPorts(NATS_SERVER_PORT)
+                .withCommand("-DV", "-auth", TOKEN_AUTH_TOKEN)
+                .waitingFor(Wait.forLogMessage(".*Server is ready.*", 1));
+        tokenAuthContainer.start();
+        String tokenAuthIp = tokenAuthContainer.getContainerIpAddress();
+        Integer tokenAuthPort = tokenAuthContainer.getMappedPort(NATS_SERVER_PORT);
+        String tokenAuthBrokerUrl = String.format("%s@%s:%d", TOKEN_AUTH_TOKEN, tokenAuthIp, tokenAuthPort);
+
+        Map<String, String> Properties = mapOf(NATS_BROKER_URL_BASIC_AUTH_CONFIG_KEY, basicAuthBrokerUrl);
+        Properties.put(NATS_BROKER_URL_NO_AUTH_CONFIG_KEY, noAuthBrokerUrl);
+        Properties.put(NATS_BROKER_URL_TOKEN_AUTH_CONFIG_KEY, tokenAuthBrokerUrl);
+        return Properties;
+    }
+
+    @Override
+    public void stop() {
+        stop(basicAuthContainer, "natsBasicAuthContainer");
+        stop(noAuthContainer, "natsNoAuthContainer");
+        stop(tokenAuthContainer, "natsTokenAuthContainer");
+    }
+
+    private void stop(GenericContainer<?> container, String id) {
+        try {
+            if (container != null) {
+                container.stop();
+            }
+        } catch (Exception ex) {
+            LOG.error("An issue occured while stopping " + id, ex);
+        }
+    }
+}
