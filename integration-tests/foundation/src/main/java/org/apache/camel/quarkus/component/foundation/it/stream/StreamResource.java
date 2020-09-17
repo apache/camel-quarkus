46896f1e25454f897b882b6e0e8f6db4a07053e9
James Netherton <jamesnetherton@gmail.com>
2020-09-17 08:09:41 +0100

    Combine some of the simpler component extensions into a single itest module

---
+++ integration-tests/foundation/src/main/java/org/apache/camel/quarkus/component/foundation/it/stream/StreamResource.java
@@ -0,0 +1,48 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.quarkus.component.foundation.it.stream;
+
+import java.io.ByteArrayOutputStream;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.apache.camel.ProducerTemplate;
+
+@Path("/stream")
+@ApplicationScoped
+public class StreamResource {
+
+    @Inject
+    ProducerTemplate producerTemplate;
+
+    @Path("/post")
+    @POST
+    @Consumes(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.TEXT_PLAIN)
+    public String greetingStream(String message) throws Exception {
+        try (ByteArrayOutputStream stream = new ByteArrayOutputStream()) {
+            producerTemplate.sendBodyAndHeader("stream:header", message.getBytes(), "stream", stream);
+            return "Hello " + stream.toString();
+        }
+    }
+}
