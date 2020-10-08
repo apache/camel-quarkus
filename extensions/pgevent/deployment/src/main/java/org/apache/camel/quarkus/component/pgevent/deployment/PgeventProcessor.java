71a1157b09779194c40be8684c5f64dbda46af20
Zineb Bendhiba <bendhiba.zineb@gmail.com>
2020-10-08 10:48:53 +0200

    PostgresSQL Event native support fixes #1719

---
+++ extensions/pgevent/deployment/src/main/java/org/apache/camel/quarkus/component/pgevent/deployment/PgeventProcessor.java
@@ -0,0 +1,68 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.quarkus.component.pgevent.deployment;
+
+import java.io.IOException;
+import java.sql.Driver;
+import java.util.Set;
+import java.util.stream.Stream;
+
+import com.impossibl.postgres.system.procs.ProcProvider;
+import io.quarkus.deployment.annotations.BuildProducer;
+import io.quarkus.deployment.annotations.BuildStep;
+import io.quarkus.deployment.builditem.FeatureBuildItem;
+import io.quarkus.deployment.builditem.nativeimage.ReflectiveClassBuildItem;
+import io.quarkus.deployment.builditem.nativeimage.ServiceProviderBuildItem;
+import io.quarkus.deployment.util.ServiceUtil;
+
+class PgeventProcessor {
+
+    private static final String PGEVENT_SERVICE_BASE = "META-INF/services/";
+
+    private static final String FEATURE = "camel-pgevent";
+
+    @BuildStep
+    FeatureBuildItem feature() {
+        return new FeatureBuildItem(FEATURE);
+    }
+
+    @BuildStep
+    ReflectiveClassBuildItem registerReflectiveClasses() {
+        return new ReflectiveClassBuildItem(false, true, "io.netty.channel.nio.NioEventLoopGroup");
+    }
+
+    @BuildStep
+    void registerNativeImageResources(BuildProducer<ServiceProviderBuildItem> services) {
+        Stream.of(
+                PGEVENT_SERVICE_BASE + ProcProvider.class.getName(),
+                PGEVENT_SERVICE_BASE + Driver.class.getName())
+                .forEach(service -> {
+                    try {
+                        Set<String> implementations = ServiceUtil.classNamesNamedIn(
+                                Thread.currentThread().getContextClassLoader(),
+                                service);
+                        services.produce(
+                                new ServiceProviderBuildItem(ProcProvider.class.getName(),
+                                        implementations.toArray(new String[0])));
+
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
+    }
+
+}
---
+++ integration-tests/pgevent/src/main/java/org/apache/camel/quarkus/component/pgevent/it/PgeventResource.java
@@ -0,0 +1,56 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.quarkus.component.pgevent.it;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
+import org.apache.camel.ProducerTemplate;
+import org.jboss.logging.Logger;
+
+@Path("/pgevent")
+@ApplicationScoped
+public class PgeventResource {
+
+    private static final Logger LOG = Logger.getLogger(PgeventResource.class);
+
+    @Inject
+    ProducerTemplate producerTemplate;
+
+    @GET
+    @Path("{event}")
+    public Response publishEvent(@PathParam("event") String event) {
+        LOG.infof("publish event %s", event);
+        String response = producerTemplate.requestBody("direct:pgevent-pub", event, String.class);
+        LOG.infof("message received : %s", response);
+        return Response.accepted(response).build();
+    }
+
+    @GET
+    @Path("datasource/{event}")
+    public Response publishEventWithDatasource(@PathParam("event") String event) {
+        LOG.infof("publish event %s", event);
+        String response = producerTemplate.requestBody("direct:pgevent-datasource", event, String.class);
+        LOG.infof("message received : %s", response);
+        return Response.accepted(response).build();
+    }
+
+}
