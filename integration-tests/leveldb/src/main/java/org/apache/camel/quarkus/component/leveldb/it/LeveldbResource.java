0ac2c6b9e66751b0899b3c1ab376952b2642aa97
JiriOndrusek <ondrusek.jiri@gmail.com>
2020-10-09 15:27:44 +0200

    LevelDB native support #1839

---
+++ integration-tests/leveldb/src/main/java/org/apache/camel/quarkus/component/leveldb/it/LeveldbResource.java
@@ -0,0 +1,135 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.quarkus.component.leveldb.it;
+
+import java.net.URI;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.component.mock.MockEndpoint;
+
+import static java.util.stream.Collectors.joining;
+
+@Path("/leveldb")
+@ApplicationScoped
+public class LeveldbResource {
+
+    public static String PARAMETER_BODY = "body";
+    public static String PARAMETER_FROM_ENDPOINT = "fromEndpoint";
+    public static String MOCKS_DELIMITER = ",";
+
+    @Inject
+    ProducerTemplate producerTemplate;
+
+    @Inject
+    CamelContext context;
+
+    @Path("/aggregate")
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response aggregateTest(List<String> messages,
+            @QueryParam("path") String path,
+            @DefaultValue(LeveldbRouteBuilder.MOCK_RESULT) @QueryParam("mocks") String mockNames) throws Exception {
+
+        String[] mockNamesArray = mockNames.split(MOCKS_DELIMITER);
+        MockEndpoint[] mocks = new MockEndpoint[mockNamesArray.length];
+
+        for (int i = 0; i < mocks.length; i++) {
+            mocks[i] = context.getEndpoint(mockNamesArray[i], MockEndpoint.class);
+            mocks[i].reset();
+
+            if (i == 0) {
+                mocks[i].expectedBodiesReceived(messages.stream().sequential().collect(joining("+")));
+            }
+        }
+
+        for (String message : messages) {
+            producerTemplate.sendBodyAndHeader(path, message, "id", 123);
+        }
+
+        mocks[0].assertIsSatisfied(context, 30, TimeUnit.SECONDS);
+
+        Map<String, List<Map<String, Object>>> data = new HashMap();
+        for (int i = 0; i < mocks.length; i++) {
+            data.put(mockNamesArray[i], extractDataFromMock(mocks[i]));
+        }
+
+        return Response
+                .created(new URI("https://camel.apache.org/"))
+                .entity(data)
+                .build();
+    }
+
+    @Path("/aggregateBinary")
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response aggregateBinaryTest(String[] messages) throws Exception {
+
+        MockEndpoint mockResult = context.getEndpoint(LeveldbRouteBuilder.MOCK_RESULT, MockEndpoint.class);
+        mockResult.reset();
+
+        byte[] longestBytes = messages[0].getBytes();
+        for (String message : messages) {
+            byte[] b = message.getBytes();
+            producerTemplate.sendBodyAndHeader(LeveldbRouteBuilder.DIRECT_BINARY, message.getBytes(), "id", 123);
+            if (b.length > longestBytes.length) {
+                longestBytes = b;
+            }
+        }
+
+        mockResult.assertIsSatisfied(context, 30, TimeUnit.SECONDS);
+
+        byte[] result = mockResult.getExchanges().get(0).getIn().getBody(byte[].class);
+
+        return Response
+                .created(new URI("https://camel.apache.org/"))
+                .entity(Arrays.equals(longestBytes, result))
+                .build();
+
+    }
+
+    private List<Map<String, Object>> extractDataFromMock(MockEndpoint mockEndpoint) {
+        List<Map<String, Object>> data = mockEndpoint.getReceivedExchanges().stream().sequential()
+                .map(exchange -> {
+                    Map<String, Object> map = new HashMap<>(exchange.getIn().getHeaders());
+                    map.put(PARAMETER_FROM_ENDPOINT, exchange.getFromEndpoint().getEndpointUri());
+                    map.put(PARAMETER_BODY, String.valueOf(exchange.getIn().getBody()));
+                    return map;
+                })
+                .collect(Collectors.toList());
+        return data;
+    }
+}
---
+++ integration-tests/leveldb/src/test/java/org/apache/camel/quarkus/component/leveldb/it/LeveldbTest.java
@@ -0,0 +1,161 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.quarkus.component.leveldb.it;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import org.apache.camel.Exchange;
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@QuarkusTest
+class LeveldbTest {
+
+    @Test
+    public void testAggregate() {
+        Map<String, List<Map<String, Object>>> data = testAggregate(LeveldbRouteBuilder.DIRECT_START,
+                Arrays.asList("S", "H", "E", "L", "D", "O", "N"));
+
+        List<Map<String, Object>> resultData = data.get(LeveldbRouteBuilder.MOCK_RESULT);
+
+        assertEquals("direct://start", resultData.get(0).get(LeveldbResource.PARAMETER_FROM_ENDPOINT));
+    }
+
+    @Test
+    public void testAggregateRecovery() {
+        Map<String, List<Map<String, Object>>> data = testAggregate(LeveldbRouteBuilder.DIRECT_START_WITH_FAILURE,
+                Arrays.asList("S", "H", "E", "L", "D", "O", "N"));
+
+        List<Map<String, Object>> resultData = data.get(LeveldbRouteBuilder.MOCK_RESULT);
+
+        assertEquals(Boolean.TRUE, resultData.get(0).get(Exchange.REDELIVERED));
+        assertEquals(2, resultData.get(0).get(Exchange.REDELIVERY_COUNTER));
+        assertEquals("direct://startWithFailure", resultData.get(0).get(LeveldbResource.PARAMETER_FROM_ENDPOINT));
+    }
+
+    @Test
+    public void testDeadLetter() {
+        Map<String, List<Map<String, Object>>> data = testAggregate(LeveldbRouteBuilder.DIRECT_START_DEAD_LETTER,
+                Arrays.asList("S", "H", "E", "L", "D", "O", "N"),
+                LeveldbRouteBuilder.MOCK_DEAD + "," + LeveldbRouteBuilder.MOCK_RESULT + ","
+                        + LeveldbRouteBuilder.MOCK_AGGREGATED);
+
+        List<Map<String, Object>> deadData = data.get(LeveldbRouteBuilder.MOCK_DEAD);
+        List<Map<String, Object>> resultData = data.get(LeveldbRouteBuilder.MOCK_RESULT);
+        List<Map<String, Object>> agreggatedData = data.get(LeveldbRouteBuilder.MOCK_AGGREGATED);
+
+        assertTrue(resultData.isEmpty());
+
+        assertFalse(agreggatedData.get(0).containsKey(Exchange.REDELIVERED));
+        assertEquals(Boolean.TRUE, agreggatedData.get(1).containsKey(Exchange.REDELIVERED));
+        assertEquals(1, agreggatedData.get(1).get(Exchange.REDELIVERY_COUNTER));
+        assertEquals(3, agreggatedData.get(1).get(Exchange.REDELIVERY_MAX_COUNTER));
+        assertEquals(Boolean.TRUE, agreggatedData.get(2).containsKey(Exchange.REDELIVERED));
+        assertEquals(2, agreggatedData.get(2).get(Exchange.REDELIVERY_COUNTER));
+        assertEquals(3, agreggatedData.get(2).get(Exchange.REDELIVERY_MAX_COUNTER));
+        assertEquals(Boolean.TRUE, agreggatedData.get(3).containsKey(Exchange.REDELIVERED));
+        assertEquals(3, agreggatedData.get(3).get(Exchange.REDELIVERY_COUNTER));
+        assertEquals(3, agreggatedData.get(3).get(Exchange.REDELIVERY_MAX_COUNTER));
+
+        assertEquals(Boolean.TRUE, deadData.get(0).containsKey(Exchange.REDELIVERED));
+        assertEquals(3, deadData.get(0).get(Exchange.REDELIVERY_COUNTER));
+        assertFalse(deadData.get(0).containsKey(Exchange.REDELIVERY_MAX_COUNTER));
+    }
+
+    @Test
+    public void testBinaryData() throws Exception {
+
+        boolean theSame = RestAssured.given()
+                .contentType(ContentType.JSON)
+                .body(Arrays.asList("ab", "Sheldon", "cde"))
+                .post("/leveldb/aggregateBinary")
+                .then()
+                .statusCode(201)
+                .extract().as(Boolean.class);
+
+        if (doeasBinaryDataWork()) {
+            assertTrue(theSame);
+        } else {
+            assertFalse(theSame);
+        }
+    }
+
+    /**
+     * Until binary payload is not supported, in native binary test will fail.
+     * Needs https://issues.apache.org/jira/browse/CAMEL-15679
+     */
+    boolean doeasBinaryDataWork() {
+        return true;
+    }
+
+    private Map<String, List<Map<String, Object>>> testAggregate(String path, List<String> messages) {
+        return testAggregate(path, messages, null);
+    }
+
+    private Map<String, List<Map<String, Object>>> testAggregate(String path, List<String> messages, String mocks) {
+        RequestSpecification rs = RestAssured.given()
+                .queryParam("path", path);
+
+        if (mocks != null) {
+            rs = rs.queryParam("mocks", mocks);
+        }
+
+        return (Map<String, List<Map<String, Object>>>) rs.contentType(ContentType.JSON)
+                .body(messages)
+                .post("/leveldb/aggregate")
+                .then()
+                .statusCode(201)
+                .extract().as(Map.class);
+    }
+
+    @AfterAll
+    public static void afterAll() throws Exception {
+        File data = new File(LeveldbRouteBuilder.DATA_FOLDER);
+        FileUtils.deleteDirectory(data);
+    }
+
+    private byte[] readQuarkusFile(String fileName) throws Exception {
+        try (InputStream is = getClass().getClassLoader().getResourceAsStream(fileName)) {
+            return readBytes(is);
+        }
+    }
+
+    static byte[] readBytes(InputStream is) throws Exception {
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        byte[] buffer = new byte[4096];
+        int len;
+        while ((len = is.read(buffer)) != -1) {
+            os.write(buffer, 0, len);
+        }
+        return os.toByteArray();
+    }
+
+}
