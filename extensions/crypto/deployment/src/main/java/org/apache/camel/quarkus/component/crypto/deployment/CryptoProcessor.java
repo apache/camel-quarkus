bdb7d1888bd1b56df819ab78028bcb0d08e94237
James Netherton <jamesnetherton@gmail.com>
2020-10-21 13:17:20 +0100

    Remove DigitalSignatureConstants reflection registration
    Fixes #1881

--- extensions/crypto/deployment/src/main/java/org/apache/camel/quarkus/component/crypto/deployment/CryptoProcessor.java
+++ extensions/crypto/deployment/src/main/java/org/apache/camel/quarkus/component/crypto/deployment/CryptoProcessor.java
@@ -19,8 +19,6 @@ package org.apache.camel.quarkus.component.crypto.deployment;
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.builditem.ExtensionSslNativeSupportBuildItem;
 import io.quarkus.deployment.builditem.FeatureBuildItem;
-import io.quarkus.deployment.builditem.nativeimage.ReflectiveClassBuildItem;
-import org.apache.camel.component.crypto.DigitalSignatureConstants;
 
 class CryptoProcessor {
 
@@ -34,12 +32,5 @@ class CryptoProcessor {
     @BuildStep
     ExtensionSslNativeSupportBuildItem activeNativeSSLSupport() {
         return new ExtensionSslNativeSupportBuildItem(FEATURE);
-    }
-
-    @BuildStep
-    ReflectiveClassBuildItem registerForReflection() {
-        // TODO: Remove this when upgrading to Camel >= 3.6.0
-        // https://github.com/apache/camel-quarkus/issues/1881
-        return new ReflectiveClassBuildItem(false, true, DigitalSignatureConstants.class);
     }
 }
