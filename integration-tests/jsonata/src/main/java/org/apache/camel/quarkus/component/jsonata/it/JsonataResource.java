9b75ac697dbb802b613ef8d87835d8f1d1a4b52b
Alexandre Gallice <aldettinger@gmail.com>
2020-12-21 14:18:10 +0100

    Added JSONata extension (#2088)
    * Added JSONata extension
    
    * Changed camel-jsonata spec file extension as it was not valid JSON
    
    * Fixed validate issue

---
+++ integration-tests/jsonata/src/main/java/org/apache/camel/quarkus/component/jsonata/it/JsonataResource.java
@@ -0,0 +1,47 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.quarkus.component.jsonata.it;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.apache.camel.ProducerTemplate;
+import org.jboss.logging.Logger;
+
+@Path("/jsonata")
+@ApplicationScoped
+public class JsonataResource {
+
+    private static final Logger LOG = Logger.getLogger(JsonataResource.class);
+
+    @Inject
+    ProducerTemplate producerTemplate;
+
+    @Path("/transform")
+    @GET
+    @Consumes(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.TEXT_PLAIN)
+    public String transform(String content) {
+        LOG.debugf("Calling transform(%s)", content);
+        return producerTemplate.requestBody("direct:start", content, String.class);
+    }
+}
