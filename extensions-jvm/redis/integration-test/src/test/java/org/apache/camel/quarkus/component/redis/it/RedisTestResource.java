3223e351c7c2cf2584597ce8f07e8abb2bbb6a62
aldettinger <aldettinger@gmail.com>
2021-01-06 16:21:50 +0100

    Added Redis Aggregation Repository support in JVM mode only #2085

---
+++ extensions-jvm/redis/integration-test/src/test/java/org/apache/camel/quarkus/component/redis/it/RedisTestResource.java
@@ -0,0 +1,62 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.camel.quarkus.component.redis.it;
+
+import java.util.Map;
+
+import org.apache.camel.quarkus.testcontainers.ContainerResourceLifecycleManager;
+import org.apache.camel.util.CollectionHelper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.utility.TestcontainersConfiguration;
+
+public class RedisTestResource implements ContainerResourceLifecycleManager {
+    private static final Logger LOG = LoggerFactory.getLogger(RedisTestResource.class);
+    private static final int REDIS_PORT = 6379;
+    private static final String REDIS_IMAGE = "redis:6.0.9";
+
+    private GenericContainer container;
+
+    @Override
+    public Map<String, String> start() {
+        LOG.info(TestcontainersConfiguration.getInstance().toString());
+
+        try {
+            container = new GenericContainer(REDIS_IMAGE).withExposedPorts(REDIS_PORT).waitingFor(Wait.forListeningPort());
+            container.start();
+
+            final String authority = container.getContainerIpAddress() + ":" + container.getMappedPort(REDIS_PORT).toString();
+            return CollectionHelper.mapOf("camel.redis.test.server.authority", authority);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void stop() {
+        try {
+            if (container != null) {
+                container.stop();
+            }
+        } catch (Exception e) {
+            // ignored
+        }
+    }
+}
