b97fc8dd682cb7286277e8f01c5ebb9d3f8fc46a
aldettinger <aldettinger@gmail.com>
2021-01-28 10:58:59 +0100

    Added OAI-PMH support

---
+++ integration-tests/oaipmh/src/main/java/org/apache/camel/quarkus/component/oaipmh/it/OaipmhRoutes.java
@@ -0,0 +1,73 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.quarkus.component.oaipmh.it;
+
+import javax.enterprise.context.ApplicationScoped;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.support.builder.Namespaces;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+@ApplicationScoped
+public class OaipmhRoutes extends RouteBuilder {
+
+    @ConfigProperty(name = "cq-oaipmh-its.test1.server.authority")
+    String test1ServerAuthority;
+    @ConfigProperty(name = "cq-oaipmh-its.test2.server.authority")
+    String test2ServerAuthority;
+    @ConfigProperty(name = "cq-oaipmh-its.test3.server.authority")
+    String test3ServerAuthority;
+    @ConfigProperty(name = "cq-oaipmh-its.test4.server.authority")
+    String test4ServerAuthority;
+
+    public void configure() {
+
+        final Namespaces ns = new Namespaces();
+        ns.add("default", "http://www.openarchives.org/OAI/2.0/");
+        ns.add("oai_dc", "http://www.openarchives.org/OAI/2.0/oai_dc/");
+        ns.add("dc", "http://purl.org/dc/elements/1.1/");
+
+        final String listRecordsXpath = "/default:OAI-PMH/default:ListRecords/default:record/default:metadata/oai_dc:dc/dc:title/text()";
+        final String getRecordXpath = "/default:OAI-PMH/default:GetRecord/default:record/default:metadata/oai_dc:dc/dc:title/text()";
+
+        fromF("oaipmh://%s/oai/request?delay=1000&from=2020-06-01T00:00:00Z&initialDelay=1000", test1ServerAuthority)
+                .split(xpath(listRecordsXpath, ns))
+                .to("mock:consumerListRecords");
+
+        fromF("oaipmh://%s/index.php?page=oai&delay=1000&from=2020-02-01T00:00:00Z&initialDelay=1000", test3ServerAuthority)
+                .split(xpath(listRecordsXpath, ns))
+                .to("mock:consumerListRecordsParticularCase");
+
+        final String uriFormat = "oaipmh://%s/oai/request?ssl=true&ignoreSSLWarnings=true&delay=1000&verb=Identify&initialDelay=1000";
+        fromF(uriFormat, test4ServerAuthority)
+                .to("mock:consumerIdentifyHttps");
+
+        from("direct:producerListRecords")
+                .setHeader("CamelOaimphFrom", constant("2020-06-01T00:00:00Z"))
+                .toF("oaipmh://%s/oai/request", test1ServerAuthority)
+                .split(body())
+                .split(xpath(listRecordsXpath, ns))
+                .to("mock:producerListRecords");
+
+        from("direct:producerGetRecord")
+                .setHeader("CamelOaimphVerb", constant("GetRecord"))
+                .toF("oaipmh://%s/oai/request", test2ServerAuthority)
+                .split(body())
+                .split(xpath(getRecordXpath, ns))
+                .to("mock:producerGetRecord");
+    }
+}
---
+++ integration-tests/oaipmh/src/test/java/org/apache/camel/quarkus/component/oaipmh/it/OaipmhTest.java
@@ -0,0 +1,70 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.quarkus.component.oaipmh.it;
+
+import java.util.concurrent.TimeUnit;
+
+import io.quarkus.test.common.QuarkusTestResource;
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.given;
+import static org.awaitility.Awaitility.await;
+import static org.hamcrest.Matchers.is;
+
+@QuarkusTest
+@QuarkusTestResource(OaipmhTestResource.class)
+class OaipmhTest {
+
+    @Test
+    void consumerListRecordsShouldReturn532Records() {
+        await().atMost(10, TimeUnit.SECONDS).until(() -> {
+            String[] records = get("/oaipmh/consumerListRecords").then().statusCode(200).extract().as(String[].class);
+            return records.length == 532;
+        });
+    }
+
+    @Test
+    void consumerListRecordsParticularCaseShouldReturn45Records() {
+        await().atMost(5, TimeUnit.SECONDS).until(() -> {
+            String path = "/oaipmh/consumerListRecordsParticularCase";
+            String[] records = get(path).then().statusCode(200).extract().as(String[].class);
+            return records.length == 45;
+        });
+    }
+
+    @Test
+    void consumerIdentifyHttpsShouldReturnSingleRecord() {
+        await().atMost(5, TimeUnit.SECONDS).until(() -> {
+            String[] records = get("/oaipmh/consumerIdentifyHttps").then().statusCode(200).extract().as(String[].class);
+            return records.length == 1;
+        });
+    }
+
+    @Test
+    void producerListRecordsShouldReturn532Records() {
+        get("/oaipmh/producerListRecords").then().statusCode(200).body("size()", is(532));
+    }
+
+    @Test
+    void producerGetRecordShouldReturnSingleRecord() {
+        String id = "oai:dspace.ucuenca.edu.ec:123456789/32374";
+        given().body(id).get("/oaipmh/producerGetRecord").then().statusCode(200).body("size()", is(1));
+    }
+
+}
