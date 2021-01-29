e958f063a173825515d8f0c00cb4651feb7f35cb
James Netherton <jamesnetherton@gmail.com>
2021-01-29 09:54:34 +0000

    StAX native support
    Fixes #1696

---
+++ extensions/stax/deployment/src/main/java/org/apache/camel/quarkus/component/stax/deployment/StaxProcessor.java
@@ -0,0 +1,56 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.quarkus.component.stax.deployment;
+
+import org.xml.sax.ContentHandler;
+import org.xml.sax.helpers.DefaultHandler;
+
+import io.quarkus.deployment.annotations.BuildStep;
+import io.quarkus.deployment.builditem.AdditionalIndexedClassesBuildItem;
+import io.quarkus.deployment.builditem.CombinedIndexBuildItem;
+import io.quarkus.deployment.builditem.FeatureBuildItem;
+import io.quarkus.deployment.builditem.nativeimage.ReflectiveClassBuildItem;
+import org.jboss.jandex.ClassInfo;
+import org.jboss.jandex.DotName;
+import org.jboss.jandex.IndexView;
+
+class StaxProcessor {
+
+    private static final String FEATURE = "camel-stax";
+
+    @BuildStep
+    FeatureBuildItem feature() {
+        return new FeatureBuildItem(FEATURE);
+    }
+
+    @BuildStep
+    AdditionalIndexedClassesBuildItem contributeClassesToIndex() {
+        return new AdditionalIndexedClassesBuildItem(DefaultHandler.class.getName());
+    }
+
+    @BuildStep
+    ReflectiveClassBuildItem registerForReflection(CombinedIndexBuildItem combinedIndex) {
+        // Register any ContentHandler impls for reflection so they can be used by the sax component
+        IndexView index = combinedIndex.getIndex();
+        String[] contentHandlers = index.getAllKnownImplementors(DotName.createSimple(ContentHandler.class.getName()))
+                .stream()
+                .map(ClassInfo::name)
+                .map(DotName::toString)
+                .toArray(String[]::new);
+        return new ReflectiveClassBuildItem(false, false, contentHandlers);
+    }
+}
---
+++ integration-tests/stax/src/main/java/org/apache/camel/quarkus/component/stax/it/StaxResource.java
@@ -0,0 +1,84 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.quarkus.component.stax.it;
+
+import java.io.InputStream;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.quarkus.component.stax.it.model.Record;
+
+@Path("/stax")
+public class StaxResource {
+
+    @Inject
+    CamelContext context;
+
+    @Inject
+    ProducerTemplate producerTemplate;
+
+    @Path("/records")
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public int staxProcessRecords() throws Exception {
+        try (InputStream resource = StaxResource.class.getResourceAsStream("/records.xml")) {
+            TagCountHandler countingHandler = producerTemplate.requestBody("direct:processRecords", resource,
+                    TagCountHandler.class);
+            return countingHandler.getCount();
+        }
+    }
+
+    @Path("/records/byref")
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public int staxProcessRecordsByRef() throws Exception {
+        try (InputStream resource = StaxResource.class.getResourceAsStream("/records.xml")) {
+            TagCountHandler countingHandler = producerTemplate.requestBody("direct:processRecordsByRef", resource,
+                    TagCountHandler.class);
+            return countingHandler.getCount();
+        }
+    }
+
+    @Path("/records/split")
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public void staxSplitRecords() throws Exception {
+        try (InputStream resource = StaxResource.class.getResourceAsStream("/records.xml")) {
+            MockEndpoint mockEndpoint = context.getEndpoint("mock:split", MockEndpoint.class);
+            mockEndpoint.expectedMessageCount(10);
+            mockEndpoint.allMessages().body().isInstanceOf(Record.class);
+
+            producerTemplate.sendBody("direct:splitRecords", resource);
+
+            mockEndpoint.assertIsSatisfied();
+        }
+    }
+
+    @javax.enterprise.inject.Produces
+    @Named("countingHandler")
+    public TagCountHandler countingHandler() {
+        return new TagCountHandler();
+    }
+}
