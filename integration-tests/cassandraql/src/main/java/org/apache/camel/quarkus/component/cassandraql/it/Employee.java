9ac1dd877a6b0da97049b780cba255209a940f3f
JiriOndrusek <ondrusek.jiri@gmail.com>
2021-02-09 09:25:08 +0100

    CassandraQL native support #1355

---
+++ integration-tests/cassandraql/src/main/java/org/apache/camel/quarkus/component/cassandraql/it/Employee.java
@@ -0,0 +1,81 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.quarkus.component.cassandraql.it;
+
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.List;
+
+public class Employee implements Serializable, Cloneable {
+
+    private int id;
+
+    private String name;
+
+    private String address;
+
+    public Employee() {
+    }
+
+    public Employee(int empId, String name, String address) {
+        this.id = empId;
+        this.name = name;
+        this.address = address;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "empId=" + id +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+
+    public Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    List getValue() {
+        return Arrays.asList(id, name, address);
+    }
+
+}
