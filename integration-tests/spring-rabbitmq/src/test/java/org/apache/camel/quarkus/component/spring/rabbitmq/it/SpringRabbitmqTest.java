cee922120d5b690a6f910c987d4536773e67ab32
JiriOndrusek <ondrusek.jiri@gmail.com>
2021-02-10 15:04:56 +0100

    camel-spring-rabbitmq - new component #2128

---
+++ integration-tests/spring-rabbitmq/src/test/java/org/apache/camel/quarkus/component/spring/rabbitmq/it/SpringRabbitmqTest.java
@@ -0,0 +1,112 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.quarkus.component.spring.rabbitmq.it;
+
+import io.quarkus.test.common.QuarkusTestResource;
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+
+import static org.hamcrest.Matchers.is;
+
+@QuarkusTest
+@QuarkusTestResource(SpringRabbitmqTestResource.class)
+class SpringRabbitmqTest {
+
+    private final static String EXCHANGE_POLLING = "polling";
+    private final static String ROUTING_KEY_POLLING = "pollingKey";
+    private ConnectionFactory connectionFactory;
+
+    @Test
+    public void testInOut() {
+        //direct has to be empty
+        getFromDirect(SpringRabbitmqResource.DIRECT_IN_OUT)
+                .then()
+                .statusCode(204);
+
+        sendToExchange(SpringRabbitmqResource.EXCHANGE_IN_OUT, SpringRabbitmqResource.ROUTING_KEY_IN_OUT, "Sheldon");
+
+        getFromDirect(SpringRabbitmqResource.DIRECT_IN_OUT)
+                .then()
+                .statusCode(200)
+                .body(is("Hello Sheldon"));
+
+    }
+
+    @Test
+    public void testPolling() throws InterruptedException {
+
+        bindQueue(SpringRabbitmqResource.POLLING_QUEUE_NAME, EXCHANGE_POLLING, ROUTING_KEY_POLLING);
+
+        //start thread with the poling consumer from exchange "polling", polling queue, routing "pollingKey", result is sent to polling direct
+        RestAssured.given()
+                .queryParam(SpringRabbitmqResource.QUERY_EXCHANGE, EXCHANGE_POLLING)
+                .queryParam(SpringRabbitmqResource.QUERY_ROUTING_KEY, ROUTING_KEY_POLLING)
+                .post("/spring-rabbitmq/startPolling");
+
+        // wait a little to demonstrate we can start poll before we have a msg on the queue
+        Thread.sleep(500);
+
+        sendToExchange(EXCHANGE_POLLING, ROUTING_KEY_POLLING, "Sheldon");
+
+        //get result from direct (for pooling) with timeout
+        getFromDirect(SpringRabbitmqResource.DIRECT_POLLING, 1000)
+                .then()
+                .statusCode(200)
+                .body(is("Polling Hello Sheldon"));
+
+    }
+
+    private void sendToExchange(String exchange, String routingKey, String body) {
+        RestAssured.given()
+                .queryParam(SpringRabbitmqResource.QUERY_EXCHANGE, exchange)
+                .queryParam(SpringRabbitmqResource.QUERY_ROUTING_KEY, routingKey)
+                .body(body)
+                .post("/spring-rabbitmq/send");
+    }
+
+    private Response getFromDirect(String direct) {
+        return getFromDirect(direct, 0);
+    }
+
+    private Response getFromDirect(String direct, int timeout) {
+        return RestAssured.given()
+                .queryParam(SpringRabbitmqResource.QUERY_DIRECT, direct)
+                .queryParam(SpringRabbitmqResource.QUERY_TIMEOUT, timeout)
+                .post("/spring-rabbitmq/getFromDirect");
+    }
+
+    private void bindQueue(String queue, String exchange, String routingKey) {
+        Queue q = new Queue(queue, false);
+        DirectExchange t = new DirectExchange(exchange);
+        AmqpAdmin admin = new RabbitAdmin(connectionFactory);
+        admin.declareQueue(q);
+        admin.declareExchange(t);
+        admin.declareBinding(BindingBuilder.bind(q).to(t).with(routingKey));
+    }
+
+    public void setConnectionFactory(ConnectionFactory connectionFactory) {
+        this.connectionFactory = connectionFactory;
+    }
+}
---
+++ integration-tests/spring-rabbitmq/src/test/java/org/apache/camel/quarkus/component/spring/rabbitmq/it/SpringRabbitmqTestResource.java
@@ -0,0 +1,78 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.quarkus.component.spring.rabbitmq.it;
+
+import java.util.Map;
+
+import io.quarkus.test.common.QuarkusTestResourceLifecycleManager;
+import org.apache.camel.util.CollectionHelper;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.testcontainers.containers.RabbitMQContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+
+public class SpringRabbitmqTestResource implements QuarkusTestResourceLifecycleManager {
+
+    private static final String RABBITMQ_IMAGE = "rabbitmq:3.7.25-management-alpine";
+    private static final int RABBITMQ_PORT = 5672;
+    private static final String RABBITMQ_USERNAME = "guest";
+    private static final String RABBITMQ_PASSWORD = "guest";
+
+    private RabbitMQContainer container;
+
+    @Override
+    public Map<String, String> start() {
+
+        try {
+            container = new RabbitMQContainer(RABBITMQ_IMAGE)
+                    .withExposedPorts(RABBITMQ_PORT)
+                    .waitingFor(Wait.forListeningPort());
+            container.start();
+
+            return CollectionHelper.mapOf(
+                    SpringRabbitmqResource.PARAMETER_PORT, container.getMappedPort(RABBITMQ_PORT).toString(),
+                    SpringRabbitmqResource.PARAMETER_HOSTNAME, container.getHost(),
+                    SpringRabbitmqResource.PARAMETER_USERNAME, RABBITMQ_USERNAME,
+                    SpringRabbitmqResource.PARAMETER_PASSWORD, RABBITMQ_PASSWORD);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void inject(Object testInstance) {
+        if (testInstance instanceof SpringRabbitmqTest) {
+            SpringRabbitmqTest test = (SpringRabbitmqTest) testInstance;
+
+            CachingConnectionFactory cf = new CachingConnectionFactory();
+            cf.setUri(String.format("amqp://%s:%d", container.getHost(), container.getMappedPort(RABBITMQ_PORT)));
+            cf.setUsername(RABBITMQ_USERNAME);
+            cf.setPassword(RABBITMQ_PASSWORD);
+
+            test.setConnectionFactory(cf);
+        }
+    }
+
+    @Override
+    public void stop() {
+        try {
+            if (container != null) {
+                container.stop();
+            }
+        } catch (Exception e) {
+            // Ignored
+        }
+    }
+}
