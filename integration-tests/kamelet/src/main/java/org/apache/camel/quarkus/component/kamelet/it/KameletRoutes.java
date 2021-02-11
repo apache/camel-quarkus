55aa43a6c0c929e3ba7a5e47507e9f0a816c76a4
James Netherton <jamesnetherton@gmail.com>
2021-02-11 11:41:36 +0000

    Add Kamelet component extension
    Fixes #2174

---
+++ integration-tests/kamelet/src/main/java/org/apache/camel/quarkus/component/kamelet/it/KameletRoutes.java
@@ -0,0 +1,51 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.quarkus.component.kamelet.it;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.builder.RouteBuilder;
+
+public class KameletRoutes extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+        routeTemplate("setBody")
+                .templateParameter("bodyValue")
+                .from("kamelet:source")
+                .setBody().constant("Hello {{bodyValue}}");
+
+        routeTemplate("setBodyFromProperties")
+                .templateParameter("bodyValueFromProperty")
+                .from("kamelet:source")
+                .setBody().constant("Hello {{bodyValueFromProperty}}");
+
+        routeTemplate("tick")
+                .from("timer:{{routeId}}?repeatCount=1&delay=-1")
+                .setBody().exchangeProperty(Exchange.TIMER_COUNTER)
+                .to("kamelet:sink");
+
+        routeTemplate("echo")
+                .templateParameter("prefix")
+                .templateParameter("suffix")
+                .from("kamelet:source")
+                .setBody().simple("{{prefix}} ${body} {{suffix}}");
+
+        from("direct:chain")
+                .to("kamelet:echo/1?prefix=Camel Quarkus&suffix=Chained")
+                .to("kamelet:echo/2?prefix=Hello&suffix=Route");
+    }
+}
---
+++ integration-tests/kamelet/src/test/java/org/apache/camel/quarkus/component/kamelet/it/KameletTest.java
@@ -0,0 +1,68 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.quarkus.component.kamelet.it;
+
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.Matchers.is;
+
+@QuarkusTest
+class KameletTest {
+
+    @Test
+    public void testKameletProducing() {
+        String message = "Camel Quarkus Kamelet";
+
+        RestAssured.given()
+                .contentType(ContentType.TEXT)
+                .body(message)
+                .post("/kamelet/produce")
+                .then()
+                .statusCode(200)
+                .body(is("Hello " + message));
+    }
+
+    @Test
+    public void testKameletConsuming() {
+        RestAssured.get("/kamelet/consume")
+                .then()
+                .statusCode(200)
+                .body(is("1"));
+    }
+
+    @Test
+    public void testKameletWithProperties() {
+        RestAssured.get("/kamelet/property")
+                .then()
+                .statusCode(200)
+                .body(is("Hello Camel Quarkus Kamelet Property"));
+    }
+
+    @Test
+    public void testKameletChain() {
+        RestAssured.given()
+                .contentType(ContentType.TEXT)
+                .body("Kamelet")
+                .post("/kamelet/chain")
+                .then()
+                .statusCode(200)
+                .body(is("Hello Camel Quarkus Kamelet Chained Route"));
+    }
+}
