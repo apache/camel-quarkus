c835e0395259b10c14577d783356e168abb84de4
James Netherton <jamesnetherton@gmail.com>
2021-02-22 08:01:13 +0000

    Upgrade to Camel 3.9.0

--- catalog/src/test/java/org/apache/camel/catalog/quarkus/QuarkusRuntimeProviderTest.java
+++ catalog/src/test/java/org/apache/camel/catalog/quarkus/QuarkusRuntimeProviderTest.java
@@ -60,7 +60,7 @@ public class QuarkusRuntimeProviderTest {
         assertNotNull(names);
         assertFalse(names.isEmpty());
 
-        assertTrue(names.contains("aws-eks"));
+        assertTrue(names.contains("aws2-eks"));
         assertTrue(names.contains("bean"));
         assertTrue(names.contains("direct"));
         assertTrue(names.contains("imap"));
