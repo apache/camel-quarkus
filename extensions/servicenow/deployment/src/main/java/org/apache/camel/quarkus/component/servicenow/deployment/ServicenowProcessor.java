1ae584df5f1eb881d1b1ed7a734a2b2b6fde97a1
James Netherton <jamesnetherton@gmail.com>
2021-03-05 14:47:08 +0000

    Use camel-servicenow-maven-plugin to generate model classes

--- extensions/servicenow/deployment/src/main/java/org/apache/camel/quarkus/component/servicenow/deployment/ServicenowProcessor.java
+++ extensions/servicenow/deployment/src/main/java/org/apache/camel/quarkus/component/servicenow/deployment/ServicenowProcessor.java
@@ -18,10 +18,18 @@ package org.apache.camel.quarkus.component.servicenow.deployment;
 
 import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
+import io.quarkus.deployment.builditem.CombinedIndexBuildItem;
 import io.quarkus.deployment.builditem.ExtensionSslNativeSupportBuildItem;
 import io.quarkus.deployment.builditem.FeatureBuildItem;
 import io.quarkus.deployment.builditem.nativeimage.ReflectiveClassBuildItem;
+import org.apache.camel.component.servicenow.ServiceNowExceptionModel;
+import org.apache.camel.component.servicenow.annotations.ServiceNowSysParm;
+import org.apache.camel.quarkus.core.deployment.util.CamelSupport;
 import org.apache.cxf.transport.http.HTTPTransportFactory;
+import org.jboss.jandex.AnnotationInstance;
+import org.jboss.jandex.AnnotationTarget;
+import org.jboss.jandex.DotName;
+import org.jboss.jandex.IndexView;
 
 class ServicenowProcessor {
 
@@ -38,7 +46,25 @@ class ServicenowProcessor {
     }
 
     @BuildStep
-    void registerForReflection(BuildProducer<ReflectiveClassBuildItem> reflectiveClass) {
-        reflectiveClass.produce(new ReflectiveClassBuildItem(false, true, HTTPTransportFactory.class.getCanonicalName()));
+    void registerForReflection(BuildProducer<ReflectiveClassBuildItem> reflectiveClass, CombinedIndexBuildItem combinedIndex) {
+        IndexView index = combinedIndex.getIndex();
+
+        // Get candidate DTOs annotated with ServiceNowSysParm
+        String[] serviceNowDtos = index.getAnnotations(DotName.createSimple(ServiceNowSysParm.class.getName()))
+                .stream()
+                .map(AnnotationInstance::target)
+                .filter(annotationTarget -> annotationTarget.kind().equals(AnnotationTarget.Kind.CLASS))
+                .map(AnnotationTarget::asClass)
+                .filter(CamelSupport::isConcrete)
+                .filter(CamelSupport::isPublic)
+                .map(classInfo -> classInfo.name().toString())
+                .filter(className -> !className.startsWith("org.apache.camel.component.servicenow.model"))
+                .toArray(String[]::new);
+
+        reflectiveClass.produce(new ReflectiveClassBuildItem(false, true, serviceNowDtos));
+
+        reflectiveClass.produce(new ReflectiveClassBuildItem(false, true,
+                HTTPTransportFactory.class,
+                ServiceNowExceptionModel.class));
     }
 }
