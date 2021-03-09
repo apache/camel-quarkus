5c8b1b50c6915e34a800fb5d2559672d9926a7ec
James Netherton <jamesnetherton@gmail.com>
2021-03-09 15:44:43 +0000

    HL7 Native support
    Fixes #1747

---
+++ integration-tests/hl7/src/main/java/org/apache/camel/quarkus/component/hl7/it/Hl7Resource.java
@@ -0,0 +1,156 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.quarkus.component.hl7.it;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import ca.uhn.hl7v2.model.v22.datatype.AD;
+import ca.uhn.hl7v2.model.v22.datatype.CK;
+import ca.uhn.hl7v2.model.v22.datatype.PN;
+import ca.uhn.hl7v2.model.v22.datatype.ST;
+import ca.uhn.hl7v2.model.v22.datatype.TN;
+import ca.uhn.hl7v2.model.v22.message.ADT_A01;
+import ca.uhn.hl7v2.model.v22.segment.PID;
+import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.component.mock.MockEndpoint;
+
+@Path("/hl7")
+@ApplicationScoped
+public class Hl7Resource {
+
+    @Inject
+    CamelContext context;
+
+    @Inject
+    ProducerTemplate producerTemplate;
+
+    @Path("/mllp")
+    @POST
+    @Consumes(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.APPLICATION_JSON)
+    public JsonObject mllp(String message) throws Exception {
+        MockEndpoint endpoint = context.getEndpoint("mock:result", MockEndpoint.class);
+        endpoint.expectedMessageCount(1);
+
+        producerTemplate.sendBody(
+                "netty:tcp://localhost:{{camel.hl7.test-tcp-port}}?sync=true&encoders=#hl7encoder&decoders=#hl7decoder",
+                message);
+
+        endpoint.assertIsSatisfied(5000L);
+        Exchange exchange = endpoint.getExchanges().get(0);
+        ADT_A01 result = exchange.getMessage().getBody(ADT_A01.class);
+
+        return adtToJsonObject(result);
+    }
+
+    @Path("/marshalUnmarshal")
+    @POST
+    @Consumes(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.TEXT_PLAIN)
+    public String marshalUnmarshal(String message) throws Exception {
+        return producerTemplate.requestBody("direct:marshalUnmarshal", message, String.class);
+    }
+
+    @Path("/validate")
+    @POST
+    @Consumes(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response validate(String message) throws Exception {
+        Exchange exchange = producerTemplate.request("direct:validate", e -> e.getMessage().setBody(message));
+        if (exchange.isFailed()) {
+            Exception exception = exchange.getException();
+            return Response.serverError().entity(exception.getMessage()).build();
+        }
+        return Response.ok().build();
+    }
+
+    @Path("/validate/custom")
+    @POST
+    @Consumes(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response validateCustom(String message) throws Exception {
+        Exchange exchange = producerTemplate.request("direct:validateCustom", e -> e.getMessage().setBody(message));
+        if (exchange.isFailed()) {
+            Exception exception = exchange.getException();
+            return Response.serverError().entity(exception.getMessage()).build();
+        }
+        return Response.ok().build();
+    }
+
+    @Path("/hl7terser")
+    @POST
+    @Consumes(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hl7terser(String message) throws Exception {
+        Exchange exchange = producerTemplate.request("direct:hl7terser", e -> e.getMessage().setBody(message));
+        return exchange.getMessage().getHeader("PATIENT_ID", String.class);
+    }
+
+    @Path("/hl7terser/bean")
+    @POST
+    @Consumes(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hl7terserBean(String message) throws Exception {
+        return producerTemplate.requestBody("direct:hl7terserBean", message, String.class);
+    }
+
+    @Path("/xml")
+    @POST
+    @Consumes(MediaType.APPLICATION_XML)
+    @Produces(MediaType.APPLICATION_JSON)
+    public JsonObject hl7Xml(String messageXml) throws Exception {
+        ADT_A01 result = producerTemplate.requestBody("direct:unmarshalXml", messageXml, ADT_A01.class);
+        return adtToJsonObject(result);
+    }
+
+    private JsonObject adtToJsonObject(ADT_A01 result) {
+        JsonObjectBuilder objectBuilder = Json.createObjectBuilder();
+
+        PID pid = result.getPID();
+        PN patientName = pid.getPatientName();
+        objectBuilder.add("first_name", patientName.getGivenName().getValue());
+        objectBuilder.add("last_name", patientName.getFamilyName().getValue());
+
+        ST birthPlace = pid.getBirthPlace();
+        objectBuilder.add("birth_place", birthPlace.getValue());
+
+        CK patientAccountNumber = pid.getPatientAccountNumber();
+        objectBuilder.add("account_number", patientAccountNumber.getIDNumber().getValue());
+
+        AD patientAddress = pid.getPatientAddress(0);
+        objectBuilder.add("street", patientAddress.getAd1_StreetAddress().getValue());
+        objectBuilder.add("city", patientAddress.getAd3_City().getValue());
+        objectBuilder.add("zip", patientAddress.getZipOrPostalCode().getValue());
+
+        TN phoneNumber = pid.getPhoneNumberHome(0);
+        objectBuilder.add("phone", phoneNumber.getValue());
+
+        return objectBuilder.build();
+    }
+}
---
+++ integration-tests/hl7/src/main/java/org/apache/camel/quarkus/component/hl7/it/Hl7Routes.java
@@ -0,0 +1,60 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.quarkus.component.hl7.it;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+import ca.uhn.hl7v2.model.v22.message.ADT_A01;
+import ca.uhn.hl7v2.parser.Parser;
+import org.apache.camel.builder.RouteBuilder;
+
+import static org.apache.camel.component.hl7.HL7.hl7terser;
+
+@ApplicationScoped
+public class Hl7Routes extends RouteBuilder {
+
+    @Inject
+    Parser parser;
+
+    @Override
+    public void configure() throws Exception {
+        from("netty:tcp://localhost:{{camel.hl7.test-tcp-port}}?sync=true&encoders=#hl7encoder&decoders=#hl7decoder")
+                .convertBodyTo(ADT_A01.class)
+                .to("mock:result");
+
+        from("direct:validate")
+                .unmarshal("hl7DataFormat");
+
+        from("direct:validateCustom")
+                .unmarshal().hl7(false)
+                .marshal().hl7(parser);
+
+        from("direct:marshalUnmarshal")
+                .unmarshal("hl7DataFormat")
+                .marshal("hl7DataFormat");
+
+        from("direct:hl7terser")
+                .setHeader("PATIENT_ID", hl7terser("PID-3-1"));
+
+        from("direct:hl7terserBean")
+                .bean("terserBean");
+
+        from("direct:unmarshalXml")
+                .unmarshal("hl7DataFormat");
+    }
+}
