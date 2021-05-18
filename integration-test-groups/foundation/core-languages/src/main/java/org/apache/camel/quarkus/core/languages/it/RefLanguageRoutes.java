14f77dc6c222a80870510b2098d56b1cc3b8deea
Peter Palaga <ppalaga@redhat.com>
2021-05-18 14:30:47 +0200

    Test the Ref language without dependency on camel-quarkus-ref #2613

---
+++ integration-test-groups/foundation/core-languages/src/main/java/org/apache/camel/quarkus/core/languages/it/RefLanguageRoutes.java
@@ -0,0 +1,50 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.quarkus.core.languages.it;
+
+import javax.enterprise.inject.Produces;
+import javax.inject.Named;
+import javax.inject.Singleton;
+
+import io.quarkus.arc.Unremovable;
+import org.apache.camel.Exchange;
+import org.apache.camel.Expression;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.support.ExpressionAdapter;
+
+public class RefLanguageRoutes extends RouteBuilder {
+
+    @Override
+    public void configure() {
+        from("direct:refLanguage")
+                .transform().ref("refLanguageExpression");
+    }
+
+    @Unremovable
+    @Singleton
+    @Named("refLanguageExpression")
+    @Produces
+    public Expression myExpression() {
+        return new ExpressionAdapter() {
+            @Override
+            public Object evaluate(Exchange exchange) {
+                return exchange.getMessage().getBody(String.class).toUpperCase();
+            }
+        };
+    }
+
+}
---
+++ integration-test-groups/foundation/core-languages/src/test/java/org/apache/camel/quarkus/core/languages/it/RefLanguageTest.java
@@ -0,0 +1,42 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.quarkus.core.languages.it;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+/**
+ * Note that a similar test is performed in {@code camel-quarkus-integration-test-ref}. The difference is that here we
+ * test without the {@code camel-quarkus-ref} dependency because the Ref language is brought through
+ * {@code camel-quarkus-core}.
+ */
+@QuarkusTest
+public class RefLanguageTest {
+    @Test
+    public void ref() {
+        given()
+                .body("My very foo")
+                .post("/core-languages/route/refLanguage/String")
+                .then()
+                .statusCode(200)
+                .body(is("MY VERY FOO"));
+    }
+
+}
