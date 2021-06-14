1eca8dae7adaef921349bdc1414a8d86a6c59a33
James Netherton <jamesnetherton@gmail.com>
2021-06-14 09:20:04 +0100

    Test setting MLLP default charset from system property for native mode
    Fixes #2554

--- integration-tests/mllp/src/test/java/org/apache/camel/quarkus/component/mllp/it/MllpTest.java
+++ integration-tests/mllp/src/test/java/org/apache/camel/quarkus/component/mllp/it/MllpTest.java
@@ -16,7 +16,6 @@
  */
 package org.apache.camel.quarkus.component.mllp.it;
 
-import io.quarkus.test.junit.DisabledOnNativeImage;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.Test;
@@ -56,7 +55,6 @@ class MllpTest {
     }
 
     @Test
-    @DisabledOnNativeImage("https://github.com/apache/camel-quarkus/issues/2554")
     public void testCharsetFromMsh18() {
         // Set up the message with a charset and some characters that it cannot deal with
         String messageWithCharset = HL7_MESSAGE.replace("NE||", "NE||ISO-8859-1").replace("INHOUSE", "ÏNHOUSE");
@@ -80,7 +78,6 @@ class MllpTest {
     }
 
     @Test
-    @DisabledOnNativeImage("https://github.com/apache/camel-quarkus/issues/2554")
     public void testDefaultCharsetFromSystemProperty() {
         RestAssured.get("/mllp/charset/default")
                 .then()
