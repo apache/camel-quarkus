acb3a59546e7bb329974f1264e1a66e91c956b45
aldettinger <aldettinger@gmail.com>
2021-07-29 10:11:59 +0200

    Fix foundation core faulttolerance itest package name

--- integration-test-groups/foundation/core-fault-tolerance/src/test/java/org/apache/camel/quarkus/core/faulttolerance/it/CoreFaultToleranceIT.java
+++ integration-test-groups/foundation/core-fault-tolerance/src/test/java/org/apache/camel/quarkus/core/faulttolerance/it/CoreFaultToleranceIT.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.camel.quarkus.core;
+package org.apache.camel.quarkus.core.faulttolerance.it;
 
 import io.quarkus.test.junit.NativeImageTest;
 
