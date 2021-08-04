bdb40460e4c43adfb2fa56784abc70079ed548cf
James Netherton <jamesnetherton@gmail.com>
2021-08-04 13:32:56 +0100

    Use shaded spring support dependencies in Spring RabbitMQ extension
    Fixes #2980

---
+++ extensions/spring-rabbitmq/runtime/src/main/java/org/springframework/aop/framework/DefaultAopProxyFactorySubstitutions.java
@@ -0,0 +1,30 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.aop.framework;
+
+import com.oracle.svm.core.annotate.Substitute;
+import com.oracle.svm.core.annotate.TargetClass;
+
+@TargetClass(DefaultAopProxyFactory.class)
+final class DefaultAopProxyFactorySubstitutions {
+
+    @Substitute
+    public AopProxy createAopProxy(AdvisedSupport config) throws AopConfigException {
+        // Cut out references to cglib
+        return new JdkDynamicAopProxy(config);
+    }
+}
