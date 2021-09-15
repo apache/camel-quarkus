425fdfa3374d55256f1b71ee6c1c7cc04ae8bf65
James Netherton <jamesnetherton@gmail.com>
2021-09-15 13:54:20 +0100

    Improve native support for org.apache.http.impl.client.BasicAuthCache
    Fixes #3079

--- extensions-support/httpclient/runtime/src/main/java/org/apache/camel/quarkus/support/httpclient/graalvm/BasicAuthCacheAlias.java
+++ extensions-support/httpclient/runtime/src/main/java/org/apache/camel/quarkus/support/httpclient/graalvm/BasicAuthCacheAlias.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.camel.quarkus.couchdb;
+package org.apache.camel.quarkus.support.httpclient.graalvm;
 
 import java.util.Map;
 
