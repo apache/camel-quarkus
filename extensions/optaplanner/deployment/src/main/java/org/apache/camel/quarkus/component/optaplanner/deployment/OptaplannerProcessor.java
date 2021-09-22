e9132f3f0a657bfaaa4ae798a6ac2fc2c4ebaadc
James Netherton <jamesnetherton@gmail.com>
2021-09-22 17:27:38 +0100

    Upgrade Quarkus to 2.4.0.CR1

--- extensions/optaplanner/deployment/src/main/java/org/apache/camel/quarkus/component/optaplanner/deployment/OptaplannerProcessor.java
+++ extensions/optaplanner/deployment/src/main/java/org/apache/camel/quarkus/component/optaplanner/deployment/OptaplannerProcessor.java
@@ -16,8 +16,10 @@
  */
 package org.apache.camel.quarkus.component.optaplanner.deployment;
 
+import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.builditem.FeatureBuildItem;
+import io.quarkus.deployment.builditem.nativeimage.RuntimeInitializedClassBuildItem;
 
 class OptaplannerProcessor {
 
@@ -26,5 +28,12 @@ class OptaplannerProcessor {
     @BuildStep
     FeatureBuildItem feature() {
         return new FeatureBuildItem(FEATURE);
+    }
+
+    @BuildStep
+    void runtimeInitializedClasses(BuildProducer<RuntimeInitializedClassBuildItem> runtimeInitializedClass) {
+        runtimeInitializedClass.produce(new RuntimeInitializedClassBuildItem("org.drools.core.util.Drools$XSTreamChekcer"));
+        runtimeInitializedClass
+                .produce(new RuntimeInitializedClassBuildItem("org.drools.compiler.kproject.models.KieModuleMarshaller"));
     }
 }
