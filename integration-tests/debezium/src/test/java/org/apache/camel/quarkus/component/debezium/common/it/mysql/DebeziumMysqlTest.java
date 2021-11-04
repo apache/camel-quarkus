64a18af14d43cd7ecd60ed6a80266370aba72cbb
JiriOndrusek <ondrusek.jiri@gmail.com>
2021-11-04 16:19:48 +0100

    DebeziumMongodbTest skipped tests #3213

--- integration-tests/debezium/src/test/java/org/apache/camel/quarkus/component/debezium/common/it/mysql/DebeziumMysqlTest.java
+++ integration-tests/debezium/src/test/java/org/apache/camel/quarkus/component/debezium/common/it/mysql/DebeziumMysqlTest.java
@@ -29,12 +29,12 @@ import org.eclipse.microprofile.config.Config;
 import org.eclipse.microprofile.config.ConfigProvider;
 import org.jboss.logging.Logger;
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
-import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
 
 @QuarkusTest
 @QuarkusTestResource(DebeziumMysqlTestResource.class)
@@ -42,8 +42,6 @@ import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
 class DebeziumMysqlTest extends AbstractDebeziumTest {
     private static final Logger LOG = Logger.getLogger(DebeziumMysqlTest.class);
 
-    //has to be constant and has to be equal to Type.mysql.getJdbcProperty
-    public static final String PROPERTY_JDBC = "mysql_jdbc";
     private static Connection connection;
 
     public DebeziumMysqlTest() {
@@ -53,13 +51,11 @@ class DebeziumMysqlTest extends AbstractDebeziumTest {
     @BeforeAll
     public static void setUp() throws SQLException {
         Config config = ConfigProvider.getConfig();
-        Optional<String> jdbcUrl = config.getOptionalValue(PROPERTY_JDBC, String.class);
+        Optional<String> jdbcUrl = config.getOptionalValue(Type.mysql.getPropertyJdbc(), String.class);
 
-        if (jdbcUrl.isPresent()) {
-            connection = DriverManager.getConnection(jdbcUrl.get());
-        } else {
-            LOG.warn("Container is not running. Connection is not created.");
-        }
+        Assumptions.assumeTrue(jdbcUrl.isPresent(), "Driver for mySql database is not provided. Container won't start.");
+
+        connection = DriverManager.getConnection(jdbcUrl.get());
     }
 
     @AfterAll
@@ -71,33 +67,11 @@ class DebeziumMysqlTest extends AbstractDebeziumTest {
 
     @Test
     @Order(0)
-    @EnabledIfSystemProperty(named = PROPERTY_JDBC, matches = ".*")
     public void testReceiveEmptyMessages() {
         //receive all empty messages before other tests
         receiveResponse("/receiveEmptyMessages")
                 .then()
                 .statusCode(204);
-    }
-
-    @Test
-    @Order(1)
-    @EnabledIfSystemProperty(named = PROPERTY_JDBC, matches = ".*")
-    public void testInsert() throws SQLException {
-        super.testInsert();
-    }
-
-    @Test
-    @Order(2)
-    @EnabledIfSystemProperty(named = PROPERTY_JDBC, matches = ".*")
-    public void testUpdate() throws SQLException {
-        super.testUpdate();
-    }
-
-    @Test
-    @Order(3)
-    @EnabledIfSystemProperty(named = PROPERTY_JDBC, matches = ".*")
-    public void testDelete() throws SQLException {
-        super.testDelete();
     }
 
     @Override
