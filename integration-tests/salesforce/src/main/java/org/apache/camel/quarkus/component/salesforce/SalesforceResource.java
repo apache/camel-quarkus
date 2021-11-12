c9d0ddbcbede56fbfda8ef605ed7b2d975238308
James Netherton <jamesnetherton@gmail.com>
2021-11-12 08:39:08 +0000

    Avoid using Camel Salesforce DTO types as the return type or method parameters in integration tests
    Fixes #3259

--- integration-tests/salesforce/src/main/java/org/apache/camel/quarkus/component/salesforce/SalesforceResource.java
+++ integration-tests/salesforce/src/main/java/org/apache/camel/quarkus/component/salesforce/SalesforceResource.java
@@ -17,12 +17,16 @@
 package org.apache.camel.quarkus.component.salesforce;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 import javax.inject.Inject;
 import javax.json.Json;
+import javax.json.JsonArrayBuilder;
 import javax.json.JsonObject;
 import javax.json.JsonObjectBuilder;
 import javax.ws.rs.Consumes;
@@ -41,9 +45,11 @@ import org.apache.camel.ConsumerTemplate;
 import org.apache.camel.Exchange;
 import org.apache.camel.FluentProducerTemplate;
 import org.apache.camel.component.salesforce.SalesforceEndpointConfig;
+import org.apache.camel.component.salesforce.api.dto.Attributes;
 import org.apache.camel.component.salesforce.api.dto.CreateSObjectResult;
 import org.apache.camel.component.salesforce.api.dto.GlobalObjects;
 import org.apache.camel.component.salesforce.api.dto.Limits;
+import org.apache.camel.component.salesforce.api.dto.RecentItem;
 import org.apache.camel.component.salesforce.api.dto.RestResources;
 import org.apache.camel.component.salesforce.api.dto.SObjectBasicInfo;
 import org.apache.camel.component.salesforce.api.dto.SObjectDescription;
@@ -82,28 +88,18 @@ public class SalesforceResource {
                 .request(String.class);
     }
 
-    @Path("/account")
+    @Path("/account/query/{id}")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Account getAccount() {
+    public JsonObject getAccountByQueryRecords(@PathParam("id") String accountId) {
+        String query = QueryHelper.queryToFetchFilteredFieldsOf(new Account(),
+                sObjectField -> sObjectField.getName().equals("Id") || sObjectField.getName().equals("AccountNumber"));
         QueryRecordsAccount request = template
-                .toF("salesforce:query?sObjectQuery=SELECT Id,AccountNumber from Account LIMIT 1&sObjectClass=%s",
+                .toF("salesforce:query?sObjectQuery=%s&sObjectClass=%s",
+                        query + " WHERE Id = '" + accountId + "'",
                         QueryRecordsAccount.class.getName())
                 .request(QueryRecordsAccount.class);
-        return request.getRecords().get(0);
-    }
-
-    @Path("/account/query")
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public Account getAccountByQueryHelper() {
-        String generatedQuery = QueryHelper.queryToFetchAllFieldsOf(new Account());
-        QueryRecordsAccount request = template
-                .toF("salesforce:query?sObjectQuery=%s LIMIT 1&sObjectClass=%s",
-                        generatedQuery,
-                        QueryRecordsAccount.class.getName())
-                .request(QueryRecordsAccount.class);
-        return request.getRecords().get(0);
+        return accountToJsonObject(request.getRecords().get(0));
     }
 
     @Path("/account")
@@ -113,6 +109,7 @@ public class SalesforceResource {
     public String createAccount(String accountName) {
         final Account account = new Account();
         account.setName(accountName);
+        account.setAccountNumber(UUID.randomUUID().toString());
 
         CreateSObjectResult result = template.to("salesforce:createSObject?sObjectName=Account")
                 .withBody(account)
@@ -137,12 +134,12 @@ public class SalesforceResource {
     @Path("/account/{id}")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Account getAccountById(@PathParam("id") String accountId) {
+    public JsonObject getAccountById(@PathParam("id") String accountId) {
         Account account = template
                 .to("salesforce:getSObjectWithId?sObjectName=Account&sObjectIdName=Id&sObjectClass=" + Account.class.getName())
                 .withBody(accountId)
                 .request(Account.class);
-        return account;
+        return accountToJsonObject(account);
     }
 
     @Path("/bulk")
@@ -184,71 +181,71 @@ public class SalesforceResource {
     @Path("/cdc")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
+    @SuppressWarnings("unchecked")
     public Map<String, Object> getCdcEvents() {
-        Map map = consumerTemplate.receiveBody("seda:events", 10000, Map.class);
-        return map;
-    }
-
-    private JobInfo createJobInfo() {
-        JobInfo jobInfo = new JobInfo();
-        jobInfo.setObject(Account.class.getSimpleName());
-        jobInfo.setContentType(ContentType.CSV);
-        return jobInfo;
-    }
-
-    private JsonObject jobInfoToJsonObject(JobInfo jobInfo) {
-        JsonObjectBuilder objectBuilder = Json.createObjectBuilder();
-        objectBuilder.add("id", jobInfo.getId());
-        objectBuilder.add("state", jobInfo.getState().value().toUpperCase(Locale.US));
-        return objectBuilder.build();
+        return consumerTemplate.receiveBody("seda:events", 10000, Map.class);
     }
 
     @Path("sobjects/force-limit")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public GlobalObjectsAndHeaders getSObjectsWithForceLimitInfo() {
+    public String getSObjectsWithForceLimitInfo() {
         // Testing producer with headers
         Exchange exchange = template.to("salesforce:getGlobalObjects")
                 .withHeader("Sforce-Limit-Info", Collections.singletonList("api-usage")).send();
         GlobalObjectsAndHeaders objectsAndHeaders = new GlobalObjectsAndHeaders(
                 exchange.getMessage().getBody(GlobalObjects.class))
                         .withHeader("Sforce-Limit-Info", exchange.getMessage().getHeader("Sforce-Limit-Info", String.class));
-        return objectsAndHeaders;
+        return objectsAndHeaders.getHeader("Sforce-Limit-Info");
     }
 
     @Path("versions")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public List<Version> getListVersions() {
-        return template.to("salesforce:getVersions").request(List.class);
+    @SuppressWarnings("unchecked")
+    public List<String> getListVersions() {
+        List<Version> versions = template.to("salesforce:getVersions").request(List.class);
+        return versions.stream()
+                .map(Version::getVersion)
+                .collect(Collectors.toList());
     }
 
     @Path("resources")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public RestResources getListResources() {
-        return template.to("salesforce:getResources").request(RestResources.class);
+    public String getListResources() {
+        RestResources restResources = template.to("salesforce:getResources").request(RestResources.class);
+        return restResources.getSobjects();
     }
 
     @Path("basic-info/account")
     @GET
-    public SObjectBasicInfo getAccountBasicInfo() {
-        return template.to("salesforce:getBasicInfo?sObjectName=Account")
-                .request(SObjectBasicInfo.class);
+    public JsonObject getAccountBasicInfo() {
+        SObjectBasicInfo basicInfo = template.to("salesforce:getBasicInfo?sObjectName=Account").request(SObjectBasicInfo.class);
+        return basicInfoToJsonObject(basicInfo);
     }
 
     @Path("describe/account")
     @GET
-    public SObjectDescription getAccountDescription() {
-        return template.to("salesforce:getDescription?sObjectName=Account")
+    public String getAccountDescription() {
+        SObjectDescription sObjectDescription = template.to("salesforce:getDescription?sObjectName=Account")
                 .request(SObjectDescription.class);
+        return sObjectDescription.getName();
     }
 
     @Path("limits")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Limits getLimits() {
-        return template.to("salesforce:limits").request(Limits.class);
+    public Map<String, Integer> getLimits() {
+        Limits limits = template.to("salesforce:limits").request(Limits.class);
+
+        Map<String, Integer> limitInfo = new HashMap<>();
+        for (Limits.Operation operation : Limits.Operation.values()) {
+            Limits.Usage usage = limits.forOperation(operation.name());
+            limitInfo.put(operation.name(), usage.getRemaining());
+        }
+
+        return limitInfo;
     }
 
     @Path("streaming")
@@ -295,5 +292,49 @@ public class SalesforceResource {
     @Produces(MediaType.APPLICATION_JSON)
     public String getPlatformEvent() {
         return consumerTemplate.receiveBody("salesforce:event/TestEvent__e?rawPayload=true", 10000, String.class);
+    }
+
+    private JsonObject accountToJsonObject(Account account) {
+        JsonObjectBuilder builder = Json.createObjectBuilder();
+        builder.add("Id", account.getId());
+        builder.add("AccountNumber", account.getAccountNumber());
+        return builder.build();
+    }
+
+    private JobInfo createJobInfo() {
+        JobInfo jobInfo = new JobInfo();
+        jobInfo.setObject(Account.class.getSimpleName());
+        jobInfo.setContentType(ContentType.CSV);
+        return jobInfo;
+    }
+
+    private JsonObject jobInfoToJsonObject(JobInfo jobInfo) {
+        JsonObjectBuilder objectBuilder = Json.createObjectBuilder();
+        objectBuilder.add("id", jobInfo.getId());
+        objectBuilder.add("state", jobInfo.getState().value().toUpperCase(Locale.US));
+        return objectBuilder.build();
+    }
+
+    private JsonObject basicInfoToJsonObject(SObjectBasicInfo basicInfo) {
+        JsonObjectBuilder builder = Json.createObjectBuilder();
+
+        List<RecentItem> recentItems = basicInfo.getRecentItems();
+        JsonArrayBuilder recentItemsArrayBuilder = Json.createArrayBuilder();
+        for (RecentItem recentItem : recentItems) {
+            JsonObjectBuilder recentItemBuilder = Json.createObjectBuilder();
+            recentItemBuilder.add("Id", recentItem.getId());
+            recentItemBuilder.add("Name", recentItem.getName());
+
+            Attributes attributes = recentItem.getAttributes();
+            JsonObjectBuilder attributesObjectBuilder = Json.createObjectBuilder();
+            attributesObjectBuilder.add("url", attributes.getUrl());
+            recentItemBuilder.add("attributes", attributesObjectBuilder);
+
+            recentItemsArrayBuilder.add(recentItemBuilder);
+        }
+
+        builder.add("recentItems", recentItemsArrayBuilder);
+
+        return builder.build();
     }
 }
