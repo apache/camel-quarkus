1a7f50be8f1cd30425d64589cf3f7c5c5cb2bb14
Zineb Bendhiba <bendhiba.zineb@gmail.com>
2021-11-13 20:23:48 +0100

    Enable Atlasmap tests and fix loading URL resources in native mode fixes #3189

--- extensions/atlasmap/runtime/src/main/java/org/apache/camel/quarkus/component/atlasmap/graalvm/Substitutions.java
+++ extensions/atlasmap/runtime/src/main/java/org/apache/camel/quarkus/component/atlasmap/graalvm/Substitutions.java
@@ -16,22 +16,54 @@
  */
 package org.apache.camel.quarkus.component.atlasmap.graalvm;
 
+import java.net.URL;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Set;
 
 import com.oracle.svm.core.annotate.Alias;
+import com.oracle.svm.core.annotate.RecomputeFieldValue;
 import com.oracle.svm.core.annotate.Substitute;
 import com.oracle.svm.core.annotate.TargetClass;
+import org.slf4j.Logger;
+
+import static com.oracle.svm.core.annotate.RecomputeFieldValue.Kind.Reset;
 
 public class Substitutions {
 }
 
 @TargetClass(className = "io.atlasmap.core.DefaultAtlasCompoundClassLoader")
 final class DefaultAtlasCompoundClassLoader_Substitute {
+
+    @Alias
+    @RecomputeFieldValue(kind = Reset)
+    private static Logger LOG = null;
+
     @Alias
     private Set<ClassLoader> delegates;
+
+    @Substitute
+    public Enumeration<URL> getResources(String name) {
+        List<URL> answer = new LinkedList<>();
+        for (ClassLoader cl : delegates) {
+            try {
+                Enumeration<URL> urls = cl.getResources(name);
+                while (urls != null && urls.hasMoreElements()) {
+                    answer.add(urls.nextElement());
+                }
+            } catch (Exception e) {
+                LOG.debug("I/O error while looking for a resource '{}' with ClassLoader '{}': {}", name, cl);
+                LOG.debug(e.getMessage(), e);
+            }
+        }
+        return Collections.enumeration(answer);
+    }
 
     @Substitute
     public synchronized void addAlternativeLoader(ClassLoader cl) {
         delegates.add(Thread.currentThread().getContextClassLoader());
     }
+
 }
