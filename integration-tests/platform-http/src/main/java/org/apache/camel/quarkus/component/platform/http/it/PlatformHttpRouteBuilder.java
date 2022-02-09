93b12459af5a5e89af5f7806001489a9c35ee918
James Netherton <jamesnetherton@gmail.com>
2022-02-09 11:13:37 +0000

    Upgrade Camel to 3.16.0

--- integration-tests/platform-http/src/main/java/org/apache/camel/quarkus/component/platform/http/it/PlatformHttpRouteBuilder.java
+++ integration-tests/platform-http/src/main/java/org/apache/camel/quarkus/component/platform/http/it/PlatformHttpRouteBuilder.java
@@ -46,14 +46,16 @@ public class PlatformHttpRouteBuilder extends RouteBuilder {
 
         rest()
                 .get("/platform-http/rest-get")
-                .route()
-                .setBody(constant("GET: /rest-get"))
-                .endRest()
+                .to("direct:echoMethodPath")
                 .post("/platform-http/rest-post")
                 .consumes("text/plain").produces("text/plain")
-                .route()
-                .setBody(constant("POST: /rest-post"))
-                .endRest();
+                .to("direct:echoMethodPath");
+
+        from("direct:echoMethodPath")
+                .setBody().simple("${header.CamelHttpMethod}: ${header.CamelHttpPath}");
+
+        from("direct:greet")
+                .setBody().simple("Hello ${header.name}");
 
         from("platform-http:/platform-http/hello?httpMethodRestrict=GET").setBody(simple("Hello ${header.name}"));
         from("platform-http:/platform-http/get-post?httpMethodRestrict=GET,POST").setBody(simple("Hello ${body}"));
@@ -122,9 +124,7 @@ public class PlatformHttpRouteBuilder extends RouteBuilder {
         rest()
                 .get("/platform-http/hello-by-name/{name}")
                 .produces("text/plain")
-                .route()
-                .setBody(e -> "Hello " + e.getIn().getHeader("name", String.class))
-                .endRest();
+                .to("direct:greet");
 
         // Webhook tests
         from("platform-http:/platform-http/webhookpath")
