d9a9a22d1a4c4abaaea799cb1c9d48f0d94946f8
James Netherton <jamesnetherton@gmail.com>
2022-02-11 14:09:57 +0000

    Poll for optaplanner results to avoid timeouts on slow machines

--- integration-tests/optaplanner/src/main/java/org/apache/camel/quarkus/component/optaplanner/it/OptaplannerResource.java
+++ integration-tests/optaplanner/src/main/java/org/apache/camel/quarkus/component/optaplanner/it/OptaplannerResource.java
@@ -16,23 +16,30 @@
  */
 package org.apache.camel.quarkus.component.optaplanner.it;
 
-import java.util.concurrent.CompletableFuture;
+import java.util.Optional;
 import java.util.concurrent.ExecutionException;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
 import org.apache.camel.ProducerTemplate;
 import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.component.optaplanner.OptaPlannerConstants;
 import org.apache.camel.quarkus.component.optaplanner.it.bootstrap.DataGenerator;
+import org.apache.camel.quarkus.component.optaplanner.it.domain.Room;
 import org.apache.camel.quarkus.component.optaplanner.it.domain.TimeTable;
-import org.jboss.logging.Logger;
+import org.apache.camel.quarkus.component.optaplanner.it.domain.Timeslot;
 import org.optaplanner.core.api.solver.SolverManager;
 import org.optaplanner.core.api.solver.SolverStatus;
 
@@ -40,8 +47,6 @@ import org.optaplanner.core.api.solver.SolverStatus;
 @ApplicationScoped
 @Produces(MediaType.APPLICATION_JSON)
 public class OptaplannerResource {
-
-    private static final Logger LOG = Logger.getLogger(OptaplannerResource.class);
 
     public static final Long SINGLETON_TIME_TABLE_ID = 1L;
 
@@ -54,38 +59,68 @@ public class OptaplannerResource {
     @Inject
     CamelContext context;
 
-    @GET
+    @POST
     @Path("solveSync")
-    public TimeTable solveSync() {
+    public void solveSync() {
         if (SolverStatus.NOT_SOLVING == solverManager.getSolverStatus(SINGLETON_TIME_TABLE_ID)) {
-            TimeTable finalSolution = producerTemplate.requestBodyAndHeader(
-                    "direct:solveSync", DataGenerator.timeTable,
-                    OptaPlannerConstants.SOLVER_MANAGER, solverManager, TimeTable.class);
-            return finalSolution;
+            producerTemplate.sendBodyAndHeader("direct:solveSync", DataGenerator.timeTable, OptaPlannerConstants.SOLVER_MANAGER,
+                    solverManager);
         }
-        return DataGenerator.timeTable;
     }
 
-    @GET
+    @POST
     @Path("solveAsync")
-    public TimeTable solveAsync() throws ExecutionException, InterruptedException {
+    public void solveAsync() throws ExecutionException, InterruptedException {
         if (SolverStatus.NOT_SOLVING == solverManager.getSolverStatus(SINGLETON_TIME_TABLE_ID)) {
-            CompletableFuture<TimeTable> response = producerTemplate.asyncRequestBodyAndHeader(
-                    "direct:solveAsync", DataGenerator.timeTable, OptaPlannerConstants.SOLVER_MANAGER,
-                    solverManager, TimeTable.class);
-
-            TimeTable finalSolution = response.get();
-            return finalSolution;
+            producerTemplate.sendBodyAndHeader("direct:solveAsync", DataGenerator.timeTable,
+                    OptaPlannerConstants.SOLVER_MANAGER, solverManager);
         }
-        return DataGenerator.timeTable;
+    }
+
+    @POST
+    @Path("consumer/{enable}")
+    public void mangeOptaplannerConsumer(@PathParam("enable") boolean enable) throws Exception {
+        if (enable) {
+            context.getRouteController().startRoute("optaplanner-consumer");
+        } else {
+            context.getRouteController().stopRoute("optaplanner-consumer");
+        }
     }
 
     @GET
-    @Path("newBestSolution")
-    public TimeTable getNewBestSolution() {
-        MockEndpoint mockEndpoint = context.getEndpoint("mock:best-solution", MockEndpoint.class);
-        return mockEndpoint.getReceivedExchanges().stream().map(
-                exchange -> exchange.getMessage().getHeader(OptaPlannerConstants.BEST_SOLUTION, TimeTable.class))
-                .findFirst().orElse(null);
+    @Path("solution/{mockEndpointUri}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public JsonObject getSolution(@PathParam("mockEndpointUri") String mockEndpointUri) {
+        MockEndpoint mockEndpoint = context.getEndpoint("mock:" + mockEndpointUri, MockEndpoint.class);
+        Optional<JsonObject> result = mockEndpoint.getReceivedExchanges()
+                .stream()
+                .map(Exchange::getMessage)
+                .map(message -> {
+                    if (mockEndpointUri.equals("bestSolution")) {
+                        return message.getHeader(OptaPlannerConstants.BEST_SOLUTION, TimeTable.class);
+                    } else {
+                        return message.getBody(TimeTable.class);
+                    }
+                })
+                .map(this::extractResults)
+                .findFirst();
+
+        if (result.isPresent()) {
+            mockEndpoint.reset();
+            return result.get();
+        }
+        return extractResults(null);
+    }
+
+    private JsonObject extractResults(TimeTable timeTable) {
+        JsonObjectBuilder builder = Json.createObjectBuilder();
+        if (timeTable != null) {
+            Timeslot timeslot = timeTable.getTimeslotList().get(0);
+            Room room = timeTable.getRoomList().get(0);
+            builder.add("timeslot", timeslot.getId()).add("room", room.getId());
+        } else {
+            builder.add("timeslot", "-1").add("room", "-1");
+        }
+        return builder.build();
     }
 }
