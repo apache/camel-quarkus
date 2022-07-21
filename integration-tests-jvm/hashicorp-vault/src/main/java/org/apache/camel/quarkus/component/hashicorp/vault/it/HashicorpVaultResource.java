ee941852d43e81aa5def46467ab418342b6a044b
Andrea Cosentino <ancosen@gmail.com>
2022-07-21 11:37:35 +0200

    Added Hashicorp Vault Camel Quarkus Extension

--- integration-tests-jvm/hashicorp-vault/src/main/java/org/apache/camel/quarkus/component/hashicorp/vault/it/HashicorpVaultResource.java
+++ integration-tests-jvm/hashicorp-vault/src/main/java/org/apache/camel/quarkus/component/hashicorp/vault/it/HashicorpVaultResource.java
@@ -16,13 +16,9 @@
  */
 package org.apache.camel.quarkus.component.hashicorp.vault.it;
 
-import java.net.URI;
-
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
-import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
