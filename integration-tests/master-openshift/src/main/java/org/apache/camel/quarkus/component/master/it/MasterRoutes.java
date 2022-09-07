b0ef3833cd359374fd6ddf38e523bf79e0499d4b
Alexandre Gallice <aldettinger@gmail.com>
2022-09-07 22:11:26 +0200

    master: add test with openshift #4077 (#4078)
    * master: add test with openshift #4077
    
    * fixup

---
+++ integration-tests/master-openshift/src/main/java/org/apache/camel/quarkus/component/master/it/MasterRoutes.java
@@ -0,0 +1,46 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.quarkus.component.master.it;
+
+import javax.enterprise.context.ApplicationScoped;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.kubernetes.cluster.KubernetesClusterService;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+@ApplicationScoped
+public class MasterRoutes extends RouteBuilder {
+
+    @ConfigProperty(name = "application.id")
+    String applicationId;
+
+    @Override
+    public void configure() throws Exception {
+        KubernetesClusterService clusterService = new KubernetesClusterService();
+        clusterService.setPodName(applicationId);
+        getContext().addService(clusterService);
+
+        // Output the id of the application into a file
+        from("master:ns:timer:test?period=100")
+                .id("leader")
+                .setBody(constant("leader"))
+                .setHeader(Exchange.FILE_NAME, constant(String.format("%s.txt", applicationId)))
+                .log(String.format("Application %s is writing into file", applicationId))
+                .to("file:target/cluster/");
+    }
+}
