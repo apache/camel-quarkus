5d6d102cdd124ab3d317fe2dff480f6c22e26102
Eugene Berman <eugene.berman@modusbox.com>
2022-09-13 16:02:12 -0700

    JCache native mode

---
+++ integration-tests/jcache/src/main/java/org/apache/camel/quarkus/component/jcache/it/JcacheResource.java
@@ -0,0 +1,61 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.quarkus.component.jcache.it;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.apache.camel.*;
+import org.jboss.logging.Logger;
+
+@Path("/jcache")
+@ApplicationScoped
+public class JcacheResource {
+
+    private static final Logger LOG = Logger.getLogger(JcacheResource.class);
+
+    private static final String COMPONENT_JCACHE = "jcache";
+    @Inject
+    CamelContext context;
+
+    @Inject
+    ProducerTemplate producerTemplate;
+
+    @Path("/sayHello")
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String sayHello() throws Exception {
+        Exchange exchange = producerTemplate.request("direct:getCachedValue", new Processor() {
+            @Override
+            public void process(Exchange exchange) throws Exception {
+                exchange.getMessage().setHeader("Cache-Key", "123");
+            }
+        });
+
+        String body = exchange.getMessage().getBody(String.class);
+        String inCacheProperty = exchange.getProperty("In-Cache", String.class);
+
+        String response = body + " : " + inCacheProperty;
+
+        return response;
+    }
+
+}
