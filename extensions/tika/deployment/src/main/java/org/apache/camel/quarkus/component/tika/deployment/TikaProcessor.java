65020378cd8efd27c36d3402d3029c0d4de3d5d6
James Netherton <jamesnetherton@gmail.com>
2022-09-20 10:39:05 +0100

    Register LegacyPDFStreamEngine for runtime initialization
    Fixes #4109

--- extensions/tika/deployment/src/main/java/org/apache/camel/quarkus/component/tika/deployment/TikaProcessor.java
+++ extensions/tika/deployment/src/main/java/org/apache/camel/quarkus/component/tika/deployment/TikaProcessor.java
@@ -21,6 +21,7 @@ import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.annotations.ExecutionTime;
 import io.quarkus.deployment.annotations.Record;
 import io.quarkus.deployment.builditem.FeatureBuildItem;
+import io.quarkus.deployment.builditem.nativeimage.RuntimeInitializedClassBuildItem;
 import org.apache.camel.component.tika.TikaComponent;
 import org.apache.camel.quarkus.component.tika.TikaRecorder;
 import org.apache.camel.quarkus.core.deployment.spi.CamelRuntimeBeanBuildItem;
@@ -54,5 +55,10 @@ class TikaProcessor {
                 "tika",
                 TikaComponent.class.getName(),
                 recorder.createTikaComponent(beanContainer.getValue()));
+    }
+
+    @BuildStep
+    RuntimeInitializedClassBuildItem runtimeInitializedClasses() {
+        return new RuntimeInitializedClassBuildItem("org.apache.pdfbox.text.LegacyPDFStreamEngine");
     }
 }
