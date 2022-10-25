6bd7e5f56c98b0c6adaf3d13ab88883ee26bddb6
Lukas Lowinger <llowinge@redhat.com>
2022-10-25 09:55:11 +0200

    Split MTOM tests into AWT and non-AWT modules

---
+++ integration-test-groups/cxf-soap/cxf-soap-mtom-awt/src/main/java/org/apache/camel/quarkus/component/cxf/soap/mtom/awt/it/CxfSoapMtomAwtResource.java
@@ -0,0 +1,88 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.quarkus.component.cxf.soap.mtom.awt.it;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.net.URI;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.imageio.ImageIO;
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.apache.camel.ProducerTemplate;
+
+import static org.apache.camel.component.cxf.common.message.CxfConstants.OPERATION_NAME;
+
+@Path("/cxf-soap/mtom-awt")
+@ApplicationScoped
+public class CxfSoapMtomAwtResource {
+
+    @Inject
+    ProducerTemplate producerTemplate;
+
+    @Path("/upload")
+    @POST
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response upload(@QueryParam("imageName") String imageName, @QueryParam("mtomEnabled") boolean mtomEnabled,
+            byte[] image) throws Exception {
+        try (ByteArrayInputStream bais = new ByteArrayInputStream(image)) {
+            final String response = producerTemplate.requestBodyAndHeader(
+                    "direct:" + mtomEndpoint(mtomEnabled),
+                    new Object[] { ImageIO.read(bais), imageName },
+                    OPERATION_NAME, "uploadImage", String.class);
+            return Response
+                    .created(new URI("https://camel.apache.org/"))
+                    .entity(response)
+                    .build();
+        }
+    }
+
+    @Path("/download")
+    @POST
+    @Consumes(MediaType.TEXT_PLAIN)
+    public Response download(@QueryParam("imageName") String imageName, @QueryParam("mtomEnabled") boolean mtomEnabled)
+            throws Exception {
+        final BufferedImage response = (BufferedImage) producerTemplate.requestBodyAndHeader(
+                "direct:" + mtomEndpoint(mtomEnabled),
+                imageName,
+                OPERATION_NAME,
+                "downloadImage", Image.class);
+        try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
+            ImageIO.write(response, "png", baos);
+            byte[] bytes = baos.toByteArray();
+            return Response
+                    .created(new URI("https://camel.apache.org/"))
+                    .entity(bytes)
+                    .build();
+        }
+    }
+
+    private String mtomEndpoint(boolean mtomEnabled) {
+        return mtomEnabled ? "mtomAwtEnabledInvoker" : "mtomAwtDisabledInvoker";
+    }
+
+}
---
+++ integration-test-groups/cxf-soap/cxf-soap-mtom-awt/src/main/java/org/apache/camel/quarkus/component/cxf/soap/mtom/awt/it/CxfSoapMtomAwtRoutes.java
@@ -0,0 +1,141 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.quarkus.component.cxf.soap.mtom.awt.it;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Produces;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.xml.ws.handler.Handler;
+
+import io.quarkus.runtime.LaunchMode;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.cxf.jaxws.CxfEndpoint;
+import org.apache.cxf.ext.logging.LoggingFeature;
+import org.apache.cxf.message.MessageContentsList;
+import org.eclipse.microprofile.config.Config;
+import org.eclipse.microprofile.config.ConfigProvider;
+
+@ApplicationScoped
+public class CxfSoapMtomAwtRoutes extends RouteBuilder {
+
+    @Inject
+    @Named("loggingMtomAwtFeatureClient")
+    LoggingFeature loggingFeature;
+
+    @Override
+    public void configure() {
+
+        from("direct:mtomAwtEnabledInvoker")
+                .to("cxf:bean:soapMtomAwtEnabledClientEndpoint?dataFormat=POJO");
+
+        from("direct:mtomAwtDisabledInvoker")
+                .to("cxf:bean:soapMtomAwtDisabledClientEndpoint?dataFormat=POJO");
+
+        from("cxf:bean:soapMtomAwtEnabledServerEndpoint?dataFormat=POJO")
+                .to("direct:processAwtImage");
+
+        from("cxf:bean:soapMtomAwtDisabledServerEndpoint?dataFormat=POJO")
+                .to("direct:processAwtImage");
+
+        from("direct:processAwtImage")
+                .process("imageAwtServiceProcessor")
+                .recipientList((simple("bean:imageAwtService?method=${header.operationName}")));
+
+    }
+
+    @ApplicationScoped
+    @Named("imageAwtServiceProcessor")
+    static class ImageServiceProcessor implements Processor {
+        @Override
+        public void process(Exchange exchange) throws Exception {
+            String operationName = (String) exchange.getIn().getHeaders().get("operationName");
+            MessageContentsList list = (MessageContentsList) exchange.getIn().getBody();
+            if ("uploadImage".equals(operationName)) {
+                exchange.getIn().getHeaders().put("image", list.get(0));
+                exchange.getIn().getHeaders().put("imageName", list.get(1));
+                exchange.getIn().getHeaders()
+                        .put("operationName", "uploadImage(${header.image},${header.imageName})");
+            } else if ("downloadImage".equals(operationName)) {
+                exchange.getIn().setBody(list.get(0));
+            }
+        }
+
+    }
+
+    @Produces
+    @ApplicationScoped
+    @Named("loggingMtomAwtFeatureClient")
+    public LoggingFeature loggingFeature() {
+        final LoggingFeature result = new LoggingFeature();
+        result.setPrettyLogging(true);
+        return result;
+    }
+
+    @Produces
+    @ApplicationScoped
+    @Named
+    CxfEndpoint soapMtomAwtEnabledClientEndpoint() {
+        return commonCxfEndpoint(true, getServerUrl() + "/soapservice/mtom-awt-enabled-image-service");
+    }
+
+    @Produces
+    @ApplicationScoped
+    @Named
+    CxfEndpoint soapMtomAwtDisabledClientEndpoint() {
+        return commonCxfEndpoint(false, getServerUrl() + "/soapservice/mtom-awt-disabled-image-service");
+    }
+
+    @Produces
+    @ApplicationScoped
+    @Named
+    CxfEndpoint soapMtomAwtEnabledServerEndpoint() {
+        return commonCxfEndpoint(true, "/mtom-awt-enabled-image-service");
+    }
+
+    @Produces
+    @ApplicationScoped
+    @Named
+    CxfEndpoint soapMtomAwtDisabledServerEndpoint() {
+        return commonCxfEndpoint(false, "/mtom-awt-disabled-image-service");
+    }
+
+    CxfEndpoint commonCxfEndpoint(boolean mtomEnabled, String address) {
+        final CxfEndpoint result = new CxfEndpoint();
+        result.getFeatures().add(loggingFeature);
+        result.setServiceClass(IImageService.class);
+        result.setMtomEnabled(mtomEnabled);
+        result.setAddress(address);
+        List<Handler> handlers = new ArrayList<>();
+        handlers.add(new MtomAttachmentChecker(mtomEnabled));
+        result.setHandlers(handlers);
+        return result;
+    }
+
+    private static String getServerUrl() {
+        Config config = ConfigProvider.getConfig();
+        final int port = LaunchMode.current().equals(LaunchMode.TEST) ? config.getValue("quarkus.http.test-port", Integer.class)
+                : config.getValue("quarkus.http.port", Integer.class);
+        return String.format("http://localhost:%d", port);
+    }
+
+}
