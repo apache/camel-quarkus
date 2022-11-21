d44a159507d2c70a80c904cf797f8db40e25fae4
James Netherton <jamesnetherton@gmail.com>
2022-11-21 08:51:08 +0000

    Disable DatasonnetIT due to #4284

--- integration-tests/datasonnet/src/test/java/org/apache/camel/quarkus/component/datasonnet/it/DatasonnetIT.java
+++ integration-tests/datasonnet/src/test/java/org/apache/camel/quarkus/component/datasonnet/it/DatasonnetIT.java
@@ -17,7 +17,9 @@
 package org.apache.camel.quarkus.component.datasonnet.it;
 
 import io.quarkus.test.junit.QuarkusIntegrationTest;
+import org.junit.jupiter.api.Disabled;
 
+@Disabled("https://github.com/apache/camel-quarkus/issues/4284")
 @QuarkusIntegrationTest
 class DatasonnetIT extends DatasonnetTest {
 
