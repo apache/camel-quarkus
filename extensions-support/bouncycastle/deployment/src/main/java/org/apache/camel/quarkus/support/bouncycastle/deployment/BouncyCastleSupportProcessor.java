77a01052aa8053b01f106bb9e21e0f57ba9845e3
JiriOndrusek <ondrusek.jiri@gmail.com>
2022-11-22 08:18:05 +0100

    Issue 2922 - remove of unecessarry  code from bouncycastle extension.

--- extensions-support/bouncycastle/deployment/src/main/java/org/apache/camel/quarkus/support/bouncycastle/deployment/BouncyCastleSupportProcessor.java
+++ extensions-support/bouncycastle/deployment/src/main/java/org/apache/camel/quarkus/support/bouncycastle/deployment/BouncyCastleSupportProcessor.java
@@ -23,45 +23,15 @@ import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.annotations.ExecutionTime;
 import io.quarkus.deployment.annotations.Record;
-import io.quarkus.deployment.builditem.CombinedIndexBuildItem;
-import io.quarkus.deployment.builditem.IndexDependencyBuildItem;
 import io.quarkus.deployment.builditem.ShutdownContextBuildItem;
-import io.quarkus.deployment.builditem.nativeimage.ReflectiveClassBuildItem;
-import io.quarkus.deployment.builditem.nativeimage.RuntimeReinitializedClassBuildItem;
 import io.quarkus.security.deployment.BouncyCastleProviderBuildItem;
 import org.apache.camel.quarkus.support.bouncycastle.BouncyCastleRecorder;
-import org.jboss.jandex.IndexView;
 
 public class BouncyCastleSupportProcessor {
 
     @BuildStep
     void produceBouncyCastleProvider(BuildProducer<BouncyCastleProviderBuildItem> bouncyCastleProvider) {
         bouncyCastleProvider.produce(new BouncyCastleProviderBuildItem());
-    }
-
-    @BuildStep
-    ReflectiveClassBuildItem registerForReflection(CombinedIndexBuildItem combinedIndex) {
-        IndexView index = combinedIndex.getIndex();
-
-        String[] dtos = index.getKnownClasses().stream()
-                .map(ci -> ci.name().toString())
-                .filter(n -> n.startsWith("org.bouncycastle.jcajce.provider.digest.") ||
-                        n.startsWith("org.bouncycastle.jcajce.provider.symmetric.") ||
-                        n.startsWith("org.bouncycastle.jcajce.provider.asymmetric.") ||
-                        n.startsWith("org.bouncycastle.jcajce.provider.keystore."))
-                .toArray(String[]::new);
-
-        return new ReflectiveClassBuildItem(false, false, dtos);
-    }
-
-    @BuildStep
-    IndexDependencyBuildItem registerBCDependencyForIndex() {
-        return new IndexDependencyBuildItem("org.bouncycastle", "bcprov-jdk15on");
-    }
-
-    @BuildStep
-    void secureRandomConfiguration(BuildProducer<RuntimeReinitializedClassBuildItem> reinitialized) {
-        reinitialized.produce(new RuntimeReinitializedClassBuildItem("java.security.SecureRandom"));
     }
 
     @BuildStep
