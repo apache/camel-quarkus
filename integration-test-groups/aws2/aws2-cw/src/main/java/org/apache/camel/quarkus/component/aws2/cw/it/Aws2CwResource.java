b79251c175171869bfbc93908820608e002f027a
JiriOndrusek <ondrusek.jiri@gmail.com>
2022-12-16 12:10:38 +0100

    Expand AWS CW test coverage #4196

--- integration-test-groups/aws2/aws2-cw/src/main/java/org/apache/camel/quarkus/component/aws2/cw/it/Aws2CwResource.java
+++ integration-test-groups/aws2/aws2-cw/src/main/java/org/apache/camel/quarkus/component/aws2/cw/it/Aws2CwResource.java
@@ -17,19 +17,26 @@
 package org.apache.camel.quarkus.component.aws2.cw.it;
 
 import java.net.URI;
+import java.time.Instant;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
 
 import io.quarkus.scheduler.Scheduled;
+import org.apache.camel.CamelExecutionException;
 import org.apache.camel.ProducerTemplate;
+import org.apache.camel.component.aws2.cw.Cw2Constants;
 
 @Path("/aws2-cw")
 @ApplicationScoped
@@ -57,6 +64,49 @@ public class Aws2CwResource {
             @PathParam("metric-name") String name,
             @PathParam("metric-unit") String unit) throws Exception {
         endpointUri = "aws2-cw://" + namespace + "?name=" + name + "&value=" + value + "&unit=" + unit;
+        return Response
+                .created(new URI("https://camel.apache.org/"))
+                .build();
+    }
+
+    @Path("/send-metric-map/{namespace}")
+    @POST
+    @Consumes("application/x-www-form-urlencoded")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response postMap(
+            @PathParam("namespace") String namespace,
+            @HeaderParam("customClientName") String customClientName,
+            MultivaluedMap<String, String> formParams) throws Exception {
+
+        String uri = "aws2-cw://" + namespace;
+        uri = customClientName != null && !customClientName.isEmpty()
+                ? uri + "?autowiredEnabled=false&amazonCwClient=#" + customClientName : uri;
+
+        Map<String, Object> typedHeaders = formParams.entrySet().stream().collect(Collectors.toMap(
+                e -> e.getKey(),
+                e -> {
+                    final String val = e.getValue().get(0);
+                    if (Cw2Constants.METRIC_TIMESTAMP.equals(e.getKey())) {
+                        return Instant.ofEpochMilli(Long.parseLong(val));
+                    } else if (Cw2Constants.METRIC_VALUE.equals(e.getKey())) {
+                        return Long.parseLong(val);
+                    } else if (Cw2Constants.METRIC_DIMENSIONS.equals(e.getKey())) {
+                        String[] keyVal = val.split("=");
+                        return Map.of(keyVal[0], keyVal[1]);
+                    }
+                    return val;
+                }));
+        try {
+            producerTemplate.requestBodyAndHeaders(uri, null, typedHeaders, String.class);
+        } catch (Exception e) {
+            if (e instanceof CamelExecutionException && e.getCause() != null) {
+                return Response
+                        .ok(e.getCause().getMessage())
+                        .build();
+            }
+            throw e;
+        }
+
         return Response
                 .created(new URI("https://camel.apache.org/"))
                 .build();
--- integration-test-groups/aws2/aws2-cw/src/test/java/org/apache/camel/quarkus/component/aws2/cw/it/Aws2CwTest.java
+++ integration-test-groups/aws2/aws2-cw/src/test/java/org/apache/camel/quarkus/component/aws2/cw/it/Aws2CwTest.java
@@ -17,23 +17,30 @@
 package org.apache.camel.quarkus.component.aws2.cw.it;
 
 import java.time.Instant;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import org.apache.camel.component.aws2.cw.Cw2Constants;
 import org.apache.camel.quarkus.test.support.aws2.Aws2Client;
 import org.apache.camel.quarkus.test.support.aws2.Aws2TestResource;
+import org.apache.camel.util.CollectionHelper;
 import org.awaitility.Awaitility;
 import org.jboss.logging.Logger;
 import org.junit.jupiter.api.Test;
 import org.testcontainers.containers.localstack.LocalStackContainer.Service;
 import software.amazon.awssdk.services.cloudwatch.CloudWatchClient;
 import software.amazon.awssdk.services.cloudwatch.model.Datapoint;
+import software.amazon.awssdk.services.cloudwatch.model.Dimension;
 import software.amazon.awssdk.services.cloudwatch.model.GetMetricStatisticsRequest;
 import software.amazon.awssdk.services.cloudwatch.model.Statistic;
+
+import static org.hamcrest.Matchers.is;
 
 @QuarkusTest
 @QuarkusTestResource(Aws2TestResource.class)
@@ -88,6 +95,150 @@ class Aws2CwTest {
                             + " && maximum ~ " + value);
                 });
 
+    }
+
+    @Test
+    public void headers() {
+
+        final Instant startTime = Instant.ofEpochMilli(System.currentTimeMillis() - 10000);
+
+        final String namespace = "cq-metrics-" + java.util.UUID.randomUUID().toString().replace("-", "");
+        final String metricName = "metricName" + java.util.UUID.randomUUID().toString().replace("-", "");
+        final int value = (int) (Math.random() * 10000);
+
+        List<Map<String, String>> data = new LinkedList<>();
+
+        data.add(CollectionHelper.mapOf(
+                Cw2Constants.METRIC_NAMESPACE, namespace,
+                Cw2Constants.METRIC_NAME, metricName,
+                Cw2Constants.METRIC_VALUE, 2 * value,
+                Cw2Constants.METRIC_UNIT, "Count",
+                Cw2Constants.METRIC_DIMENSION_NAME, "type",
+                Cw2Constants.METRIC_DIMENSION_VALUE, "even"));
+        data.add(CollectionHelper.mapOf(
+                Cw2Constants.METRIC_NAMESPACE, namespace,
+                Cw2Constants.METRIC_NAME, metricName,
+                Cw2Constants.METRIC_VALUE, 2 * value + 2,
+                Cw2Constants.METRIC_UNIT, "Count",
+                Cw2Constants.METRIC_DIMENSIONS, "type=even"));
+        data.add(CollectionHelper.mapOf(
+                Cw2Constants.METRIC_NAMESPACE, namespace,
+                Cw2Constants.METRIC_NAME, metricName,
+                Cw2Constants.METRIC_VALUE, 2 * value + 4,
+                Cw2Constants.METRIC_UNIT, "Count",
+                Cw2Constants.METRIC_DIMENSION_NAME, "type",
+                Cw2Constants.METRIC_DIMENSION_VALUE, "even"));
+
+        data.add(CollectionHelper.mapOf(
+                Cw2Constants.METRIC_NAMESPACE, namespace,
+                Cw2Constants.METRIC_NAME, metricName,
+                Cw2Constants.METRIC_VALUE, 2 * value + 1,
+                Cw2Constants.METRIC_UNIT, "Count",
+                Cw2Constants.METRIC_DIMENSION_NAME, "type",
+                Cw2Constants.METRIC_DIMENSION_VALUE, "odd"));
+        data.add(CollectionHelper.mapOf(
+                Cw2Constants.METRIC_NAMESPACE, namespace, Cw2Constants.METRIC_NAME, metricName,
+                Cw2Constants.METRIC_VALUE, 2 * value + 3,
+                Cw2Constants.METRIC_UNIT, "Count",
+                Cw2Constants.METRIC_DIMENSION_NAME, "type",
+                Cw2Constants.METRIC_DIMENSION_VALUE, "odd"));
+        // ignored because of timestamp
+        data.add(CollectionHelper.mapOf(
+                Cw2Constants.METRIC_NAMESPACE, namespace, Cw2Constants.METRIC_NAME, metricName,
+                Cw2Constants.METRIC_VALUE, 2 * value + 5,
+                Cw2Constants.METRIC_TIMESTAMP, System.currentTimeMillis() - 24 * 60 * 60 * 1000,
+                Cw2Constants.METRIC_UNIT, "Count",
+                Cw2Constants.METRIC_DIMENSION_NAME, "type",
+                Cw2Constants.METRIC_DIMENSION_VALUE, "odd"));
+
+        for (Map<String, String> item : data) {
+            RestAssured.given()
+                    .contentType("application/x-www-form-urlencoded; charset=utf-8")
+                    .formParams(item)
+                    .post("/aws2-cw/send-metric-map/" + namespace)
+                    .then()
+                    .statusCode(201);
+        }
+
+        Awaitility.await().pollInterval(1, TimeUnit.SECONDS).atMost(120, TimeUnit.SECONDS).until(
+                () -> {
+
+                    List<Datapoint> oddDatapoints = client.getMetricStatistics(
+                            GetMetricStatisticsRequest.builder()
+                                    .namespace(namespace)
+                                    .metricName(metricName)
+                                    .statistics(Statistic.SAMPLE_COUNT, Statistic.MINIMUM, Statistic.MAXIMUM)
+                                    .startTime(startTime)
+                                    .dimensions(Dimension.builder().name("type").value("odd").build())
+                                    .endTime(Instant.ofEpochMilli(System.currentTimeMillis() + 10000))
+                                    .period(30)
+                                    .build())
+                            .datapoints();
+
+                    List<Datapoint> evenDatapoints = client.getMetricStatistics(
+                            GetMetricStatisticsRequest.builder()
+                                    .namespace(namespace)
+                                    .metricName(metricName)
+                                    .statistics(Statistic.SAMPLE_COUNT, Statistic.AVERAGE)
+                                    .startTime(startTime)
+                                    .dimensions(Dimension.builder().name("type").value("even").build())
+                                    .endTime(Instant.ofEpochMilli(System.currentTimeMillis() + 10000))
+                                    .period(30)
+                                    .build())
+                            .datapoints();
+                    LOG.debug("Expecting some datapoints for metric " + namespace + "/" + metricName + " (type='odd'), got "
+                            + oddDatapoints);
+                    if (oddDatapoints.isEmpty()) {
+                        return false;
+                    }
+                    LOG.debug("Expecting some datapoints for metric " + namespace + "/" + metricName + " (type='even'), got "
+                            + evenDatapoints);
+                    if (evenDatapoints.isEmpty()) {
+                        return false;
+                    }
+
+                    Datapoint oddDp = oddDatapoints.get(0);
+                    Datapoint evenDp = evenDatapoints.get(0);
+
+                    if (!(oddDp.sampleCount().intValue() == 2
+                            && oddDp.minimum().intValue() == 2 * value + 1
+                            && oddDp.maximum().intValue() == 2 * value + 3)) {
+                        throw new RuntimeException("Unexpected odd datapoint " + oddDp
+                                + "; expected sampleCount == 2 && minimum ~ " + (2 * value + 1)
+                                + " && maximum ~ " + (2 * value + 3));
+                    }
+
+                    if (!(evenDp.average() == 2 * value + 2
+                            && evenDp.sampleCount() == 3)) {
+                        throw new RuntimeException("Unexpected even datapoint " + evenDp
+                                + "; expected sampleCount == 3 && average == " + (2 * value + 2));
+                    }
+
+                    return true;
+                });
+
+    }
+
+    @Test
+    public void customClient() {
+        final String namespace = "cq-metrics-" + java.util.UUID.randomUUID().toString().replace("-", "");
+        final String metricName = "metricName" + java.util.UUID.randomUUID().toString().replace("-", "");
+        final int value = (int) (Math.random() * 10000);
+
+        Map<String, Object> data = CollectionHelper.mapOf(
+                Cw2Constants.METRIC_NAMESPACE, namespace,
+                Cw2Constants.METRIC_NAME, metricName,
+                Cw2Constants.METRIC_VALUE, 2 * value,
+                Cw2Constants.METRIC_UNIT, "Count");
+
+        RestAssured.given()
+                .contentType("application/x-www-form-urlencoded; charset=utf-8")
+                .header("customClientName", "customClient")
+                .formParams(data)
+                .post("/aws2-cw/send-metric-map/" + namespace)
+                .then()
+                .statusCode(200)
+                .body(is("CloudWatchClientMock"));
     }
 
 }
