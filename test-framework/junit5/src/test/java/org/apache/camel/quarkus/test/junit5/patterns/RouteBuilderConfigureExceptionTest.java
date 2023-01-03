df7368332bf850eddb96a38322e291096cb528bc
JiriOndrusek <ondrusek.jiri@gmail.com>
2023-01-03 16:12:34 +0100

    DoBeforeEach does not work with Advice #4362

--- test-framework/junit5/src/test/java/org/apache/camel/quarkus/test/junit5/patterns/RouteBuilderConfigureExceptionTest.java
+++ test-framework/junit5/src/test/java/org/apache/camel/quarkus/test/junit5/patterns/RouteBuilderConfigureExceptionTest.java
@@ -20,7 +20,6 @@ import io.quarkus.test.junit.QuarkusTest;
 import org.apache.camel.Predicate;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.quarkus.test.CamelQuarkusTestSupport;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.fail;
@@ -31,7 +30,6 @@ public class RouteBuilderConfigureExceptionTest extends CamelQuarkusTestSupport 
     private Predicate iAmNull;
 
     @Override
-    @BeforeEach
     public void setUp() {
         try {
             super.setUp();
