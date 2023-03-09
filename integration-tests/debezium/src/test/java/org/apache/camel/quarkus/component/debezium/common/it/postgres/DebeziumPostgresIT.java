edc257b9a4181b34c00ebb056c0a6711eb5fa552
James Netherton <jamesnetherton@gmail.com>
2023-03-09 15:31:16 +0000

    Disable DebeziumMongodbIT and DebeziumPostgresIT due to #4638

--- integration-tests/debezium/src/test/java/org/apache/camel/quarkus/component/debezium/common/it/postgres/DebeziumPostgresIT.java
+++ integration-tests/debezium/src/test/java/org/apache/camel/quarkus/component/debezium/common/it/postgres/DebeziumPostgresIT.java
@@ -17,7 +17,9 @@
 package org.apache.camel.quarkus.component.debezium.common.it.postgres;
 
 import io.quarkus.test.junit.QuarkusIntegrationTest;
+import org.junit.jupiter.api.Disabled;
 
+@Disabled("https://github.com/apache/camel-quarkus/issues/4638")
 @QuarkusIntegrationTest
 class DebeziumPostgresIT extends DebeziumPostgresTest {
 
