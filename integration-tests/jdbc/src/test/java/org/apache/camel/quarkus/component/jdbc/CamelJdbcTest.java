f8e7a8fa18db8ba31eb793052c9697afe170e911
Lucia Drozdová <8985786+ldrozdo@users.noreply.github.com>
2023-03-24 14:27:02 +0100

    Ref #4596: Expand JDBC tests - named parameters and samples (#4655)
    * Ref #4596: Expand JDBC tests - named parameters and samples
    
    * Fix imports

--- integration-tests/jdbc/src/test/java/org/apache/camel/quarkus/component/jdbc/CamelJdbcTest.java
+++ integration-tests/jdbc/src/test/java/org/apache/camel/quarkus/component/jdbc/CamelJdbcTest.java
@@ -16,6 +16,8 @@
  */
 package org.apache.camel.quarkus.component.jdbc;
 
+import java.util.List;
+
 import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.h2.H2DatabaseTestResource;
 import io.quarkus.test.junit.QuarkusTest;
@@ -23,7 +25,11 @@ import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @QuarkusTest
 @QuarkusTestResource(H2DatabaseTestResource.class)
@@ -53,4 +59,109 @@ public class CamelJdbcTest {
                 .post("/test/execute")
                 .then().body(is("[{ID=3}, {ID=2}, {ID=1}]"));
     }
+
+    @Test
+    void testCamelRetrieveGeneratedKeysHeader() {
+        List generatedIDs = RestAssured.given()
+                .get("test/generated-keys/rows")
+                .then().extract().body()
+                .jsonPath().getList("ID");
+
+        String selectResult = RestAssured.given()
+                .contentType(ContentType.TEXT).body("select id from camelsGenerated")
+                .post("/test/execute")
+                .then().extract().body().asString();
+
+        generatedIDs.forEach(generatedID -> assertTrue(selectResult.contains(generatedID.toString())));
+    }
+
+    @Test
+    void testHeadersFromInsertOrUpdateQuery() {
+        RestAssured.given()
+                .get("test/headers/insert")
+                .then()
+                .body(containsString("CamelGeneratedKeysRowCount=2"))
+                .and()
+                .body(containsString("CamelJdbcUpdateCount=2"))
+                .and()
+                .body(containsString("CamelRetrieveGeneratedKeys=true"))
+                .and()
+                .body(not(containsString("CamelJdbcRowCount")))
+                .and()
+                .body(not(containsString("CamelJdbcColumnNames")))
+                .and()
+                .body(not(containsString("CamelJdbcParameters")))
+                .and()
+                .body(not(containsString("CamelGeneratedColumns")));
+    }
+
+    @Test
+    void testHeadersFromSelectQuery() {
+        RestAssured.given()
+                .get("test/headers/select")
+                .then()
+                .body(not(containsString("CamelGeneratedKeysRowCount")))
+                .and()
+                .body(not(containsString("CamelJdbcUpdateCount")))
+                .and()
+                .body(not(containsString("CamelRetrieveGeneratedKeys")))
+                .and()
+                .body(not(containsString("CamelJdbcParameters")))
+                .and()
+                .body(not(containsString("CamelGeneratedColumns")))
+                .and()
+                .body(containsString("CamelJdbcRowCount"))
+                .and()
+                .body(containsString("CamelJdbcColumnNames=[ID, SPECIES]"));
+    }
+
+    @Test
+    void testNamedParameters() {
+        RestAssured.given()
+                .get("test/named-parameters/headers-as-parameters")
+                .then()
+                .body(containsString("{ID=1, SPECIES=Camelus dromedarius}"))
+                .and()
+                .body(containsString("{ID=2, SPECIES=Camelus bactrianus}"));
+    }
+
+    @Test
+    void testCamelJdbcParametersHeader() {
+        RestAssured.given()
+                .get("test/named-parameters/headers-as-parameters-map")
+                .then()
+                .body(containsString("{ID=2, SPECIES=Camelus bactrianus}"));
+    }
+
+    @Test
+    void testTimeIntervalDatabasePolling() {
+        String selectResult = RestAssured.given()
+                .contentType(ContentType.TEXT).body("select * from camelsGenerated order by id desc")
+                .post("/test/execute")
+                .then().extract().body().asString();
+
+        RestAssured.given()
+                .body(selectResult)
+                .get("/test/interval-polling")
+                .then()
+                .statusCode(204);
+    }
+
+    @Test
+    void testMoveDataBetweenDatasources() {
+        String camelsDbResult = RestAssured.given()
+                .contentType(ContentType.TEXT).body("select * from camels order by id desc")
+                .post("/test/execute")
+                .then().extract().body().asString();
+
+        RestAssured.given()
+                .post("test/move-between-datasources");
+
+        RestAssured.given()
+                .contentType(ContentType.TEXT).body("select * from camelsProcessed order by id desc")
+                .post("/test/execute")
+                .then()
+                .body(equalTo(camelsDbResult));
+    }
+
 }
