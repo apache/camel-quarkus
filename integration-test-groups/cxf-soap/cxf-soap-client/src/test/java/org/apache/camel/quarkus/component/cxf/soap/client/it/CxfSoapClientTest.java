beae6109eeb1cf279bc10fd522c976a4c6d2985c
JiriOndrusek <ondrusek.jiri@gmail.com>
2023-03-28 09:58:37 +0200

    Fix in cxf-soap-client

--- integration-test-groups/cxf-soap/cxf-soap-client/src/test/java/org/apache/camel/quarkus/component/cxf/soap/client/it/CxfSoapClientTest.java
+++ integration-test-groups/cxf-soap/cxf-soap-client/src/test/java/org/apache/camel/quarkus/component/cxf/soap/client/it/CxfSoapClientTest.java
@@ -53,7 +53,7 @@ class CxfSoapClientTest {
                 .post("/cxf-soap/client/simple")
                 .then()
                 .statusCode(201)
-                .body(equalTo("3"));
+                .body(equalTo("12"));
     }
 
     @ParameterizedTest
