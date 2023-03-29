1095175e570d54ff08c70f19c8c139d76e398903
Alexandre Gallice <aldettinger@gmail.com>
2023-03-29 14:01:48 +0200

    Complete vertx-http test coverage #4658

--- integration-tests/http/src/main/java/org/apache/camel/quarkus/component/http/it/HttpService.java
+++ integration-tests/http/src/main/java/org/apache/camel/quarkus/component/http/it/HttpService.java
@@ -16,13 +16,21 @@
  */
 package org.apache.camel.quarkus.component.http.it;
 
+import java.util.Map;
+
 import javax.enterprise.context.ApplicationScoped;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.CookieParam;
+import javax.ws.rs.ForbiddenException;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.NewCookie;
+import javax.ws.rs.core.Response;
 
 @Path("/service")
 @ApplicationScoped
@@ -41,4 +49,53 @@ public class HttpService {
     public String toUpper(String message) {
         return message.toUpperCase();
     }
+
+    @POST
+    @Path("/multipart-form-params")
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Produces(MediaType.TEXT_PLAIN)
+    public String multipartFormParams(@FormParam("organization") String organization, @FormParam("project") String project) {
+        return String.format("multipartFormParams(%s, %s)", organization, project);
+    }
+
+    @POST
+    @Path("/multipart-form-data")
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    @Produces(MediaType.TEXT_PLAIN)
+    public String multipartFormData(Map<String, String> parts) {
+        if (parts.size() != 2 || !parts.keySet().contains("part-1") || !parts.keySet().contains("part-2")) {
+            throw new IllegalArgumentException(
+                    "There should be exactly 2 parts named \"part-1\" and \"parts-2\" in the multipart upload");
+        }
+        return String.format("multipartFormData(%s, %s)", parts.get("part-1"), parts.get("part-2"));
+    }
+
+    @GET
+    @Path("/custom-vertx-options")
+    public void customVertxOptions() {
+        // We are not expected to pass here as the Vert.x HTTP client should throw IllegalArgumentException 
+    }
+
+    @GET
+    @Path("/session-management/secure")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getSecuredContent(@CookieParam("sessionId") String cookie) {
+        if ("my-session-id-123".equals(cookie)) {
+            return "Some secret content";
+        } else {
+            throw new ForbiddenException("A cookie with session id is needed to access the secured content");
+        }
+    }
+
+    @GET
+    @Path("/session-management/login")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response login(@HeaderParam("username") String username, @HeaderParam("password") String password) {
+        if ("my-username".equals(username) && "my-password".equals(password)) {
+            NewCookie cookie = new NewCookie("sessionId", "my-session-id-123");
+            return Response.ok().cookie(cookie).build();
+        }
+        throw new ForbiddenException("Wrong username/password, no cookie will be created");
+    }
+
 }
