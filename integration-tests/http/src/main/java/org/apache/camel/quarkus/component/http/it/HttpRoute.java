6219742e5bec8681fa42aee460ef01e73df408d1
aldettinger <aldettinger@gmail.com>
2023-03-30 10:34:44 +0200

    vertx-http: remove deprecated serviceCall eip

--- integration-tests/http/src/main/java/org/apache/camel/quarkus/component/http/it/HttpRoute.java
+++ integration-tests/http/src/main/java/org/apache/camel/quarkus/component/http/it/HttpRoute.java
@@ -66,17 +66,6 @@ public class HttpRoute extends RouteBuilder {
                     }
                 });
 
-        from("netty-http:http://0.0.0.0:{{camel.netty-http.test-port}}/test/server/serviceCall")
-                .serviceCall()
-                .name("myService/test/server/myService")
-                .component("netty-http")
-                .staticServiceDiscovery()
-                .servers("myService@localhost:{{camel.netty-http.test-port}}")
-                .end();
-
-        from("netty-http:http://0.0.0.0:{{camel.netty-http.test-port}}/test/server/myService")
-                .transform().constant("Hello from myService");
-
         from("direct:vertx-http-buffer-conversion-with-charset")
                 .convertBodyTo(Buffer.class);
     }
