463e79aee4d977f69d7af0d7d5e521e1721068c4
Peter Palaga <ppalaga@redhat.com>
2023-03-31 11:45:35 +0200

    Add back XML Security Sign #4684

--- integration-tests/xmlsecurity/src/main/java/org/apache/camel/quarkus/component/xmlsecurity/it/XmlsecurityResource.java
+++ integration-tests/xmlsecurity/src/main/java/org/apache/camel/quarkus/component/xmlsecurity/it/XmlsecurityResource.java
@@ -22,7 +22,6 @@ import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
-
 import org.apache.camel.ProducerTemplate;
 
 @Path("/xmlsecurity")
