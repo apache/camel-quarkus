a79d56b75be2ebe090847e917de42e11b0e6c57f
Lucia Drozdova <ldrozdo@redhat.com>
2023-04-06 14:57:04 +0200

    Ref#4596 Cover different db types for jdbc

--- integration-tests/jdbc/src/main/java/org/apache/camel/quarkus/component/jdbc/JdbcRoutes.java
+++ integration-tests/jdbc/src/main/java/org/apache/camel/quarkus/component/jdbc/JdbcRoutes.java
@@ -20,26 +20,29 @@ import jakarta.enterprise.context.ApplicationScoped;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
-import org.jboss.logging.Logger;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 @ApplicationScoped
 public class JdbcRoutes extends RouteBuilder {
-    private static final Logger LOG = Logger.getLogger(JdbcRoutes.class);
+
+    @ConfigProperty(name = "quarkus.datasource.cameldb.db-kind")
+    String dbKind;
 
     @Override
     public void configure() {
         from("direct://get-generated-keys")
-                .to("jdbc:camel-ds")
+                .to("jdbc:cameldb")
                 .process(new Processor() {
                     @Override
                     public void process(Exchange exchange) throws Exception {
+                        System.out.println(exchange.getIn().getHeaders());
                         Object in = exchange.getIn().getHeader("CamelGeneratedKeysRows");
                         exchange.getIn().setBody(in);
                     }
                 });
 
         from("direct://get-headers")
-                .to("jdbc:camel-ds")
+                .to("jdbc:cameldb")
                 .process(new Processor() {
                     @Override
                     public void process(Exchange exchange) throws Exception {
@@ -49,18 +52,25 @@ public class JdbcRoutes extends RouteBuilder {
                 });
 
         from("direct://headers-as-parameters")
-                .to("jdbc:camel-ds?useHeadersAsParameters=true");
+                .to("jdbc:cameldb?useHeadersAsParameters=true");
 
         from("timer://interval-polling?delay=2000&repeatCount=1").routeId("jdbc-poll").autoStartup(false)
                 .setBody(constant("select * from camelsGenerated order by id desc"))
-                .to("jdbc:camel-ds")
+                .to("jdbc:cameldb")
                 .to("mock:interval-polling");
+
+        String species;
+        if (dbKind.equals("postgresql") || dbKind.equals("mysql") || dbKind.equals("mariadb") || dbKind.equals("mssql")) {
+            species = "species";
+        } else {
+            species = "SPECIES";
+        }
 
         from("direct://move-between-datasources")
                 .setBody(constant("select * from camels"))
-                .to("jdbc:camel-ds")
+                .to("jdbc:cameldb")
                 .split(body())
-                .setBody(simple("insert into camelsProcessed values('${body[ID]}','${body[SPECIES]}')"))
-                .to("jdbc:camel-ds");
+                .setBody(simple("insert into camelsProcessed (species) values('${body[" + species + "]}')"))
+                .to("jdbc:cameldb");
     }
 }
