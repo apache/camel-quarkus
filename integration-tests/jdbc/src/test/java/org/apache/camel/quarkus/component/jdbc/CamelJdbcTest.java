294f2f8330122b85e1396d4ae4c221ad05ec58e3
Lucia Drozdova <ldrozdo@redhat.com>
2023-04-06 14:57:04 +0200

    Ref#4596 Cover different db types for jdbc

--- integration-tests/jdbc/src/test/java/org/apache/camel/quarkus/component/jdbc/CamelJdbcTest.java
+++ integration-tests/jdbc/src/test/java/org/apache/camel/quarkus/component/jdbc/CamelJdbcTest.java
@@ -18,28 +18,29 @@ package org.apache.camel.quarkus.component.jdbc;
 
 import java.util.List;
 
-import io.quarkus.test.common.QuarkusTestResource;
-import io.quarkus.test.h2.H2DatabaseTestResource;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledIfSystemProperty;
 
-import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.containsStringIgnoringCase;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.equalToIgnoringCase;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.not;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @QuarkusTest
-@QuarkusTestResource(H2DatabaseTestResource.class)
+@DisabledIfSystemProperty(named = "cq.jdbcKind", matches = "derby")
+//https://github.com/quarkusio/quarkus/issues/23083
 public class CamelJdbcTest {
-
     @Test
     void testGetSpeciesById() {
-        RestAssured.when().get("/test/species/1").then().body(is("[{SPECIES=Camelus dromedarius}]"));
-        RestAssured.when().get("/test/species/2").then().body(is("[{SPECIES=Camelus bactrianus}]"));
-        RestAssured.when().get("/test/species/3").then().body(is("[{SPECIES=Camelus ferus}]"));
+        RestAssured.when().get("/test/species/1").then().body(equalToIgnoringCase("[{SPECIES=Camelus dromedarius}]"));
+        RestAssured.when().get("/test/species/2").then().body(equalToIgnoringCase("[{SPECIES=Camelus bactrianus}]"));
+        RestAssured.when().get("/test/species/3").then().body(equalToIgnoringCase("[{SPECIES=Camelus ferus}]"));
     }
 
     @Test
@@ -49,7 +50,7 @@ public class CamelJdbcTest {
 
     @Test
     void testGetSpeciesByIdWithDefinedType() {
-        RestAssured.when().get("/test/species/1/type").then().body(is("Camelus dromedarius 1"));
+        RestAssured.when().get("/test/species/1/type").then().body(equalToIgnoringCase("Camelus dromedarius 1"));
     }
 
     @Test
@@ -57,22 +58,25 @@ public class CamelJdbcTest {
         RestAssured.given()
                 .contentType(ContentType.TEXT).body("select id from camels order by id desc")
                 .post("/test/execute")
-                .then().body(is("[{ID=3}, {ID=2}, {ID=1}]"));
+                .then().body(equalToIgnoringCase("[{ID=3}, {ID=2}, {ID=1}]"));
     }
 
     @Test
     void testCamelRetrieveGeneratedKeysHeader() {
+        String idKey = RestAssured.when()
+                .get("/test/get-id-key")
+                .then()
+                .extract()
+                .body().asString();
+
         List generatedIDs = RestAssured.given()
                 .get("test/generated-keys/rows")
                 .then().extract().body()
-                .jsonPath().getList("ID");
+                .jsonPath().getList(idKey);
 
-        String selectResult = RestAssured.given()
-                .contentType(ContentType.TEXT).body("select id from camelsGenerated")
-                .post("/test/execute")
-                .then().extract().body().asString();
+        assertFalse(generatedIDs.isEmpty());
+        assertNotNull(generatedIDs.get(0));
 
-        generatedIDs.forEach(generatedID -> assertTrue(selectResult.contains(generatedID.toString())));
     }
 
     @Test
@@ -80,19 +84,19 @@ public class CamelJdbcTest {
         RestAssured.given()
                 .get("test/headers/insert")
                 .then()
-                .body(containsString("CamelGeneratedKeysRowCount=2"))
+                .body(containsStringIgnoringCase("CamelGeneratedKeysRowCount=1"))
                 .and()
-                .body(containsString("CamelJdbcUpdateCount=2"))
+                .body(containsStringIgnoringCase("CamelJdbcUpdateCount=1"))
                 .and()
-                .body(containsString("CamelRetrieveGeneratedKeys=true"))
+                .body(containsStringIgnoringCase("CamelRetrieveGeneratedKeys=true"))
                 .and()
-                .body(not(containsString("CamelJdbcRowCount")))
+                .body(not(containsStringIgnoringCase("CamelJdbcRowCount")))
                 .and()
-                .body(not(containsString("CamelJdbcColumnNames")))
+                .body(not(containsStringIgnoringCase("CamelJdbcColumnNames")))
                 .and()
-                .body(not(containsString("CamelJdbcParameters")))
+                .body(not(containsStringIgnoringCase("CamelJdbcParameters")))
                 .and()
-                .body(not(containsString("CamelGeneratedColumns")));
+                .body(not(containsStringIgnoringCase("CamelGeneratedColumns")));
     }
 
     @Test
@@ -100,19 +104,19 @@ public class CamelJdbcTest {
         RestAssured.given()
                 .get("test/headers/select")
                 .then()
-                .body(not(containsString("CamelGeneratedKeysRowCount")))
+                .body(not(containsStringIgnoringCase("CamelGeneratedKeysRowCount")))
                 .and()
-                .body(not(containsString("CamelJdbcUpdateCount")))
+                .body(not(containsStringIgnoringCase("CamelJdbcUpdateCount")))
                 .and()
-                .body(not(containsString("CamelRetrieveGeneratedKeys")))
+                .body(not(containsStringIgnoringCase("CamelRetrieveGeneratedKeys")))
                 .and()
-                .body(not(containsString("CamelJdbcParameters")))
+                .body(not(containsStringIgnoringCase("CamelJdbcParameters")))
                 .and()
-                .body(not(containsString("CamelGeneratedColumns")))
+                .body(not(containsStringIgnoringCase("CamelGeneratedColumns")))
                 .and()
-                .body(containsString("CamelJdbcRowCount"))
+                .body(containsStringIgnoringCase("CamelJdbcRowCount"))
                 .and()
-                .body(containsString("CamelJdbcColumnNames=[ID, SPECIES]"));
+                .body(containsStringIgnoringCase("CamelJdbcColumnNames=[ID, SPECIES]"));
     }
 
     @Test
@@ -120,9 +124,9 @@ public class CamelJdbcTest {
         RestAssured.given()
                 .get("test/named-parameters/headers-as-parameters")
                 .then()
-                .body(containsString("{ID=1, SPECIES=Camelus dromedarius}"))
+                .body(containsStringIgnoringCase("{ID=1, SPECIES=Camelus dromedarius}"))
                 .and()
-                .body(containsString("{ID=2, SPECIES=Camelus bactrianus}"));
+                .body(containsStringIgnoringCase("{ID=2, SPECIES=Camelus bactrianus}"));
     }
 
     @Test
@@ -130,7 +134,7 @@ public class CamelJdbcTest {
         RestAssured.given()
                 .get("test/named-parameters/headers-as-parameters-map")
                 .then()
-                .body(containsString("{ID=2, SPECIES=Camelus bactrianus}"));
+                .body(containsStringIgnoringCase("{ID=2, SPECIES=Camelus bactrianus}"));
     }
 
     @Test
@@ -163,5 +167,4 @@ public class CamelJdbcTest {
                 .then()
                 .body(equalTo(camelsDbResult));
     }
-
 }
