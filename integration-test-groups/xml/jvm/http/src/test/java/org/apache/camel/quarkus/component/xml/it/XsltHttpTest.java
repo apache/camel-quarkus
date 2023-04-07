22b014977f2cab87ea24d99623bfc6a22dd30e33
Zheng Feng <zh.feng@gmail.com>
2023-04-07 21:48:56 +0800

    Fix #4043 to group xml tests (#4753)

---
+++ integration-test-groups/xml/jvm/http/src/test/java/org/apache/camel/quarkus/component/xml/it/XsltHttpTest.java
@@ -0,0 +1,45 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.quarkus.component.xml.it;
+
+import io.quarkus.test.common.QuarkusTestResource;
+import io.quarkus.test.junit.DisabledOnIntegrationTest;
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+@QuarkusTest
+@QuarkusTestResource(XmlHttpTestResource.class)
+class XsltHttpTest {
+    private static final String BODY = "<mail><subject>Hey</subject><body>Hello world!</body></mail>";
+
+    @Test
+    @DisabledOnIntegrationTest("Generating xslt templates dynamically does not be supported in native mode")
+    public void xsltBean() {
+        final String actual = RestAssured.given()
+                .body(BODY)
+                .post("/xml/xslt-http")
+                .then()
+                .statusCode(200)
+                .extract().body().asString().trim().replaceAll(">\\s+<", "><");
+
+        Assertions.assertEquals(
+                "<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?><classpath-xsl subject=\"Hey\"><cheese><mail><subject>Hey</subject><body>Hello world!</body></mail></cheese></classpath-xsl>",
+                actual);
+    }
+}
---
+++ integration-test-groups/xml/native/classpath/src/main/java/org/apache/camel/quarkus/component/xml/it/XsltRouteBuilder.java
@@ -0,0 +1,58 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.quarkus.component.xml.it;
+
+import org.w3c.dom.Document;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.support.builder.Namespaces;
+import org.apache.xpath.XPathAPI;
+
+public class XsltRouteBuilder extends RouteBuilder {
+    public static final String DIRECT_HTML_TRANSFORM = "direct:html-transform";
+    public static final String DIRECT_HTML_TO_TEXT = "direct:html-to-text";
+    public static final String DIRECT_XML_CBR = "direct:xml-cbr";
+    public static final String DIRECT_XTOKENIZE = "direct:xtokenize";
+
+    @Override
+    public void configure() {
+        from(DIRECT_HTML_TRANSFORM)
+                .convertBodyTo(Document.class)
+                .to("xslt:xslt/html-transform.xsl");
+
+        from(DIRECT_HTML_TO_TEXT)
+                .convertBodyTo(Document.class)
+                .to("xslt:xslt/html-to-text.xsl");
+
+        from(DIRECT_XML_CBR)
+                .choice()
+                .when(xpath("//order/country = 'UK'"))
+                .process(exchange -> {
+                    Document body = exchange.getIn().getBody(Document.class);
+                    String country = XPathAPI.eval(body, "//order/country").toString();
+                    exchange.getIn().setBody("Country " + country);
+                })
+                .otherwise()
+                .setBody(constant("Invalid country code"));
+
+        from(DIRECT_XTOKENIZE)
+                .split()
+                .xtokenize("//C:child", new Namespaces("C", "urn:c"))
+                .to("seda:xtokenize-result");
+
+    }
+}
