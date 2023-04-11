febdbf60553da93b6bf39a17f59166fb5affc020
Peter Palaga <ppalaga@redhat.com>
2023-04-11 17:30:22 +0200

    Upgrade to quarkus-cxf 2.0.0.Alpha7

--- extensions/cxf-soap/deployment/src/main/java/org/apache/camel/quarkus/component/cxf/soap/deployment/CxfSoapProcessor.java
+++ extensions/cxf-soap/deployment/src/main/java/org/apache/camel/quarkus/component/cxf/soap/deployment/CxfSoapProcessor.java
@@ -16,9 +16,17 @@
  */
 package org.apache.camel.quarkus.component.cxf.soap.deployment;
 
+import java.lang.reflect.Modifier;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.function.Predicate;
 import java.util.stream.Stream;
 
+import io.quarkiverse.cxf.deployment.CxfDeploymentUtils;
+import io.quarkiverse.cxf.deployment.CxfEndpointImplementationBuildItem;
 import io.quarkiverse.cxf.deployment.CxfRouteRegistrationRequestorBuildItem;
+import io.quarkiverse.cxf.deployment.ServiceSeiBuildItem;
 import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.builditem.CombinedIndexBuildItem;
@@ -26,6 +34,9 @@ import io.quarkus.deployment.builditem.FeatureBuildItem;
 import io.quarkus.deployment.builditem.SystemPropertyBuildItem;
 import io.quarkus.deployment.builditem.nativeimage.ReflectiveClassBuildItem;
 import io.quarkus.deployment.builditem.nativeimage.ReflectiveMethodBuildItem;
+import org.apache.camel.quarkus.core.deployment.util.PathFilter;
+import org.apache.camel.quarkus.core.deployment.util.PathFilter.Builder;
+import org.jboss.jandex.ClassInfo;
 import org.jboss.jandex.DotName;
 import org.jboss.jandex.IndexView;
 
@@ -51,8 +62,7 @@ class CxfSoapProcessor {
 
         IndexView index = combinedIndex.getIndex();
 
-        Stream.of(
-                "org.apache.wss4j.dom.handler.WSHandler") // can we remove this?
+        Stream.of("org.apache.wss4j.dom.handler.WSHandler") // can we remove this?
                 .map(DotName::createSimple)
                 .flatMap(dotName -> index.getAllKnownSubclasses(dotName).stream())
                 .map(classInfo -> classInfo.name().toString())
@@ -64,8 +74,71 @@ class CxfSoapProcessor {
 
     }
 
+    /**
+     * {@code quarkus-cxf} takes care for generating ancillary classes only for services having an implementation
+     * annotated with <code>@WebService</code> or similar. That does not catch Camel service implementations. So we need
+     * to pass a {@link ServiceSeiBuildItem} holding a service interface to {@code quarkus-cxf} ourselves. That's enough
+     * for {@code quarkus-cxf} to generate the necessary ancillary classes for us.
+     *
+     * @param combinedIndex
+     * @param endpointImplementations
+     * @param serviceSeis
+     */
+    @BuildStep
+    void serviceSeis(
+            CxfBuildTimeConfig cxfBuildTimeConfig,
+            CombinedIndexBuildItem combinedIndex,
+            List<CxfEndpointImplementationBuildItem> endpointImplementations,
+            BuildProducer<ServiceSeiBuildItem> serviceSeis) {
+
+        final Builder b = new PathFilter.Builder();
+        cxfBuildTimeConfig.classGeneration.excludePatterns
+                .stream()
+                .map(pattern -> pattern.replace('.', '/'))
+                .forEach(b::include);
+        final Predicate<DotName> seiFilter = b.build().asDotNamePredicate().negate();
+
+        final Set<String> alreadyRegisteredInterfaces = new LinkedHashSet<>();
+        IndexView index = combinedIndex.getIndex();
+        endpointImplementations.stream()
+                .map(CxfEndpointImplementationBuildItem::getImplementor)
+                .forEach(impl -> {
+                    walkParents(index, DotName.createSimple(impl), alreadyRegisteredInterfaces);
+                });
+
+        CxfDeploymentUtils.webServiceAnnotations(index)
+                .map(annotation -> annotation.target().asClass())
+                .filter(wsClassInfo -> Modifier.isInterface(wsClassInfo.flags()))
+                .map(wsClassInfo -> wsClassInfo.name())
+                .filter(seiFilter)
+                .map(DotName::toString)
+                .filter(intf -> !alreadyRegisteredInterfaces.contains(intf))
+                .map(ServiceSeiBuildItem::new)
+                .forEach(serviceSeis::produce);
+
+    }
+
+    static void walkParents(IndexView index, DotName className, Set<String> alreadyRegisteredInterfaces) {
+        if (className.toString().startsWith("java.")) {
+            /* java.* classes incl. java.lang.Object are definitely not something we look for */
+            return;
+        }
+        final ClassInfo cl = index.getClassByName(className);
+        if (cl == null) {
+            throw new IllegalStateException("Failed to look up " + className + " in Jandex");
+        }
+        if (Modifier.isInterface(cl.flags())) {
+            alreadyRegisteredInterfaces.add(className.toString());
+        }
+        if (cl.superName() != null) {
+            walkParents(index, cl.superName(), alreadyRegisteredInterfaces);
+        }
+        cl.interfaceNames().stream().forEach(intf -> walkParents(index, intf, alreadyRegisteredInterfaces));
+    }
+
     @BuildStep
     CxfRouteRegistrationRequestorBuildItem requestCxfRouteRegistration() {
         return new CxfRouteRegistrationRequestorBuildItem(FEATURE);
     }
+
 }
