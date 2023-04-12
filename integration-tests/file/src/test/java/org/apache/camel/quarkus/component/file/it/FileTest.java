582225d7b63cf9c3212cba97f5d4a104258e03b7
aldettinger <aldettinger@gmail.com>
2023-04-12 16:57:02 +0200

    file: disabling idempotent test in order to experiment around flakiness #3584

--- integration-tests/file/src/test/java/org/apache/camel/quarkus/component/file/it/FileTest.java
+++ integration-tests/file/src/test/java/org/apache/camel/quarkus/component/file/it/FileTest.java
@@ -37,6 +37,7 @@ import io.restassured.response.ValidatableResponse;
 import org.apache.camel.quarkus.core.util.FileUtils;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.apache.camel.quarkus.component.file.it.FileResource.CONSUME_BATCH;
@@ -111,6 +112,7 @@ class FileTest {
         });
     }
 
+    @Disabled("Disabling as an experiment in the context of CAMEL-QUARKUS-3584. Let's check whether filter and idempotent are sort of polluting each other.")
     @Test
     public void idempotent() throws IOException {
         // Create a new file
