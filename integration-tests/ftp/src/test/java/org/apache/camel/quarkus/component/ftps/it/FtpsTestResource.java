361fad985dfa17d743793b734d3a73e685a4e44f
James Netherton <jamesnetherton@gmail.com>
2023-05-15 15:29:16 +0100

    Align org.apache.ftpserver test dependency versions with Camel

--- integration-tests/ftp/src/test/java/org/apache/camel/quarkus/component/ftps/it/FtpsTestResource.java
+++ integration-tests/ftp/src/test/java/org/apache/camel/quarkus/component/ftps/it/FtpsTestResource.java
@@ -16,7 +16,12 @@
  */
 package org.apache.camel.quarkus.component.ftps.it;
 
-import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Map;
+import java.util.Objects;
 
 import org.apache.camel.quarkus.component.ftp.it.FtpTestResource;
 import org.apache.ftpserver.listener.ListenerFactory;
@@ -24,20 +29,45 @@ import org.apache.ftpserver.ssl.SslConfigurationFactory;
 
 public class FtpsTestResource extends FtpTestResource {
 
+    private Path keystoreFilePath;
+
     public FtpsTestResource() {
         super("ftps");
     }
 
     @Override
+    public Map<String, String> start() {
+        try (InputStream stream = Thread.currentThread().getContextClassLoader().getResourceAsStream("server.jks")) {
+            Objects.requireNonNull(stream, "FTP keystore file server.jks could not be loaded");
+            keystoreFilePath = Files.createTempFile("camel-ftps-keystore", "jks");
+            Files.write(keystoreFilePath, stream.readAllBytes());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return super.start();
+    }
+
+    @Override
     protected ListenerFactory createListenerFactory(int port) {
         SslConfigurationFactory sslConfigFactory = new SslConfigurationFactory();
-        sslConfigFactory.setKeystoreFile(new File("server.jks"));
+        sslConfigFactory.setKeystoreFile(keystoreFilePath.toFile());
         sslConfigFactory.setKeystoreType("PKCS12");
         sslConfigFactory.setKeystorePassword("password");
         sslConfigFactory.setKeyPassword("password");
+        sslConfigFactory.setSslProtocol("TLSv1.3");
 
         ListenerFactory factory = super.createListenerFactory(port);
         factory.setSslConfiguration(sslConfigFactory.createSslConfiguration());
         return factory;
+    }
+
+    @Override
+    public void stop() {
+        super.stop();
+        try {
+            Files.deleteIfExists(keystoreFilePath);
+        } catch (IOException e) {
+            // Ignored
+        }
     }
 }
