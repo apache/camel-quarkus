b481e4c203a1cfd4e22bdc6fdd46b15ce8a05046
James Netherton <jamesnetherton@gmail.com>
2023-05-26 15:27:03 +0100

    Disable DebeziumMysqlIT & DebeziumSqlserverIT due to #4638

--- integration-tests/debezium/src/test/java/org/apache/camel/quarkus/component/debezium/common/it/sqlserver/DebeziumSqlserverIT.java
+++ integration-tests/debezium/src/test/java/org/apache/camel/quarkus/component/debezium/common/it/sqlserver/DebeziumSqlserverIT.java
@@ -17,7 +17,9 @@
 package org.apache.camel.quarkus.component.debezium.common.it.sqlserver;
 
 import io.quarkus.test.junit.QuarkusIntegrationTest;
+import org.junit.jupiter.api.Disabled;
 
+@Disabled("https://github.com/apache/camel-quarkus/issues/4638")
 @QuarkusIntegrationTest
 class DebeziumSqlserverIT extends DebeziumSqlserverTest {
 
