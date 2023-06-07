c9ea1c17eebb85fc5657b13c1e236becff262f42
James Netherton <jamesnetherton@gmail.com>
2023-06-07 10:08:32 +0100

    Add test coverage for MDC logging
    Fixes #4947

--- integration-test-groups/foundation/log/src/main/java/org/apache/camel/quarkus/component/log/it/LogResource.java
+++ integration-test-groups/foundation/log/src/main/java/org/apache/camel/quarkus/component/log/it/LogResource.java
@@ -16,19 +16,29 @@
  */
 package org.apache.camel.quarkus.component.log.it;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.Optional;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.event.Observes;
 import jakarta.inject.Inject;
+import jakarta.inject.Named;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.MediaType;
+import org.apache.camel.Exchange;
 import org.apache.camel.ProducerTemplate;
+import org.apache.camel.component.log.LogComponent;
 import org.apache.camel.quarkus.main.events.AfterStart;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 @ApplicationScoped
+@Path("/log")
 public class LogResource {
-
-    public static final String LOG_MESSAGE = "Lets's fool io.quarkus.test.common.LauncherUtil.CaptureListeningDataReader: Listening on: http://0.0.0.0:";
     @Inject
     ProducerTemplate producerTemplate;
 
@@ -38,13 +48,35 @@ public class LogResource {
     Optional<Integer> httpPort;
 
     private int getEffectivePort() {
-        final boolean isNativeMode = "executable".equals(System.getProperty("org.graalvm.nativeimage.kind"));
-        Optional<Integer> portSource = isNativeMode ? httpPort : httpTestPort;
-        return portSource.isPresent() ? portSource.get().intValue() : 0;
+        Optional<Integer> portSource = LogUtils.isNativeMode() ? httpPort : httpTestPort;
+        return portSource.orElse(0);
     }
 
-    public void info(@Observes AfterStart event) {
-        producerTemplate.sendBody("log:foo-topic", LOG_MESSAGE + getEffectivePort());
+    public void afterApplicationStartup(@Observes AfterStart event) {
+        try {
+            Files.writeString(LogUtils.resolveQuarkusLogPath(),
+                    "Listening on: http://0.0.0.0:" + getEffectivePort() + System.lineSeparator());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
+    @jakarta.enterprise.inject.Produces
+    @Named("mdcLog")
+    LogComponent logComponent() {
+        // Use a new LogComponent instance because when we run tests 'grouped',
+        // integration-test-customized-log-component introduces a custom exchangeFormatter
+        // which messes up the log output expected by the MDC tests
+        LogComponent logComponent = new LogComponent();
+        logComponent.setCamelContext(producerTemplate.getCamelContext());
+        return logComponent;
+    }
+
+    @POST
+    @Consumes(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.TEXT_PLAIN)
+    public String logMessage(@QueryParam("endpointUri") String endpointUri, String message) {
+        Exchange result = producerTemplate.request(endpointUri, exchange -> exchange.getMessage().setBody(message));
+        return result.getExchangeId();
+    }
 }
