bc4a1599b41f46c138b0997b5477a1cd21214429
Zineb Bendhiba <bendhiba.zineb@gmail.com>
2023-08-31 09:57:50 +0200

    Disabling temporary Opentelemetry native test

--- integration-tests/opentelemetry/src/test/java/org/apache/camel/quarkus/component/opentelemetry/it/OpenTelemetryIT.java
+++ integration-tests/opentelemetry/src/test/java/org/apache/camel/quarkus/component/opentelemetry/it/OpenTelemetryIT.java
@@ -17,8 +17,10 @@
 package org.apache.camel.quarkus.component.opentelemetry.it;
 
 import io.quarkus.test.junit.QuarkusIntegrationTest;
+import org.junit.jupiter.api.Disabled;
 
 @QuarkusIntegrationTest
+@Disabled //https://github.com/apache/camel-quarkus/issues/5240
 class OpenTelemetryIT extends OpenTelemetryTest {
 
 }
