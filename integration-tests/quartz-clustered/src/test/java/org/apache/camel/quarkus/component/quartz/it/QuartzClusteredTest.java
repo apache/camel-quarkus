3fa02392692adf75869aa571eb86f542d30a7b6e
James Netherton <jamesnetherton@gmail.com>
2023-09-01 11:24:09 +0100

    Fix resolution of postgres.container.image config property in Quartz Clustered tests
    Fixes #5241

--- integration-tests/quartz-clustered/src/test/java/org/apache/camel/quarkus/component/quartz/it/QuartzClusteredTest.java
+++ integration-tests/quartz-clustered/src/test/java/org/apache/camel/quarkus/component/quartz/it/QuartzClusteredTest.java
@@ -22,6 +22,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.concurrent.TimeUnit;
 
+import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.RestAssured;
 import org.apache.camel.quarkus.test.support.process.QuarkusProcessExecutor;
@@ -33,6 +34,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.zeroturnaround.exec.StartedProcess;
 
+@QuarkusTestResource(QuartzClusteredTestResource.class)
 @QuarkusTest
 class QuartzClusteredTest {
 
