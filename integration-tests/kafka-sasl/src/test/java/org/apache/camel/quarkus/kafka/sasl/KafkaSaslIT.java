6d77553a7a3a9c4744ba66d805f4b33f821cc321
Zheng Feng <zh.feng@gmail.com>
2023-10-11 09:35:04 +0800

    Revert "Disable Kafka SSL tests due to #5381"
    This reverts commit 234f9bdf68cdb316c993279811a2979646d53c59.

--- integration-tests/kafka-sasl/src/test/java/org/apache/camel/quarkus/kafka/sasl/KafkaSaslIT.java
+++ integration-tests/kafka-sasl/src/test/java/org/apache/camel/quarkus/kafka/sasl/KafkaSaslIT.java
@@ -16,10 +16,8 @@
  */
 package org.apache.camel.quarkus.kafka.sasl;
 
-import io.quarkus.test.junit.DisabledOnIntegrationTest;
 import io.quarkus.test.junit.QuarkusIntegrationTest;
 
-@DisabledOnIntegrationTest("https://github.com/apache/camel-quarkus/issues/5381")
 @QuarkusIntegrationTest
 public class KafkaSaslIT extends KafkaSaslBindingTest {
 }
