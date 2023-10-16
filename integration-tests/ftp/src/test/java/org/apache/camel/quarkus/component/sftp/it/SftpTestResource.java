b3691d5a82af65b67244487f7adb62b9d0ab0948
JiriOndrusek <ondrusek.jiri@gmail.com>
2023-10-16 10:35:38 +0200

    Ftp fails in the FIPS because of the not supported key.

--- integration-tests/ftp/src/test/java/org/apache/camel/quarkus/component/sftp/it/SftpTestResource.java
+++ integration-tests/ftp/src/test/java/org/apache/camel/quarkus/component/sftp/it/SftpTestResource.java
@@ -64,7 +64,7 @@ public class SftpTestResource implements QuarkusTestResourceLifecycleManager {
 
             sshServer = SshServer.setUpDefaultServer();
             sshServer.setPort(port);
-            sshServer.setKeyPairProvider(new ClassLoadableResourceKeyPairProvider("hostkey.pem"));
+            sshServer.setKeyPairProvider(new ClassLoadableResourceKeyPairProvider("test.key"));
             sshServer.setSubsystemFactories(Collections.singletonList(new SftpSubsystemFactory()));
             sshServer.setCommandFactory(new ScpCommandFactory());
             sshServer.setPasswordAuthenticator((username, password, session) -> true);
