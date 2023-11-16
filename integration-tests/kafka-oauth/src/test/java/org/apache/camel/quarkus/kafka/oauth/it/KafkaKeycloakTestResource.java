aec25ff1d4aa8f6b211fcef9452d0f6126e5a7cf
James Netherton <jamesnetherton@gmail.com>
2023-11-16 12:11:12 +0000

    Improve container setup for kafka-oauth test

--- integration-tests/kafka-oauth/src/test/java/org/apache/camel/quarkus/kafka/oauth/it/KafkaKeycloakTestResource.java
+++ integration-tests/kafka-oauth/src/test/java/org/apache/camel/quarkus/kafka/oauth/it/KafkaKeycloakTestResource.java
@@ -16,13 +16,19 @@
  */
 package org.apache.camel.quarkus.kafka.oauth.it;
 
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
 
 import io.quarkus.test.common.QuarkusTestResourceLifecycleManager;
+import io.quarkus.test.keycloak.client.KeycloakTestClient;
+import io.quarkus.test.keycloak.server.KeycloakContainer;
 import io.strimzi.test.container.StrimziKafkaContainer;
-import org.apache.camel.quarkus.kafka.oauth.it.container.KeycloakContainer;
+import org.apache.commons.io.IOUtils;
 import org.eclipse.microprofile.config.ConfigProvider;
 import org.jboss.logging.Logger;
 import org.testcontainers.utility.MountableFile;
@@ -34,7 +40,8 @@ import static io.strimzi.test.container.StrimziKafkaContainer.KAFKA_PORT;
  */
 public class KafkaKeycloakTestResource implements QuarkusTestResourceLifecycleManager {
 
-    private static final Logger log = Logger.getLogger(KafkaKeycloakTestResource.class);
+    private static final Logger LOG = Logger.getLogger(KafkaKeycloakTestResource.class);
+    private static final String REALM_JSON = "keycloak/realms/kafka-authz-realm.json";
     private StrimziKafkaContainer kafka;
     private KeycloakContainer keycloak;
 
@@ -46,23 +53,56 @@ public class KafkaKeycloakTestResource implements QuarkusTestResourceLifecycleMa
         keycloak = new KeycloakContainer();
         keycloak.withStartupTimeout(Duration.ofMinutes(5));
         keycloak.start();
-        log.info(keycloak.getLogs());
-        keycloak.createHostsFile();
+        LOG.info(keycloak.getLogs());
+
+        Path realmJson = null;
+        try {
+            URL resource = Thread.currentThread().getContextClassLoader().getResource(REALM_JSON);
+            if (resource == null) {
+                throw new RuntimeException("Unable to load " + REALM_JSON);
+            }
+
+            realmJson = Files.createTempFile("keycloak-auth", ".json");
+            IOUtils.copy(resource, realmJson.toFile());
+
+            KeycloakTestClient client = new KeycloakTestClient(keycloak.getServerUrl());
+            client.createRealmFromPath(realmJson.toAbsolutePath().toString());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (realmJson != null) {
+                try {
+                    Files.deleteIfExists(realmJson);
+                } catch (IOException e) {
+                    // Ignored
+                }
+            }
+        }
 
         //Start kafka container
         String imageName = ConfigProvider.getConfig().getValue("strimzi-kafka.container.image", String.class);
         this.kafka = new StrimziKafkaContainer(imageName)
                 .withBrokerId(1)
-                .withKafkaConfigurationMap(Map.of("listener.security.protocol.map", "JWT:SASL_PLAINTEXT,BROKER1:PLAINTEXT"))
+                .withKafkaConfigurationMap(Map.of("listener.security.protocol.map",
+                        "JWT:SASL_PLAINTEXT,BROKER1:PLAINTEXT",
+                        "listener.name.jwt.oauthbearer.sasl.jaas.config",
+                        getOauthSaslJaasConfig(keycloak.getInternalUrl(), keycloak.getServerUrl()),
+                        "listener.name.jwt.plain.sasl.jaas.config",
+                        getPlainSaslJaasConfig(keycloak.getInternalUrl(), keycloak.getServerUrl())))
                 .withNetworkAliases("kafka")
                 .withServerProperties(MountableFile.forClasspathResource("kafkaServer.properties"))
                 .withBootstrapServers(
                         c -> String.format("JWT://%s:%s", c.getHost(), c.getMappedPort(KAFKA_PORT)));
         this.kafka.start();
-        log.info(this.kafka.getLogs());
+        LOG.info(this.kafka.getLogs());
+
         properties.put("kafka.bootstrap.servers", this.kafka.getBootstrapServers());
         properties.put("camel.component.kafka.brokers", kafka.getBootstrapServers());
-
+        properties.put("camel.component.kafka.security-protocol", "SASL_PLAINTEXT");
+        properties.put("camel.component.kafka.sasl-mechanism", "OAUTHBEARER");
+        properties.put("camel.component.kafka.additional-properties[sasl.login.callback.handler.class]",
+                "io.strimzi.kafka.oauth.client.JaasClientOauthLoginCallbackHandler");
+        properties.put("camel.component.kafka.sasl-jaas-config", getClientSaslJaasConfig(keycloak.getServerUrl()));
         return properties;
     }
 
@@ -74,5 +114,31 @@ public class KafkaKeycloakTestResource implements QuarkusTestResourceLifecycleMa
         if (keycloak != null) {
             keycloak.stop();
         }
+    }
+
+    private String getClientSaslJaasConfig(String keycloakServerUrl) {
+        return "org.apache.kafka.common.security.oauthbearer.OAuthBearerLoginModule required" +
+                " oauth.client.id=\"kafka-client\"" +
+                " oauth.client.secret=\"kafka-client-secret\"" +
+                " oauth.token.endpoint.uri=\"" + keycloakServerUrl + "/realms/kafka-authz/protocol/openid-connect/token\";";
+    }
+
+    private String getPlainSaslJaasConfig(String keycloakInternalUrl, String keycloakServerUrl) {
+        return "'org.apache.kafka.common.security.plain.PlainLoginModule required " +
+                "oauth.jwks.endpoint.uri=\"" + keycloakInternalUrl + "/realms/kafka-authz/protocol/openid-connect/certs\" " +
+                "oauth.valid.issuer.uri=\"" + keycloakServerUrl + "/realms/kafka-authz\" " +
+                "oauth.token.endpoint.uri=\"" + keycloakInternalUrl + "/realms/kafka-authz/protocol/openid-connect/token\" " +
+                "oauth.client.id=\"kafka\" " +
+                "oauth.client.secret=\"kafka-secret\" " +
+                "unsecuredLoginStringClaim_sub=\"admin\";'";
+    }
+
+    private String getOauthSaslJaasConfig(String keycloakInternalUrl, String keycloakServerUrl) {
+        return "'org.apache.kafka.common.security.oauthbearer.OAuthBearerLoginModule required " +
+                "oauth.jwks.endpoint.uri=\"" + keycloakInternalUrl + "/realms/kafka-authz/protocol/openid-connect/certs\" " +
+                "oauth.valid.issuer.uri=\"" + keycloakServerUrl + "/realms/kafka-authz\" " +
+                "oauth.token.endpoint.uri=\"" + keycloakInternalUrl + "/realms/kafka-authz/protocol/openid-connect/token\" " +
+                "oauth.client.id=\"kafka\" " +
+                "oauth.client.secret=\"kafka-secret\";'";
     }
 }
