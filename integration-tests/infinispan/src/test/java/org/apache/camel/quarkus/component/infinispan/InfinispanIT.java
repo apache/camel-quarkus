4551a0da9919ce480a4989a588f46047e1f99977
James Netherton <jamesnetherton@gmail.com>
2023-12-08 10:50:53 +0000

    Move Infinispan test classes to the correct package

--- integration-tests/infinispan/src/test/java/org/apache/camel/quarkus/component/infinispan/InfinispanIT.java
+++ integration-tests/infinispan/src/test/java/org/apache/camel/quarkus/component/infinispan/InfinispanIT.java
@@ -14,6 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package org.apache.camel.quarkus.component.infinispan;
 
 import io.quarkus.test.junit.QuarkusIntegrationTest;
 
