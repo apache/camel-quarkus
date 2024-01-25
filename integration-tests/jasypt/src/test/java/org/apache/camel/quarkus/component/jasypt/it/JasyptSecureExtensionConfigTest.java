086460179ce2ef70ea7700f81ea8ee0e9c3a8c1a
James Netherton <jamesnetherton@gmail.com>
2024-01-25 08:24:10 +0000

    Disable JasyptSecureExtensionConfigTest.secureDirectComponentTimeout due to #5675

--- integration-tests/jasypt/src/test/java/org/apache/camel/quarkus/component/jasypt/it/JasyptSecureExtensionConfigTest.java
+++ integration-tests/jasypt/src/test/java/org/apache/camel/quarkus/component/jasypt/it/JasyptSecureExtensionConfigTest.java
@@ -19,6 +19,7 @@ package org.apache.camel.quarkus.component.jasypt.it;
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.junit.TestProfile;
 import io.restassured.RestAssured;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.Matchers.is;
@@ -35,6 +36,7 @@ class JasyptSecureExtensionConfigTest {
                 .body(is("camel"));
     }
 
+    @Disabled("https://github.com/apache/camel-quarkus/issues/5675")
     @Test
     void secureDirectComponentTimeout() throws InterruptedException {
         RestAssured.given()
