1cbc957bb8992735f60f660477bf5ec74c2efc94
JiriOndrusek <ondrusek.jiri@gmail.com>
2024-01-26 13:42:45 +0100

    CXF-SOAP: Cover possible regression prior CXF fix causing hang of the client

--- integration-test-groups/cxf-soap/cxf-soap-ws-security-client/src/test/java/org/apache/camel/quarkus/component/cxf/soap/wss/client/it/CxfSoapWssClientTest.java
+++ integration-test-groups/cxf-soap/cxf-soap-ws-security-client/src/test/java/org/apache/camel/quarkus/component/cxf/soap/wss/client/it/CxfSoapWssClientTest.java
@@ -17,18 +17,30 @@
 package org.apache.camel.quarkus.component.cxf.soap.wss.client.it;
 
 import java.io.IOException;
+import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
 import io.quarkus.test.common.QuarkusTestResource;
+import io.quarkus.test.junit.DisabledOnIntegrationTest;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.RestAssured;
+import jakarta.xml.ws.BindingProvider;
+import jakarta.xml.ws.Service;
+import jakarta.xml.ws.soap.SOAPFaultException;
+import org.apache.camel.quarkus.components.cxf.soap.wss.client.helloworld.SayHelloService;
+import org.apache.camel.quarkus.components.cxf.soap.wss.client.helloworld.SayHelloWrongWS;
+import org.apache.cxf.ws.security.SecurityConstants;
 import org.eclipse.microprofile.config.ConfigProvider;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.Test;
+import org.testcontainers.shaded.org.awaitility.Awaitility;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
@@ -86,6 +98,56 @@ class CxfSoapWssClientTest {
             Assertions.fail("The static WSDL copy in " + staticCopyPath
                     + " went out of sync with the WSDL served by the container. The content was updated by the test, you just need to review and commit the changes.");
         }
-
     }
+
+    /**
+     * In case of the wrong security configuration and before this
+     * <a href="https://github.com/jboss-fuse/cxf/pull/496">fix<a/>
+     * in CXF (can be simulated on camel-quarkus tag 3.0.0-RC2), the client would hang indefinitely.
+     * This tests covers such wrong configuration and verifies that no regression causing indefinite hang is present in
+     * the current code.
+     */
+    @Test
+    @DisabledOnIntegrationTest("Test doesn't have native part, no server for SayHello.wsdl exists")
+    public void testWrongClientNotHanging() {
+
+        Awaitility.await().atMost(30, TimeUnit.SECONDS).pollInterval(10, TimeUnit.SECONDS).until(() -> {
+            try {
+                //always fails because there is no server implementation
+                createSayHelloWrongClient().sayHelloWrong("Sheldon");
+            } catch (SOAPFaultException e) {
+                return "Connection refused".equals(e.getMessage());
+            }
+            //can not happen (client does not work)
+            return false;
+        });
+    }
+
+    SayHelloWrongWS createSayHelloWrongClient() {
+
+        final URL serviceUrl = Thread.currentThread().getContextClassLoader().getResource("wsdl/HelloWorld.wsdl");
+        final Service service = Service.create(serviceUrl, SayHelloService.SERVICE);
+
+        SayHelloWrongWS port = service.getPort(SayHelloWrongWS.class);
+        BindingProvider bp = (BindingProvider) port;
+
+        Map<String, Object> requestContext = bp.getRequestContext();
+
+        //non-existing server url is used and
+        requestContext.put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, "http://localhost:45698/soapservice/sayHelloWrong");
+
+        Properties securityProps = new Properties();
+        securityProps.put("org.apache.wss4j.crypto.provider", "org.apache.wss4j.common.crypto.Merlin");
+        securityProps.put("org.apache.wss4j.crypto.merlin.keystore.type", "pkcs12");
+        securityProps.put("org.apache.wss4j.crypto.merlin.keystore.file", "alice_wrong.jks");
+        securityProps.put("org.apache.wss4j.crypto.merlin.keystore.password", "password");
+        securityProps.put("org.apache.wss4j.crypto.merlin.keystore.alias", "alice_wrong");
+        securityProps.put("org.apache.wss4j.crypto.merlin.keystore.private.password", "password");
+        securityProps.put("org.apache.wss4j.crypto.merlin.keystore.private.caching", "true");
+
+        requestContext.put(SecurityConstants.SIGNATURE_PROPERTIES, securityProps);
+
+        return port;
+    }
+
 }
