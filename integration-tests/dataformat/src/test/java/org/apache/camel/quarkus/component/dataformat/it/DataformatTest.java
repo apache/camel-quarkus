a418f62936642676a77d31f2bbc108b17a84979a
James Netherton <jamesnetherton@gmail.com>
2024-06-26 12:49:06 +0100

    Remove iCal UTC timezone format appendage or 'Z' to the timestamp

--- integration-tests/dataformat/src/test/java/org/apache/camel/quarkus/component/dataformat/it/DataformatTest.java
+++ integration-tests/dataformat/src/test/java/org/apache/camel/quarkus/component/dataformat/it/DataformatTest.java
@@ -98,11 +98,7 @@ class DataformatTest {
     }
 
     static String toFormatedLocalDateTime(ZonedDateTime zonedDateTime) {
-        String result = zonedDateTime.format(DateTimeFormatter.ofPattern("yyyyMMdd'T'hhmmss"));
-        if (zonedDateTime.getZone().getId().equals("Etc/UTC")) {
-            result += "Z";
-        }
-        return result;
+        return zonedDateTime.format(DateTimeFormatter.ofPattern("yyyyMMdd'T'hhmmss"));
     }
 
 }
