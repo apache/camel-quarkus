d6b72c548497f50b7eb8384d49c86855d8edfb86
Peter Palaga <ppalaga@redhat.com>
2024-07-30 15:26:49 +0200

    cxf-soap SSL tests fail with Quarkus CXF 3.13.0 fix #6294

--- integration-test-groups/cxf-soap/cxf-soap-ssl/src/test/java/org/apache/camel/quarkus/component/cxf/soap/ssl/it/CxfSoapGlobalUntrustedSslTest.java
+++ integration-test-groups/cxf-soap/cxf-soap-ssl/src/test/java/org/apache/camel/quarkus/component/cxf/soap/ssl/it/CxfSoapGlobalUntrustedSslTest.java
@@ -20,12 +20,10 @@ import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.junit.QuarkusTestProfile;
 import io.quarkus.test.junit.TestProfile;
 import io.restassured.RestAssured;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.Matchers.containsString;
 
-@Disabled("https://github.com/apache/camel-quarkus/issues/6294")
 @QuarkusTest
 @TestProfile(CxfSoapGlobalUntrustedSslTest.class)
 public class CxfSoapGlobalUntrustedSslTest implements QuarkusTestProfile {
