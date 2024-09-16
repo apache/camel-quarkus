87aa26fb574be7f9a188d775d9e362a1ab0d056a
James Netherton <jamesnetherton@gmail.com>
2024-09-16 07:35:29 +0100

    Discover Datasonnet libraries at build time
    Fixes #6425

--- extensions/datasonnet/deployment/src/main/java/org/apache/camel/quarkus/component/datasonnet/deployment/DatasonnetProcessor.java
+++ extensions/datasonnet/deployment/src/main/java/org/apache/camel/quarkus/component/datasonnet/deployment/DatasonnetProcessor.java
@@ -16,17 +16,29 @@
  */
 package org.apache.camel.quarkus.component.datasonnet.deployment;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Stream;
 
+import io.quarkus.deployment.ApplicationArchive;
 import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
+import io.quarkus.deployment.annotations.ExecutionTime;
+import io.quarkus.deployment.annotations.Record;
+import io.quarkus.deployment.builditem.ApplicationArchivesBuildItem;
 import io.quarkus.deployment.builditem.FeatureBuildItem;
 import io.quarkus.deployment.builditem.IndexDependencyBuildItem;
 import io.quarkus.deployment.builditem.nativeimage.NativeImageResourceBuildItem;
 import io.quarkus.deployment.builditem.nativeimage.ReflectiveClassBuildItem;
 import io.quarkus.deployment.builditem.nativeimage.RuntimeInitializedClassBuildItem;
+import org.apache.camel.language.datasonnet.DatasonnetLanguage;
+import org.apache.camel.quarkus.component.datasonnet.DatasonnetLanguageRecorder;
+import org.apache.camel.quarkus.core.deployment.spi.CamelBeanBuildItem;
 
 class DatasonnetProcessor {
 
@@ -45,6 +57,38 @@ class DatasonnetProcessor {
     }
 
     @BuildStep
+    DatasonnetLibrariesBuildItem discoverDatasonnetLibraries(ApplicationArchivesBuildItem applicationArchivesBuildItem) {
+        Map<String, String> datasonnetLibraries = new HashMap<>();
+        for (ApplicationArchive archive : applicationArchivesBuildItem.getAllApplicationArchives()) {
+            for (Path root : archive.getRootDirectories()) {
+                try (Stream<Path> files = Files.walk(root)) {
+                    files.filter(Files::isRegularFile)
+                            .map(root::relativize)
+                            .filter(resource -> resource.toString().endsWith(".libsonnet"))
+                            .forEach(resource -> {
+                                try {
+                                    datasonnetLibraries.put(resource.toString(), Files.readString(resource));
+                                } catch (IOException e) {
+                                    throw new RuntimeException(e);
+                                }
+                            });
+                } catch (IOException e) {
+                    throw new RuntimeException("Could not walk " + root, e);
+                }
+            }
+        }
+        return new DatasonnetLibrariesBuildItem(datasonnetLibraries);
+    }
+
+    @Record(ExecutionTime.STATIC_INIT)
+    @BuildStep
+    CamelBeanBuildItem createDatasonnetLanguage(DatasonnetLibrariesBuildItem datasonnetLibraries,
+            DatasonnetLanguageRecorder recorder) {
+        return new CamelBeanBuildItem("datasonnet", DatasonnetLanguage.class.getName(),
+                recorder.createDatasonnetLanguage(datasonnetLibraries.getLibraries()));
+    }
+
+    @BuildStep
     void runtimeInitializedClasses(BuildProducer<RuntimeInitializedClassBuildItem> runtimeInitializedClass) {
         Stream.of("scala.util.Random$")
                 .map(RuntimeInitializedClassBuildItem::new)
@@ -52,8 +96,13 @@ class DatasonnetProcessor {
     }
 
     @BuildStep
-    void process(BuildProducer<NativeImageResourceBuildItem> resource) {
-        resource.produce(new NativeImageResourceBuildItem("util.libsonnet"));
+    void nativeImageResources(
+            DatasonnetLibrariesBuildItem datasonnetLibraries,
+            BuildProducer<NativeImageResourceBuildItem> nativeImageResource) {
+        datasonnetLibraries.getLibraryPaths()
+                .stream()
+                .map(NativeImageResourceBuildItem::new)
+                .forEach(nativeImageResource::produce);
     }
 
     @BuildStep
