65f075ec6bbbe908e7e51da18c36cdc192c28436
JiriOndrusek <ondrusek.jiri@gmail.com>
2024-10-02 11:56:38 +0200

    Fixes #6576 - test coverage for smb producer

--- integration-tests/smb/src/main/java/org/apache/camel/quarkus/component/smb/it/SmbRoute.java
+++ integration-tests/smb/src/main/java/org/apache/camel/quarkus/component/smb/it/SmbRoute.java
@@ -16,13 +16,69 @@
  */
 package org.apache.camel.quarkus.component.smb.it;
 
-import org.apache.camel.builder.RouteBuilder;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CopyOnWriteArrayList;
 
+import com.hierynomus.smbj.share.File;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.inject.Produces;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+import jakarta.inject.Singleton;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.smb.SmbConstants;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+@ApplicationScoped
 public class SmbRoute extends RouteBuilder {
+
+    @ConfigProperty(name = "smb.host")
+    String host;
+
+    @ConfigProperty(name = "smb.port")
+    String port;
+
+    @ConfigProperty(name = "smb.username")
+    String username;
+
+    @ConfigProperty(name = "smb.password")
+    String password;
+
+    @ConfigProperty(name = "smb.share")
+    String share;
+
+    @Inject
+    @Named("smbReceivedMsgs")
+    List<Map<String, String>> receivedContents;
 
     @Override
     public void configure() throws Exception {
-        from("smb:{{smb.host}}:{{smb.port}}/{{smb.share}}?username={{smb.username}}&password={{smb.password}}&path=/&repeatCount=1")
+        from("smb:{{smb.host}}:{{smb.port}}/{{smb.share}}?username={{smb.username}}&password={{smb.password}}&path=/&repeatCount=1&searchPattern=*.txt")
                 .to("mock:result");
+
+        from("direct:send")
+                .toF("smb:%s:%s/%s?username=%s&password=%s&path=/", host, port, share, username, password);
+
+        from("smb:{{smb.host}}:{{smb.port}}/{{smb.share}}?username={{smb.username}}&password={{smb.password}}&path=/&searchPattern=*.tx1")
+                .process(e -> {
+                    receivedContents.add(Map.of(
+                            "path", e.getIn().getBody(File.class).getPath(),
+                            "content", new String(e.getIn().getBody(InputStream.class).readAllBytes(), "UTF-8"),
+                            SmbConstants.SMB_FILE_PATH, e.getIn().getHeader(SmbConstants.SMB_FILE_PATH, String.class),
+                            SmbConstants.SMB_UNC_PATH, e.getIn().getHeader(SmbConstants.SMB_UNC_PATH, String.class)));
+                });
     }
+
+    static class Producers {
+
+        @Singleton
+        @Produces
+        @Named("smbReceivedMsgs")
+        List<Map<String, String>> smbReceivedMsgs() {
+            return new CopyOnWriteArrayList<>();
+        }
+    }
+
 }
