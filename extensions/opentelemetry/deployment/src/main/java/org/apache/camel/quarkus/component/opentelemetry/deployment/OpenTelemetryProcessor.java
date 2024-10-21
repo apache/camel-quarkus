876c9a2ede126a4ef9dc3d929d53b1b835a7d7ff
James Netherton <jamesnetherton@gmail.com>
2024-10-21 14:53:17 +0100

    Work around Camel OpenTelemetry thread factory services not being compatible with Quarkus OpenTelemetry

--- extensions/opentelemetry/deployment/src/main/java/org/apache/camel/quarkus/component/opentelemetry/deployment/OpenTelemetryProcessor.java
+++ extensions/opentelemetry/deployment/src/main/java/org/apache/camel/quarkus/component/opentelemetry/deployment/OpenTelemetryProcessor.java
@@ -16,11 +16,19 @@
  */
 package org.apache.camel.quarkus.component.opentelemetry.deployment;
 
+import java.nio.file.Paths;
+import java.util.Map;
+
 import io.quarkus.arc.deployment.AdditionalBeanBuildItem;
+import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.builditem.FeatureBuildItem;
 import io.quarkus.opentelemetry.deployment.tracing.TracerEnabled;
 import org.apache.camel.quarkus.component.opentelemetry.OpenTelemetryTracerProducer;
+import org.apache.camel.quarkus.core.deployment.spi.CamelServiceBuildItem;
+import org.apache.camel.quarkus.core.deployment.spi.CamelServiceDestination;
+import org.apache.camel.quarkus.core.deployment.spi.CamelServicePatternBuildItem;
+import org.apache.camel.spi.FactoryFinder;
 
 class OpenTelemetryProcessor {
 
@@ -37,5 +45,26 @@ class OpenTelemetryProcessor {
                 .setUnremovable()
                 .addBeanClass(OpenTelemetryTracerProducer.class)
                 .build();
+    }
+
+    // TODO: Remove this: https://github.com/apache/camel-quarkus/issues/6669
+    @BuildStep
+    void overrideCamelOpenTelemetryThreadPoolServices(
+            BuildProducer<CamelServicePatternBuildItem> camelServicePattern,
+            BuildProducer<CamelServiceBuildItem> camelService) {
+
+        Map.of("thread-pool-factory", "OpenTelemetryInstrumentedThreadPoolFactory",
+                "thread-factory-listener", "OpenTelemetryInstrumentedThreadFactoryListener")
+                .forEach((serviceName, type) -> {
+                    String servicePath = FactoryFinder.DEFAULT_PATH + serviceName;
+                    // Disable broken original service
+                    camelServicePattern
+                            .produce(new CamelServicePatternBuildItem(CamelServiceDestination.DISCOVERY, false, servicePath));
+
+                    // Replace with working
+                    camelService.produce(new CamelServiceBuildItem(Paths.get(servicePath),
+                            "org.apache.camel.quarkus.component.opentelemetry.patch.%s".formatted(type)));
+                });
+
     }
 }
