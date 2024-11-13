595740e6e563722351b551f71bdfb8cb31930cb2
James Netherton <jamesnetherton@gmail.com>
2024-11-13 11:04:48 +0000

    Remove dependency on optional dependency org.brotli:dec
    Fixes #6777

--- extensions-support/httpclient5/deployment/src/main/java/org/apache/camel/quarkus/support/httpclient5/deployment/HttpClient5Processor.java
+++ extensions-support/httpclient5/deployment/src/main/java/org/apache/camel/quarkus/support/httpclient5/deployment/HttpClient5Processor.java
@@ -16,12 +16,24 @@
  */
 package org.apache.camel.quarkus.support.httpclient5.deployment;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Modifier;
+
+import io.quarkus.deployment.GeneratedClassGizmoAdaptor;
 import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
+import io.quarkus.deployment.builditem.GeneratedClassBuildItem;
 import io.quarkus.deployment.builditem.nativeimage.NativeImageResourceBuildItem;
 import io.quarkus.deployment.builditem.nativeimage.RuntimeInitializedClassBuildItem;
+import io.quarkus.deployment.pkg.steps.NativeOrNativeSourcesBuild;
+import io.quarkus.gizmo.ClassCreator;
+import io.quarkus.gizmo.MethodCreator;
+import io.quarkus.gizmo.MethodDescriptor;
+import org.apache.camel.quarkus.support.httpclient5.graal.BrotliAbsentBooleanSupplier;
 
 class HttpClient5Processor {
+    private static final String BROTLI_INPUT_STREAM_CLASS_NAME = "org.brotli.dec.BrotliInputStream";
     private static final String NTLM_ENGINE_IMPL = "org.apache.hc.client5.http.impl.auth.NTLMEngineImpl";
 
     @BuildStep
@@ -33,5 +45,55 @@ class HttpClient5Processor {
     @BuildStep
     void runtimeInitializedClasses(BuildProducer<RuntimeInitializedClassBuildItem> runtimeInitializedClasses) {
         runtimeInitializedClasses.produce(new RuntimeInitializedClassBuildItem(NTLM_ENGINE_IMPL));
+    }
+
+    @BuildStep(onlyIf = { NativeOrNativeSourcesBuild.class, BrotliAbsentBooleanSupplier.class })
+    void generateBrotliInputStreamClass(BuildProducer<GeneratedClassBuildItem> generatedClass) {
+        try (ClassCreator classCreator = ClassCreator.builder()
+                .className(BROTLI_INPUT_STREAM_CLASS_NAME)
+                .superClass(InputStream.class)
+                .classOutput(new GeneratedClassGizmoAdaptor(generatedClass, false))
+                .build()) {
+
+            /*
+             * Creates a simplified impl of BrotliInputStream to satisfy the native compiler:
+             *
+             * public class BrotliInputStream extends InputStream {
+             *   public BrotliInputStream() {
+             *   }
+             *
+             *   public BrotliInputStream(InputStream stream) {
+             *   }
+             *
+             *   public int read() {
+             *     throw new UnsupportedOperationException();
+             *   }
+             * }
+             */
+
+            try (MethodCreator defaultConstructor = classCreator.getMethodCreator("<init>", void.class)) {
+                defaultConstructor.setModifiers(Modifier.PUBLIC);
+                defaultConstructor.invokeSpecialMethod(
+                        MethodDescriptor.ofMethod(BROTLI_INPUT_STREAM_CLASS_NAME, "<init>", void.class),
+                        defaultConstructor.getThis());
+                defaultConstructor.returnValue(null);
+            }
+
+            try (MethodCreator constructorWithInputStreamArg = classCreator.getMethodCreator("<init>", void.class,
+                    InputStream.class)) {
+                constructorWithInputStreamArg.setModifiers(Modifier.PUBLIC);
+                constructorWithInputStreamArg.invokeSpecialMethod(
+                        MethodDescriptor.ofMethod(BROTLI_INPUT_STREAM_CLASS_NAME, "<init>", void.class),
+                        constructorWithInputStreamArg.getThis());
+                constructorWithInputStreamArg.returnValue(null);
+            }
+
+            try (MethodCreator readMethod = classCreator.getMethodCreator("read", int.class)) {
+                readMethod.setModifiers(Modifier.PUBLIC);
+                readMethod.addException(IOException.class);
+                readMethod.throwException(UnsupportedOperationException.class,
+                        "Cannot read from BrotliInputStream. Add org.brotli:dec to the application classpath");
+            }
+        }
     }
 }
