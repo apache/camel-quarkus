13f42d97ef065de737768194fd7facb4076b0536
James Netherton <jamesnetherton@gmail.com>
2025-01-09 12:55:48 +0000

    Switch from docker.io to gcr.io for container images

--- integration-tests/paho/src/test/java/org/apache/camel/quarkus/component/paho/it/PahoTestResource.java
+++ integration-tests/paho/src/test/java/org/apache/camel/quarkus/component/paho/it/PahoTestResource.java
@@ -22,6 +22,7 @@ import java.util.Map;
 import io.quarkus.test.common.QuarkusTestResourceLifecycleManager;
 import org.apache.camel.quarkus.test.support.certificate.CertificatesUtil;
 import org.apache.camel.util.CollectionHelper;
+import org.eclipse.microprofile.config.ConfigProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testcontainers.containers.BindMode;
@@ -33,8 +34,7 @@ import org.testcontainers.utility.TestcontainersConfiguration;
 
 public class PahoTestResource implements QuarkusTestResourceLifecycleManager {
     private static final Logger LOGGER = LoggerFactory.getLogger(PahoTestResource.class);
-    //    private static final String IMAGE = ConfigProvider.getConfig().getValue("eclipse-mosquitto.container.image", String.class);
-    private static final String IMAGE = "docker.io/eclipse-mosquitto:2.0.18";
+    private static final String IMAGE = ConfigProvider.getConfig().getValue("eclipse-mosquitto.container.image", String.class);
     private static final int TCP_PORT = 1883;
     private static final int SSL_PORT = 8883;
     private static final int WS_PORT = 9001;
