d021ac38aa3861c15e08b5a2de3a5ced63428eb3
Jiri Ondrusek <ondrusek.jiri@gmail.com>
2025-01-15 10:54:46 +0100

    fixes #6909 ssh: extend test coverage

--- integration-tests/ssh/src/test/java/org/apache/camel/quarkus/component/ssh/it/SshTest.java
+++ integration-tests/ssh/src/test/java/org/apache/camel/quarkus/component/ssh/it/SshTest.java
@@ -16,14 +16,27 @@
  */
 package org.apache.camel.quarkus.component.ssh.it;
 
+import java.util.Map;
+
 import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.smallrye.certs.Format;
+import io.smallrye.certs.junit5.Certificate;
+import org.apache.camel.component.ssh.SshConstants;
+import org.apache.camel.quarkus.test.DisabledIfFipsMode;
+import org.apache.camel.quarkus.test.support.certificate.TestCertificates;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@TestCertificates(certificates = {
+        @Certificate(name = "user01", formats = {
+                Format.PEM }, password = "changeit"),
+        @Certificate(name = "eddsa", formats = {
+                Format.PEM }, password = "changeit") })
 @QuarkusTest
 @QuarkusTestResource(SshTestResource.class)
 class SshTest {
@@ -48,6 +61,81 @@ class SshTest {
                 .body().asString();
 
         assertEquals(fileContent, sshFileContent);
+    }
+
+    @Test
+    public void testHeaders() {
+        RestAssured.given()
+                .contentType(ContentType.JSON)
+                .body(Map.of(SshConstants.USERNAME_HEADER, SshTestResource.USERNAME, SshConstants.PASSWORD_HEADER,
+                        SshTestResource.PASSWORD))
+                .queryParam("command", "wrong")
+                .post("/ssh/send/")
+                .then()
+                .statusCode(200)
+                .body("", Matchers.hasEntry(SshConstants.EXIT_VALUE, "127"))
+                .body("", Matchers.hasEntry(Matchers.matchesRegex(SshConstants.STDERR),
+                        Matchers.containsString("command not found")));
+    }
+
+    @Test
+    public void testProducerInRoute() {
+        RestAssured.given()
+                .body("echo Hello World")
+                .post("/ssh/sendToDirect/exampleProducer")
+                .then()
+                .statusCode(200)
+                .body(Matchers.equalTo("Hello World"));
+    }
+
+    @Test
+    public void testKeyProvider() {
+        RestAssured.given()
+                .contentType(ContentType.JSON)
+                .queryParam("component", "ssh-with-key-provider")
+                .queryParam("command", "echo test")
+                .queryParam("serverType", "user01Key")
+                .post("/ssh/send")
+                .then()
+                .statusCode(200)
+                .body("", Matchers.hasEntry(SshConstants.EXIT_VALUE, "0"))
+                //expecting error from command factory
+                .body("", Matchers.hasEntry(SshConstants.STDERR, "Expected Error:echo test"));
+    }
+
+    @Test
+    public void testCertificate() {
+        RestAssured.given()
+                .contentType(ContentType.JSON)
+                .queryParam("component", "ssh-cert")
+                .queryParam("command", "echo test")
+                .queryParam("serverType", "user01Key")
+                .queryParam("pathSuffix", "certResource=file:target/certs/user01.key&certResourcePassword=changeit")
+                .post("/ssh/send")
+                .then()
+                .statusCode(200)
+                .body("", Matchers.hasEntry(SshConstants.EXIT_VALUE, "0"))
+                //expecting error from command factory
+                .body("", Matchers.hasEntry(SshConstants.STDERR, "Expected Error:echo test"));
+    }
+
+    @DisabledIfFipsMode //ED25519 keys are not allowed in FIPS mode
+    @Test
+    public void testProducerWithEdDSAKeyType() {
+        RestAssured.given()
+                .contentType(ContentType.JSON)
+                .queryParam("command", "echo Hello!")
+                .queryParam("serverType", "edKey")
+                .queryParam("pathSuffix",
+                        "timeout=3000&knownHostsResource=/edDSA/known_hosts&failOnUnknownHost=true")
+                .body(Map.of(SshConstants.USERNAME_HEADER, SshTestResource.USERNAME, SshConstants.PASSWORD_HEADER,
+                        SshTestResource.PASSWORD))
+                .post("/ssh/send")
+                .then()
+                .statusCode(200)
+                .body("", Matchers.hasEntry(SshConstants.EXIT_VALUE, "0"))
+                //expecting error from command factory
+                .body("", Matchers.hasEntry(SshConstants.STDERR, "Expected Error:echo Hello!"));
     }
 
 }
