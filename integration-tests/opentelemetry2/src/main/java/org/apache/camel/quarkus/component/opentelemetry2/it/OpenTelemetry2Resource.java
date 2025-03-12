62651dcfe004b7ccc84535c282879c3cefbd1fcf
Pasquale Congiusti <pasquale.congiusti@gmail.com>
2025-03-12 16:58:49 +0100

    feat(it): add otel2 tests

--- integration-tests/opentelemetry2/src/main/java/org/apache/camel/quarkus/component/opentelemetry2/it/OpenTelemetry2Resource.java
+++ integration-tests/opentelemetry2/src/main/java/org/apache/camel/quarkus/component/opentelemetry2/it/OpenTelemetry2Resource.java
@@ -16,53 +16,26 @@
  */
 package org.apache.camel.quarkus.component.opentelemetry2.it;
 
-import java.net.URI;
-
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
-import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
-import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
-import jakarta.ws.rs.core.Response;
-import org.apache.camel.ConsumerTemplate;
 import org.apache.camel.ProducerTemplate;
-import org.jboss.logging.Logger;
 
 @Path("/opentelemetry2")
 @ApplicationScoped
-public class Opentelemetry2Resource {
-
-    private static final Logger LOG = Logger.getLogger(Opentelemetry2Resource.class);
+public class OpenTelemetry2Resource {
 
     @Inject
     ProducerTemplate producerTemplate;
 
-    @Inject
-    ConsumerTemplate consumerTemplate;
-
-    @Path("/get")
+    @Path("/trace")
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String get() throws Exception {
-        final String message = consumerTemplate.receiveBodyNoWait("opentelemetry2:--fix-me--", String.class);
-        LOG.infof("Received from opentelemetry2: %s", message);
-        return message;
+    public String traceRoute() {
+        return producerTemplate.requestBody("direct:start", null, String.class);
     }
 
-    @Path("/post")
-    @POST
-    @Consumes(MediaType.TEXT_PLAIN)
-    @Produces(MediaType.TEXT_PLAIN)
-    public Response post(String message) throws Exception {
-        LOG.infof("Sending to opentelemetry2: %s", message);
-        final String response = producerTemplate.requestBody("opentelemetry2:--fix-me--", message, String.class);
-        LOG.infof("Got response from opentelemetry2: %s", response);
-        return Response
-                .created(new URI("https://camel.apache.org/"))
-                .entity(response)
-                .build();
-    }
 }
--- integration-tests/opentelemetry2/src/test/java/org/apache/camel/quarkus/component/opentelemetry2/it/Opentelemetry2Test.java
+++ integration-tests/opentelemetry2/src/test/java/org/apache/camel/quarkus/component/opentelemetry2/it/Opentelemetry2Test.java
@@ -16,30 +16,50 @@
  */
 package org.apache.camel.quarkus.component.opentelemetry2.it;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.awaitility.Awaitility.await;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @QuarkusTest
 class Opentelemetry2Test {
 
+    static List<Map<String, String>> getSpans() {
+        return RestAssured.given()
+                .get("/opentelemetry2/exporter/spans")
+                .then()
+                .statusCode(200)
+                .extract()
+                .body()
+                .jsonPath()
+                .get();
+    }
+
     @Test
-    public void test() {
-        final String msg = java.util.UUID.randomUUID().toString().replace("-", "");
-        RestAssured.given() //
-                .contentType(ContentType.TEXT)
-                .body(msg)
-                .post("/opentelemetry2/post") //
+    public void testTracedCamelRoute() throws IOException, InterruptedException {
+        RestAssured.get("/opentelemetry2/trace")
                 .then()
-                .statusCode(201);
+                .statusCode(200)
+                .body(equalTo("Traced direct:start"));
 
-        Assertions.fail("Add some assertions to " + getClass().getName());
-
-        RestAssured.get("/opentelemetry2/get")
-                .then()
-                .statusCode(200);
+        await().atMost(30, TimeUnit.SECONDS).pollDelay(50, TimeUnit.MILLISECONDS).until(() -> getSpans().size() == 3);
+        List<Map<String, String>> spans = getSpans();
+        assertEquals(3, spans.size());
+        //Same trace
+        assertEquals(spans.get(2).get("traceId"), spans.get(1).get("traceId"));
+        assertEquals(spans.get(2).get("traceId"), spans.get(0).get("traceId"));
+        // Parent relationship
+        assertEquals("0000000000000000", spans.get(2).get("parentId"));
+        assertEquals(spans.get(2).get("spanId"), spans.get(1).get("parentId"));
+        assertEquals(spans.get(1).get("spanId"), spans.get(0).get("parentId"));
     }
 
 }
