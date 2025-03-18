4c0512cc7823ef5175555628bb0f9eb8491f5ae5
James Netherton <jamesnetherton@gmail.com>
2025-03-18 12:35:40 +0000

    Fix potential Jolokia java.net.BindException in dev mode
    Fixes #7148

---
+++ extensions/jolokia/runtime/src/main/java/org/apache/camel/quarkus/jolokia/devmode/DevModeJolokiaServerShutdownListener.java
@@ -0,0 +1,40 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.quarkus.jolokia.devmode;
+
+import io.quarkus.arc.Arc;
+import io.quarkus.runtime.shutdown.ShutdownListener;
+import jakarta.enterprise.inject.Instance;
+import org.apache.camel.quarkus.jolokia.CamelQuarkusJolokiaServer;
+
+/**
+ * {@link ShutdownListener} to stop the Jolokia server in development mode.
+ */
+public class DevModeJolokiaServerShutdownListener implements ShutdownListener {
+    @Override
+    public void shutdown(ShutdownNotification notification) {
+        Instance<CamelQuarkusJolokiaServer> instance = Arc.container().select(CamelQuarkusJolokiaServer.class);
+        if (instance.isResolvable()) {
+            try {
+                instance.get().stop();
+            } catch (Exception e) {
+                // Ignore - Jolokia shutdown hook will run on SIGTERM
+            }
+        }
+        notification.done();
+    }
+}
