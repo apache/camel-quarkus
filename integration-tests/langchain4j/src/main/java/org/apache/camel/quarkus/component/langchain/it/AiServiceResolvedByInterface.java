a43f682491762d7edd7ad08a300101461d57bc53
Jiri Ondrusek <ondrusek.jiri@gmail.com>
2025-05-30 08:13:21 +0200

    Upgrade Camel to 4.13.0
    Co-authored-by: James Netherton <jamesnetherton@gmail.com>

--- integration-tests/langchain4j/src/main/java/org/apache/camel/quarkus/component/langchain/it/AiServiceResolvedByInterface.java
+++ integration-tests/langchain4j/src/main/java/org/apache/camel/quarkus/component/langchain/it/AiServiceResolvedByInterface.java
@@ -19,8 +19,9 @@ package org.apache.camel.quarkus.component.langchain.it;
 import java.util.function.Supplier;
 
 import dev.langchain4j.data.message.AiMessage;
-import dev.langchain4j.model.chat.ChatLanguageModel;
-import dev.langchain4j.model.output.Response;
+import dev.langchain4j.model.chat.ChatModel;
+import dev.langchain4j.model.chat.request.ChatRequest;
+import dev.langchain4j.model.chat.response.ChatResponse;
 import dev.langchain4j.service.UserMessage;
 import io.quarkiverse.langchain4j.RegisterAiService;
 import jakarta.enterprise.context.ApplicationScoped;
@@ -30,10 +31,17 @@ import org.apache.camel.Handler;
 @RegisterAiService(chatLanguageModelSupplier = AiServiceResolvedByInterface.AiServiceResolvedByInterfaceModelSupplier.class)
 public interface AiServiceResolvedByInterface {
 
-    public static class AiServiceResolvedByInterfaceModelSupplier implements Supplier<ChatLanguageModel> {
+    class AiServiceResolvedByInterfaceModelSupplier implements Supplier<ChatModel> {
         @Override
-        public ChatLanguageModel get() {
-            return (messages) -> new Response<>(new AiMessage("AiServiceResolvedByInterface has been resolved"));
+        public ChatModel get() {
+            return new ChatModel() {
+                @Override
+                public ChatResponse doChat(ChatRequest chatRequest) {
+                    return ChatResponse.builder()
+                            .aiMessage(new AiMessage("AiServiceResolvedByInterface has been resolved"))
+                            .build();
+                }
+            };
         }
     }
 
