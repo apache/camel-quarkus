c7f70a3f88f8eb31c1d97836c7930c698a02283f
Jiri Ondrusek <ondrusek.jiri@gmail.com>
2025-06-11 09:52:40 +0200

    Revert "Disabled CamelMainRoutesIncludePatternWithDefaultLocationsDevModeTest test because of CAMEL-22155"
    This reverts commit 7328c4b960a43908b3b9309528556e4da79b8857.

--- integration-tests/main-devmode/src/test/java/org/apache/camel/quarkus/main/CamelMainRoutesIncludePatternWithDefaultLocationsDevModeTest.java
+++ integration-tests/main-devmode/src/test/java/org/apache/camel/quarkus/main/CamelMainRoutesIncludePatternWithDefaultLocationsDevModeTest.java
@@ -29,7 +29,6 @@ import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.Asset;
 import org.jboss.shrinkwrap.api.asset.StringAsset;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
@@ -103,7 +102,6 @@ public class CamelMainRoutesIncludePatternWithDefaultLocationsDevModeTest {
     }
 
     @Test
-    @Disabled //https://issues.apache.org/jira/browse/CAMEL-22155
     public void testRoutesDiscoveryFromDefaultLocation() {
         await().atMost(10, TimeUnit.SECONDS).untilAsserted(() -> {
             Response res = RestAssured.when().get("/test/describe").thenReturn();
