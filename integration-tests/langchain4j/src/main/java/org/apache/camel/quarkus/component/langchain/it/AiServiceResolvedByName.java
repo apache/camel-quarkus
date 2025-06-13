30c448e7bde7003cd4139c88ca21aa6ac3520550
James Netherton <jamesnetherton@gmail.com>
2025-06-13 09:55:16 +0100

    Upgrade Quarkus Langchain4j to 1.0.0

--- integration-tests/langchain4j/src/main/java/org/apache/camel/quarkus/component/langchain/it/AiServiceResolvedByName.java
+++ integration-tests/langchain4j/src/main/java/org/apache/camel/quarkus/component/langchain/it/AiServiceResolvedByName.java
@@ -19,8 +19,9 @@ package org.apache.camel.quarkus.component.langchain.it;
 import java.util.function.Supplier;
 
 import dev.langchain4j.data.message.AiMessage;
-import dev.langchain4j.model.chat.ChatLanguageModel;
-import dev.langchain4j.model.output.Response;
+import dev.langchain4j.model.chat.ChatModel;
+import dev.langchain4j.model.chat.request.ChatRequest;
+import dev.langchain4j.model.chat.response.ChatResponse;
 import dev.langchain4j.service.UserMessage;
 import io.quarkiverse.langchain4j.RegisterAiService;
 import jakarta.enterprise.context.ApplicationScoped;
@@ -32,11 +33,16 @@ import org.apache.camel.Handler;
 @RegisterAiService(chatLanguageModelSupplier = AiServiceResolvedByName.AiServiceResolvedByNameModelSupplier.class)
 public interface AiServiceResolvedByName {
 
-    public static class AiServiceResolvedByNameModelSupplier implements Supplier<ChatLanguageModel> {
+    class AiServiceResolvedByNameModelSupplier implements Supplier<ChatModel> {
         @Override
-        public ChatLanguageModel get() {
-            return (messages) -> {
-                return new Response<>(new AiMessage("AiServiceResolvedByName has been resolved"));
+        public ChatModel get() {
+            return new ChatModel() {
+                @Override
+                public ChatResponse doChat(ChatRequest chatRequest) {
+                    return ChatResponse.builder()
+                            .aiMessage(new AiMessage("AiServiceResolvedByName has been resolved"))
+                            .build();
+                }
             };
         }
     }
