b1b1584c26bfb3cce16a8092a4a323a621ee6057
James Netherton <jamesnetherton@gmail.com>
2025-06-20 07:46:38 +0100

    Rework upload-source.sh to fetch sources and signatures from repository.apache.org

--- release-utils/scripts/upload-source.sh
+++ release-utils/scripts/upload-source.sh
@@ -19,21 +19,35 @@
 set -e
 
 if [ "$#" -lt 2 ]; then
-    echo "usage: $0 upload-sources release-version destination-version"
+    echo "usage: $0 upload-sources release-version nexus-staging-repository-id"
     exit 1
 fi
 
 location=$(dirname $0)
 version=$1
+stagingRepoId=$2
+sourcesUrl=https://repository.apache.org/content/repositories/orgapachecamel-${stagingRepoId}/org/apache/camel/quarkus/camel-quarkus/${version}
 
-mkdir $1/
-cd $1/
+if [[ "$(curl -k -L -s -o /dev/null -w "%{http_code}" ${sourcesUrl})" != "200" ]]; then
+  echo "Failed to access ${sourcesUrl}. Is the ${version} staging repository closed?"
+  exit 1
+fi
 
-wget https://github.com/apache/camel-quarkus/archive/refs/tags/$1.zip -O apache-camel-quarkus-$1-src.zip
-cp ../../../target/checkout/target/camel-quarkus-$1-sbom.json apache-camel-quarkus-$1-sbom.json
-cp ../../../target/checkout/target/camel-quarkus-$1-sbom.xml apache-camel-quarkus-$1-sbom.xml
-cd ../
-./sign.sh $1/
-svn import $1/ https://dist.apache.org/repos/dist/dev/camel/camel-quarkus/$2/ -m "Import camel-quarkus release"
+mkdir ${version}/
+cd ${version}/
 
-rm -rf $1/
+wget ${sourcesUrl}/camel-quarkus-${version}-src.zip -O apache-camel-quarkus-${version}-src.zip
+wget ${sourcesUrl}/camel-quarkus-${version}-src.zip.asc -O apache-camel-quarkus-${version}-src.zip.asc
+sha512sum -b apache-camel-quarkus-${version}-src.zip > apache-camel-quarkus-${version}-src.zip.sha512
+
+wget ${sourcesUrl}/camel-quarkus-${version}-cyclonedx.json -O apache-camel-quarkus-${version}-sbom.json
+wget ${sourcesUrl}/camel-quarkus-${version}-cyclonedx.json.asc -O apache-camel-quarkus-${version}-sbom.json.asc
+sha512sum -b apache-camel-quarkus-${version}-sbom.json > apache-camel-quarkus-${version}-sbom.json.sha512
+
+wget ${sourcesUrl}/camel-quarkus-${version}-cyclonedx.xml -O apache-camel-quarkus-${version}-sbom.xml
+wget ${sourcesUrl}/camel-quarkus-${version}-cyclonedx.xml.asc -O apache-camel-quarkus-${version}-sbom.xml.asc
+sha512sum -b apache-camel-quarkus-${version}-sbom.xml > apache-camel-quarkus-${version}-sbom.xml.sha512
+
+svn import ${version}/ https://dist.apache.org/repos/dist/dev/camel/camel-quarkus/${version}/ -m "Import camel-quarkus ${version} release"
+
+rm -rf ${version}/
