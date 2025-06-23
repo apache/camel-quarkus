0e00a3cdcac665aa614fe4cb75ff55e548b7ea81
Jiri Ondrusek <ondrusek.jiri@gmail.com>
2025-06-23 16:47:36 +0200

    Disabled fory tests, because of #7466

--- integration-tests/fory/src/test/java/org/apache/camel/quarkus/component/fory/it/ForyTest.java
+++ integration-tests/fory/src/test/java/org/apache/camel/quarkus/component/fory/it/ForyTest.java
@@ -24,6 +24,7 @@ import org.junit.jupiter.api.Test;
 import static org.hamcrest.Matchers.equalTo;
 
 @QuarkusTest
+@Disabled //https://github.com/apache/camel-quarkus/issues/7466
 class ForyTest {
 
     @Test
