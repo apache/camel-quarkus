ef18c071a64af56ae35c1d67e92b5cc90f1e9041
James Netherton <jamesnetherton@gmail.com>
2025-08-14 10:41:37 +0100

    Avoid configuring JasyptPropertiesParser unless encrypted properties are detected
    Fixes #7632

--- extensions/jasypt/runtime/src/main/java/org/apache/camel/quarkus/component/jasypt/CamelJasyptRecorder.java
+++ extensions/jasypt/runtime/src/main/java/org/apache/camel/quarkus/component/jasypt/CamelJasyptRecorder.java
@@ -16,17 +16,25 @@
  */
 package org.apache.camel.quarkus.component.jasypt;
 
+import java.util.regex.Pattern;
+
 import io.quarkus.runtime.RuntimeValue;
 import io.quarkus.runtime.annotations.Recorder;
+import io.smallrye.config.SmallRyeConfig;
 import org.apache.camel.CamelContext;
 import org.apache.camel.component.jasypt.JasyptPropertiesParser;
 import org.apache.camel.component.properties.PropertiesComponent;
 import org.apache.camel.main.MainConfigurationProperties;
 import org.apache.camel.quarkus.main.CamelMain;
 import org.apache.camel.spi.CamelContextCustomizer;
+import org.apache.camel.util.ObjectHelper;
+import org.eclipse.microprofile.config.ConfigProvider;
+import org.eclipse.microprofile.config.spi.ConfigSource;
 
 @Recorder
 public class CamelJasyptRecorder {
+    private static final Pattern JASYPT_ENC = Pattern.compile("ENC\\(\\s*\\S.*\\)");
+
     public void disableCamelMainAutoConfigFromSysEnv(RuntimeValue<CamelMain> camelMainRuntimeValue) {
         CamelMain main = camelMainRuntimeValue.getValue();
         MainConfigurationProperties configurationProperties = main.getMainConfigurationProperties();
@@ -38,12 +46,29 @@ public class CamelJasyptRecorder {
         return new RuntimeValue<>(new CamelContextCustomizer() {
             @Override
             public void configure(CamelContext camelContext) {
-                PropertiesComponent component = (PropertiesComponent) camelContext.getPropertiesComponent();
-                JasyptPropertiesParser jasyptPropertiesParser = CamelJasyptPropertiesParserHolder
-                        .getJasyptPropertiesParser();
-                jasyptPropertiesParser.setPropertiesComponent(component);
-                component.setPropertiesParser(jasyptPropertiesParser);
+                // Since CamelJasyptSecretKeysHandlerFactory relies on LazySecretKeysHandler, we need to avoid
+                // prematurely setting up Jasypt if there are no ENC(..) property values.
+                // Otherwise, the Jasypt configuration will never get triggered.
+                if (isAnyEncryptedPropertyPresent()) {
+                    PropertiesComponent component = (PropertiesComponent) camelContext.getPropertiesComponent();
+                    JasyptPropertiesParser jasyptPropertiesParser = CamelJasyptPropertiesParserHolder
+                            .getJasyptPropertiesParser();
+                    jasyptPropertiesParser.setPropertiesComponent(component);
+                    component.setPropertiesParser(jasyptPropertiesParser);
+                }
             }
         });
+    }
+
+    private boolean isAnyEncryptedPropertyPresent() {
+        SmallRyeConfig config = ConfigProvider.getConfig().unwrap(SmallRyeConfig.class);
+        for (ConfigSource configSource : config.getConfigSources()) {
+            for (String value : configSource.getProperties().values()) {
+                if (ObjectHelper.isNotEmpty(value) && JASYPT_ENC.matcher(value).matches()) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
