dfc5afa99123e8c56191f2c6db121a416aa8fb9c
James Netherton <jamesnetherton@gmail.com>
2025-08-20 10:06:33 +0100

    Demote Hazelcast extension to JVM only due to #7535

--- integration-tests/hazelcast/src/main/java/org/apache/camel/quarkus/component/hazelcast/it/HazelcastProducers.java
+++ integration-tests/hazelcast/src/main/java/org/apache/camel/quarkus/component/hazelcast/it/HazelcastProducers.java
@@ -1,4 +1,33 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.camel.quarkus.component.hazelcast.it;
 
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Singleton;
+
+/**
+ * TODO: Remove when quarkus-hazelcast > 4.0.0 is available - https://github.com/apache/camel-quarkus/issues/7535
+ */
+@ApplicationScoped
 public class HazelcastProducers {
+    @Singleton
+    HazelcastInstance hazelcastInstance() {
+        return Hazelcast.newHazelcastInstance();
+    }
 }
