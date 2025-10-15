a40d54e0e7d6cb55830e4fa9fd8895dbefad63c3
Aurélien Pupier <apupier@ibm.com>
2025-10-15 11:34:41 +0200

    attempt to configure native compilation
    the initialize at runtime configured in the code is not working, I do
    not understand why; It is working when using the comad-lien parameter
    `-Dquarkus.native.additional-build-args="--initialize-at-run-time=org.apache.flink.util.AbstractID,--initialize-at-run-time=com.esotericsoftware.kryo.util.Object"`
    
    the SPI classes are not working. i tried to register by reflection all
    specified classes, also to enable ServiceLoader
    (`-H:+UseServiceLoaderFeature,-H:+UnlockExperimentalVMOptions` in the
    additional native build args) without success as ti is specified
    `-H:-UseServiceLoaderFeature` somewhere else and it is this one which is
    picked)

--- extensions/flink/deployment/src/main/java/org/apache/camel/quarkus/component/flink/deployment/FlinkProcessor.java
+++ extensions/flink/deployment/src/main/java/org/apache/camel/quarkus/component/flink/deployment/FlinkProcessor.java
@@ -16,21 +16,79 @@
  */
 package org.apache.camel.quarkus.component.flink.deployment;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
-import io.quarkus.deployment.annotations.ExecutionTime;
-import io.quarkus.deployment.annotations.Record;
 import io.quarkus.deployment.builditem.FeatureBuildItem;
-import io.quarkus.deployment.pkg.steps.NativeOrNativeSourcesBuild;
-import org.apache.camel.quarkus.core.JvmOnlyRecorder;
+import io.quarkus.deployment.builditem.nativeimage.NativeImageResourceBuildItem;
+import io.quarkus.deployment.builditem.nativeimage.ReflectiveClassBuildItem;
+import io.quarkus.deployment.builditem.nativeimage.RuntimeInitializedClassBuildItem;
+import io.quarkus.deployment.builditem.nativeimage.RuntimeReinitializedClassBuildItem;
+import io.quarkus.runtime.annotations.RegisterForReflection;
+import org.apache.flink.client.deployment.ClusterClientFactory;
+import org.apache.flink.client.deployment.executors.LocalExecutorFactory;
 import org.jboss.logging.Logger;
 
+@RegisterForReflection(targets = { LocalExecutorFactory.class })
 class FlinkProcessor {
 
     private static final Logger LOG = Logger.getLogger(FlinkProcessor.class);
     private static final String FEATURE = "camel-flink";
+    private static final String FLINK_SERVICE_BASE = "META-INF/services/";
 
     @BuildStep
     FeatureBuildItem feature() {
         return new FeatureBuildItem(FEATURE);
+    }
+
+    @BuildStep
+    void runtimeInitializedClasses(BuildProducer<RuntimeInitializedClassBuildItem> runtimeInitializedClass) {
+        System.out.println("runtimeInitializedClasses - Why not called??");
+        runtimeInitializedClass.produce(new RuntimeInitializedClassBuildItem("com.esotericsoftware.kryo.util.ObjectMap"));
+        runtimeInitializedClass.produce(new RuntimeInitializedClassBuildItem("org.apache.flink.util.AbstractID"));
+    }
+    
+    @BuildStep
+    RuntimeInitializedClassBuildItem runtimeInitKryoObjectMap() {
+       System.out.println("runtimeInitKryoObjectMap - Why not called??");
+        // This class uses a Random which needs to be initialized at run time
+        return new RuntimeInitializedClassBuildItem("com.esotericsoftware.kryo.util.ObjectMap");
+    }
+
+    @BuildStep
+    RuntimeInitializedClassBuildItem runtimeInitFlinkAbstractID() {
+        System.out.println("runtimeInitFlinkAbstractID - Why not called??");
+        // This class uses a Random which needs to be initialized at run time
+        return new RuntimeInitializedClassBuildItem("org.apache.flink.util.AbstractID");
+    }
+
+    @BuildStep
+    void registerServiceProviders(BuildProducer<ReflectiveClassBuildItem> reflectiveClass,
+            BuildProducer<NativeImageResourceBuildItem> nativeImage) {
+
+        String[] servicePaths = new String[] {
+                FLINK_SERVICE_BASE + "org.apache.flink.client.deployment.ClusterClientFactory",
+                FLINK_SERVICE_BASE + "org.apache.flink.core.execution.PipelineExecutorFactory",
+        };
+
+        for (String path : servicePaths) {
+            reflectiveClass.produce(ReflectiveClassBuildItem.builder(getServiceClasses(path)).methods().build());
+        }
+
+        nativeImage.produce(new NativeImageResourceBuildItem(servicePaths));
+    }
+
+    private String[] getServiceClasses(String servicePath) {
+        try (InputStream resource = ClusterClientFactory.class.getClassLoader().getResourceAsStream(servicePath)) {
+            Properties properties = new Properties();
+            properties.load(resource);
+            System.out.println("### service classes found: " + properties.keySet());
+            return properties.keySet().toArray(new String[] {});
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
     }
 }
