482fafa22399b1085c79f1c896b4d9154f5bcc58
Lukas Lowinger <llowinge@redhat.com>
2025-10-17 16:57:18 +0200

    Refactor Splunk test container

--- integration-tests-support/splunk/src/test/java/org/apache/camel/quarkus/test/support/splunk/SplunkTestResource.java
+++ integration-tests-support/splunk/src/test/java/org/apache/camel/quarkus/test/support/splunk/SplunkTestResource.java
@@ -17,6 +17,7 @@
 package org.apache.camel.quarkus.test.support.splunk;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -32,11 +33,9 @@ import org.eclipse.microprofile.config.ConfigProvider;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.testcontainers.containers.Container;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.images.builder.Transferable;
-import org.testcontainers.utility.MountableFile;
 
 public class SplunkTestResource implements QuarkusTestResourceLifecycleManager {
 
@@ -90,68 +89,23 @@ public class SplunkTestResource implements QuarkusTestResourceLifecycleManager {
                 //extraction of private key can not be done by keytool (only openssl), but it can be done programmatically
                 byte[] concatenate = concatenateKeyAndCertificates(banner);
 
-                container.withCopyToContainer(Transferable.of(concatenate), "/opt/splunk/etc/auth/mycerts/myServerCert.pem")
-                        .withCopyToContainer(Transferable.of(Files.readAllBytes(Paths.get(caCertPath))),
-                                "/opt/splunk/etc/auth/mycerts/cacert.pem");
+                container.withCopyToContainer(Transferable.of(concatenate),
+                        "/opt/splunk/etc/auth/servercerts/myServerCert.pem");
+                // note we are using different root path "/etc/..." because on Podman 5.4.0 it is unable to copy two files from the same root path for unknown reason
+                container.withCopyToContainer(Transferable.of(Files.readAllBytes(Paths.get(caCertPath))),
+                        "/etc/cacert.pem");
             } else {
                 LOG.debug("Internal certificates are used for Splunk server.");
             }
 
+            // based on https://splunk.github.io/docker-splunk/ADVANCED.html#runtime-configuration
+            container.withCopyToContainer(Transferable.of(readResourceToBytes("default.yml")), "/tmp/defaults/default.yml");
             container.start();
-
-            container.copyFileToContainer(MountableFile.forClasspathResource("local_server.conf"),
-                    "/opt/splunk/etc/system/local/server.conf");
-            container.copyFileToContainer(MountableFile.forClasspathResource("local_inputs.conf"),
-                    "/opt/splunk/etc/system/local/inputs.conf");
-
-            container.copyFileToContainer(MountableFile.forClasspathResource("local_server.conf"),
-                    "/opt/splunk/etc/system/local/server.conf");
-            container.copyFileToContainer(MountableFile.forClasspathResource("local_inputs.conf"),
-                    "/opt/splunk/etc/system/local/inputs.conf");
-
-            container.execInContainer("sudo", "sed", "-i", "s/minFreeSpace = 5000/minFreeSpace = 100/",
-                    "/opt/splunk/etc/system/local/server.conf");
 
             /* uncomment for troubleshooting purposes - copy configuration from container
             container.copyFileFromContainer("/opt/splunk/etc/system/local/server.conf",
                     Path.of(getClass().getResource("/").getPath()).resolve("local_server_from_container.conf").toFile()
                             .getAbsolutePath());*/
-
-            assertExecResult(container.execInContainer("sudo", "microdnf", "--nodocs", "update", "tzdata"), "tzdata install");//install tzdata package so we can specify tz other than UTC
-
-            LOG.debug(banner);
-            LOG.debug("Restarting splunk server.");
-            LOG.debug(banner);
-
-            assertExecResult(container.execInContainer("sudo", "./bin/splunk", "restart"), "splunk restart");
-
-            container.execInContainer("sudo", "./bin/splunk", "add", "index", TEST_INDEX);
-            container.execInContainer("sudo", "./bin/splunk", "add", "tcp", String.valueOf(SplunkConstants.TCP_PORT),
-                    "-sourcetype", "TCP");
-
-            /*uncomment for troubleshooting purposes - copy from container conf and log files
-            container.copyFileFromContainer("/opt/splunk/etc/system/local/server.conf",
-                    Path.of(getClass().getResource("/").getPath()).resolve("local-server-from-container.conf").toFile()
-                            .getAbsolutePath());
-            container.copyFileFromContainer("/opt/splunk/etc/system/default/server.conf",
-                    Path.of(getClass().getResource("/").getPath()).resolve("default-server-from-container.log").toFile()
-                            .getAbsolutePath());
-            if (localhostCertPath != null && localhostKeystorePath != null && caCertPath != null && keystorePassword != null) {
-                container.copyFileFromContainer("/opt/splunk/etc/auth/mycerts/myServerCert.pem",
-                        Path.of(getClass().getResource("/").getPath()).resolve("myServerCert-from-container.pem").toFile()
-                                .getAbsolutePath());
-                container.copyFileFromContainer("/opt/splunk/etc/auth/mycerts/cacert.pem",
-                        Path.of(getClass().getResource("/").getPath()).resolve("cacert-from-container.pem").toFile()
-                                .getAbsolutePath());
-            } else {
-                container.copyFileFromContainer("/opt/splunk/etc/auth/server.pem",
-                        Path.of(getClass().getResource("/").getPath()).resolve("myServerCert-from-container.pem").toFile()
-                                .getAbsolutePath());
-                container.copyFileFromContainer("/opt/splunk/etc/auth/cacert.pem",
-                        Path.of(getClass().getResource("/").getPath()).resolve("cacert-from-container.pem").toFile()
-                                .getAbsolutePath());
-            }
-            */
 
             String splunkHost = container.getHost();
 
@@ -171,7 +125,6 @@ public class SplunkTestResource implements QuarkusTestResourceLifecycleManager {
             LOG.debug(banner);
 
             return m;
-
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
@@ -193,17 +146,6 @@ public class SplunkTestResource implements QuarkusTestResourceLifecycleManager {
         return (severCert + ca + pemKey).getBytes(StandardCharsets.UTF_8);
     }
 
-    private static void assertExecResult(Container.ExecResult res, String cmd) {
-        if (res.getExitCode() != 0) {
-            LOG.error("Command: " + cmd);
-            LOG.error("Stdout: " + res.getStdout());
-            LOG.error("Stderr: " + res.getStderr());
-            throw new RuntimeException("Command " + cmd + ") failed. " + res.getStdout());
-        } else {
-            LOG.debug("Command: " + cmd + " succeeded!");
-        }
-    }
-
     @Override
     public void stop() {
         try {
@@ -212,6 +154,20 @@ public class SplunkTestResource implements QuarkusTestResourceLifecycleManager {
             }
         } catch (Exception e) {
             // Ignored
+        }
+    }
+
+    public byte[] readResourceToBytes(String resourcePath) throws IOException {
+        ClassLoader classLoader = getClass().getClassLoader();
+        try (InputStream inputStream = classLoader.getResourceAsStream(resourcePath)) {
+            if (inputStream == null) {
+                throw new IOException("Resource not found on classpath: " + resourcePath);
+            }
+
+            return inputStream.readAllBytes();
+
+        } catch (IOException e) {
+            throw new IOException("Failed to read resource '" + resourcePath + "' into byte array.", e);
         }
     }
 }
