85d6e37637127bca2f9b2080e5b715b07e065cb1
Aurélien Pupier <apupier@ibm.com>
2025-10-23 09:28:47 +0200

    Replace deprecated static attributes by setters in http component tests
    Signed-off-by: Aurélien Pupier <apupier@ibm.com>

--- integration-test-groups/http/http/src/main/java/org/apache/camel/quarkus/component/http/http/HttpProducers.java
+++ integration-test-groups/http/http/src/main/java/org/apache/camel/quarkus/component/http/http/HttpProducers.java
@@ -45,8 +45,8 @@ public class HttpProducers {
         authCache.put(new HttpHost("localhost", port), basicAuth);
 
         HttpClientContext context = HttpClientContext.create();
-        context.setAttribute(HttpClientContext.AUTH_CACHE, authCache);
-        context.setAttribute(HttpClientContext.CREDS_PROVIDER, provider);
+        context.setAuthCache(authCache);
+        context.setCredentialsProvider(provider);
 
         return context;
     }
