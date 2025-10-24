7face24d9b73771f7370d9a42d39e1604f06ddd9
Jiri Ondrusek <ondrusek.jiri@gmail.com>
2025-10-24 09:02:40 +0200

    Enabled consul - fix of import

--- integration-tests/consul/src/test/java/org/apache/camel/quarkus/component/consul/it/ConsulTest.java
+++ integration-tests/consul/src/test/java/org/apache/camel/quarkus/component/consul/it/ConsulTest.java
@@ -21,7 +21,6 @@ import java.util.UUID;
 import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.RestAssured;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.core.Is.is;
