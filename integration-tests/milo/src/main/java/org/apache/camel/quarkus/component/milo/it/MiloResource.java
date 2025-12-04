31afa1590adb367c779356e44fdcd45355dbe859
James Netherton <jamesnetherton@gmail.com>
2025-12-04 09:59:04 +0000

    Restore Milo extension
    Fixes #8029

---
+++ integration-tests/milo/src/main/java/org/apache/camel/quarkus/component/milo/it/MiloResource.java
@@ -0,0 +1,77 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.quarkus.component.milo.it;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.MediaType;
+import org.apache.camel.ConsumerTemplate;
+import org.apache.camel.FluentProducerTemplate;
+import org.apache.camel.component.milo.MiloConstants;
+import org.eclipse.milo.opcua.stack.core.types.builtin.DataValue;
+import org.eclipse.milo.opcua.stack.core.types.builtin.Variant;
+
+@Path("/milo")
+@ApplicationScoped
+public class MiloResource {
+    @Inject
+    FluentProducerTemplate producerTemplate;
+
+    @Inject
+    ConsumerTemplate consumerTemplate;
+
+    @Path("/send")
+    @POST
+    @Consumes(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.TEXT_PLAIN)
+    public void sendMessage(
+            @QueryParam("endpointUri") String endpointUri,
+            @QueryParam("bodyType") BodyType bodyType,
+            String message) {
+
+        Object body;
+        if (bodyType == BodyType.PLAIN) {
+            body = message;
+        } else if (bodyType == BodyType.VARIANT) {
+            body = new Variant(message);
+        } else {
+            body = new DataValue(new Variant(message));
+        }
+
+        producerTemplate.to(endpointUri)
+                .withHeader(MiloConstants.HEADER_AWAIT, true)
+                .withBody(body)
+                .send();
+    }
+
+    @Path("/receive/{itemId}")
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String receiveMessage(@PathParam("itemId") String itemId) {
+        DataValue dataValue = consumerTemplate.receiveBody("seda:" + itemId, 10000, DataValue.class);
+        Variant variant = dataValue.getValue();
+        Object result = variant.value();
+        return result == null ? null : result.toString();
+    }
+}
