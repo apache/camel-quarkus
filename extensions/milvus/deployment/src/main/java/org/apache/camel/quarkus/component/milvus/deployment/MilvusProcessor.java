e0721f29558286be4766b0e051b1627dce621e88
souvik ghosh <souvik.kgp15@gmail.com>
2026-03-05 20:42:49 +0530

    Add tests and native support for milvus extension
    Fixes #6025
    
    * Add tests and native support for milvus extension
    
    * Move to quarkus-grpc-common to remove unnecessary stuff
    
    ---------
    
    Co-authored-by: Souvik Ghosh <soghosh@redhat.com>

---
+++ extensions/milvus/deployment/src/main/java/org/apache/camel/quarkus/component/milvus/deployment/MilvusProcessor.java
@@ -0,0 +1,86 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.quarkus.component.milvus.deployment;
+
+import io.quarkus.deployment.annotations.BuildProducer;
+import io.quarkus.deployment.annotations.BuildStep;
+import io.quarkus.deployment.builditem.BytecodeTransformerBuildItem;
+import io.quarkus.deployment.builditem.CombinedIndexBuildItem;
+import io.quarkus.deployment.builditem.IndexDependencyBuildItem;
+import org.objectweb.asm.ClassVisitor;
+import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.commons.ClassRemapper;
+import org.objectweb.asm.commons.Remapper;
+
+class MilvusProcessor {
+
+    /**
+     * Intercepts all Milvus SDK classes during the Quarkus build process to perform
+     * bytecode transformation. This ensures the SDK points to the correct
+     * gRPC/Netty implementations, avoiding 'Class Not Found'
+     */
+
+    @BuildStep
+    void relocateAllShadedCalls(
+            CombinedIndexBuildItem index,
+            BuildProducer<BytecodeTransformerBuildItem> transformers) {
+
+        index.getIndex().getKnownClasses().stream()
+                .filter(ci -> {
+                    String name = ci.name().toString();
+                    return name.startsWith("io.milvus");
+                })
+                .forEach(ci -> {
+                    transformers.produce(new BytecodeTransformerBuildItem(
+                            ci.name().toString(),
+                            (name, cv) -> new ShadedRelocationVisitor(cv)));
+                });
+    }
+
+    //This build step ensures that the Milvus Java SDK is indexed by Jandex.
+
+    @BuildStep
+    IndexDependencyBuildItem indexDependencie() {
+        return new IndexDependencyBuildItem("io.milvus", "milvus-sdk-java");
+    }
+
+    /**
+     * Custom ClassRemapper used during the Quarkus build step to intercept and
+     * redirect shaded Netty/gRPC calls within the Milvus SDK.
+     */
+
+    private static class ShadedRelocationVisitor extends ClassRemapper {
+        public ShadedRelocationVisitor(ClassVisitor cv) {
+            super(Opcodes.ASM9, cv, new Remapper(Opcodes.ASM9) {
+                @Override
+                public String map(String internalName) {
+                    if (internalName == null)
+                        return null;
+
+                    if (internalName.startsWith("io/grpc/netty/shaded/io/grpc")) {
+                        return internalName.replace("io/grpc/netty/shaded/io/grpc", "io/grpc");
+                    }
+                    if (internalName.startsWith("io/grpc/netty/shaded/io/netty")) {
+                        return internalName.replace("io/grpc/netty/shaded/io/netty", "io/netty");
+                    }
+                    return super.map(internalName);
+                }
+            });
+        }
+    }
+
+}
---
+++ integration-tests/milvus/src/test/java/org/apache/camel/quarkus/component/milvus/it/MilvusTestResource.java
@@ -0,0 +1,59 @@
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.quarkus.component.milvus.it;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import io.quarkus.test.common.QuarkusTestResourceLifecycleManager;
+import org.eclipse.microprofile.config.ConfigProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testcontainers.milvus.MilvusContainer;
+
+public class MilvusTestResource implements QuarkusTestResourceLifecycleManager {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MilvusTestResource.class);
+    private static final String MILVUS_IMAGE = ConfigProvider.getConfig().getValue("milvus.container.image",
+            String.class);
+    private MilvusContainer milvus;
+
+    @Override
+    public Map<String, String> start() {
+        Map<String, String> properties = new HashMap<>();
+        milvus = new MilvusContainer(MILVUS_IMAGE);
+        milvus.start();
+        properties.put("camel.component.milvus.host", milvus.getHost());
+        properties.put("camel.component.milvus.port", String.valueOf(milvus.getMappedPort(19530)));
+        LOGGER.info("Properties: {}", properties);
+        return properties;
+
+    }
+
+    @Override
+    public void stop() {
+        try {
+            if (milvus != null) {
+                milvus.stop();
+            }
+        } catch (Exception e) {
+            LOGGER.error("An error occurred while stopping Milvus container", e);
+        }
+
+    }
+}
