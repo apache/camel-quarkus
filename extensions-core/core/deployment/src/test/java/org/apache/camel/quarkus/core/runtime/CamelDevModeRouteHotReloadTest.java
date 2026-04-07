e70bcc24477e2c851c67a08a6c03ce99108492a4
Lukas Lowinger <llowinge@redhat.com>
2026-04-07 14:10:05 +0200

    [fixes #8318] Improve CamelHotReplacementSetup for dev mode reloads

---
+++ extensions-core/core/deployment/src/test/java/org/apache/camel/quarkus/core/runtime/CamelDevModeRouteHotReloadTest.java
@@ -0,0 +1,86 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.quarkus.core.runtime;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+
+import io.quarkus.test.QuarkusDevModeTest;
+import org.awaitility.Awaitility;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.Asset;
+import org.jboss.shrinkwrap.api.asset.StringAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+/**
+ * Test that verifies Camel route hot reload works correctly with CamelHotReplacementSetup's own scanning,
+ * without other Quarkus extension HotReplacementSetup implementations involved.
+ * <p>
+ * This test specifically avoids extensions like quarkus-vertx-http (which can also initiate restarts via
+ * <a href=
+ * "https://github.com/quarkusio/quarkus/blob/3.34.2/extensions/vertx-http/runtime/src/main/java/io/quarkus/vertx/http/runtime/devmode/VertxHttpHotReplacementSetup.java#L171">
+ * VertxHttpHotReplacementSetup</a>) to ensure Camel's timer-based scanning mechanism works independently.
+ * <p>
+ * This also verifies the fix for race conditions (issue #8318) doesn't break Camel's own hot reload functionality.
+ */
+public class CamelDevModeRouteHotReloadTest {
+
+    public static final String ORIGINAL_MESSAGE = "Original Route Message";
+    public static final String MODIFIED_MESSAGE = "Modified Route Message";
+
+    @RegisterExtension
+    static final QuarkusDevModeTest TEST = new QuarkusDevModeTest()
+            .setLogRecordPredicate(record -> record.getLevel().equals(Level.INFO))
+            .setArchiveProducer(() -> ShrinkWrap
+                    .create(JavaArchive.class)
+                    .addClass(HotReloadRoute.class)
+                    .addAsResource(applicationProperties(), "application.properties"));
+
+    public static Asset applicationProperties() {
+        Writer writer = new StringWriter();
+        Properties props = new Properties();
+        props.setProperty("quarkus.banner.enabled", "false");
+        try {
+            props.store(writer, "");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return new StringAsset(writer.toString());
+    }
+
+    @Test
+    public void testRouteHotReload() {
+        // Wait for original message to be logged
+        Awaitility.await().atMost(10, TimeUnit.SECONDS).until(() -> TEST.getLogRecords().stream()
+                .anyMatch(logRecord -> logRecord.getMessage().contains(ORIGINAL_MESSAGE)));
+
+        // Modify the java route
+        TEST.modifySourceFile(HotReloadRoute.class, s -> s.replace(
+                ORIGINAL_MESSAGE, MODIFIED_MESSAGE));
+
+        // Wait for modified message to be logged after hot reload
+        Awaitility.await().atMost(10, TimeUnit.SECONDS).until(() -> TEST.getLogRecords().stream()
+                .anyMatch(logRecord -> logRecord.getMessage().contains(MODIFIED_MESSAGE)));
+    }
+
+}
