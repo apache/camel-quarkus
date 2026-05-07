7d23daeb3a0c0532e609eb5e7f00f5fb69182e36
Gaelle Fournier <gaelle.fournier.work@gmail.com>
2026-05-07 15:50:45 +0200

    fix: Missing jsch reflection classes

--- extensions/ftp/deployment/src/main/java/org/apache/camel/quarkus/component/ftp/deployment/FtpProcessor.java
+++ extensions/ftp/deployment/src/main/java/org/apache/camel/quarkus/component/ftp/deployment/FtpProcessor.java
@@ -39,9 +39,12 @@ class FtpProcessor {
                 "com.jcraft.jsch.KeyPairEd25519",
                 "com.jcraft.jsch.KeyPairEd448",
                 "com.jcraft.jsch.KeyPairDSA",
-                "com.jcraft.jsch.SignatureRSA",
-                "com.jcraft.jsch.SignatureECDSA",
-                "com.jcraft.jsch.jce.SignatureEd25519")
+                "com.jcraft.jsch.KeyPairEdDSA",
+                "com.jcraft.jsch.KeyPairPKCS8",
+                "com.jcraft.jsch.jce.SignatureEd25519",
+                "com.jcraft.jsch.IdentityFile",
+                "com.jcraft.jsch.LocalIdentityRepository",
+                "com.jcraft.jsch.jce.KeyPairGenEdDSA")
                 .build();
     }
 }
