bc5d146a4a66d29bebe7d03ee3c2a13ff7237995
James Netherton <jamesnetherton@gmail.com>
2026-05-14 09:06:23 +0100

    Upgrade Quarkus CXF to 3.27.2

--- integration-test-groups/cxf-soap/cxf-soap-ws-security-client/src/test/java/org/apache/camel/quarkus/component/cxf/soap/wss/client/it/CxfSoapWssClientTest.java
+++ integration-test-groups/cxf-soap/cxf-soap-ws-security-client/src/test/java/org/apache/camel/quarkus/component/cxf/soap/wss/client/it/CxfSoapWssClientTest.java
@@ -17,7 +17,6 @@
 package org.apache.camel.quarkus.component.cxf.soap.wss.client.it;
 
 import java.io.IOException;
-import java.net.ConnectException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -33,7 +32,7 @@ import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.RestAssured;
 import jakarta.xml.ws.BindingProvider;
 import jakarta.xml.ws.Service;
-import jakarta.xml.ws.WebServiceException;
+import jakarta.xml.ws.soap.SOAPFaultException;
 import org.apache.camel.quarkus.components.cxf.soap.wss.client.helloworld.SayHelloService;
 import org.apache.camel.quarkus.components.cxf.soap.wss.client.helloworld.SayHelloWrongWS;
 import org.apache.cxf.ws.security.SecurityConstants;
@@ -115,8 +114,8 @@ class CxfSoapWssClientTest {
             try {
                 //always fails because there is no server implementation
                 createSayHelloWrongClient().sayHelloWrong("Sheldon");
-            } catch (WebServiceException e) {
-                return e.getCause() != null && e.getCause() instanceof ConnectException;
+            } catch (SOAPFaultException e) {
+                return e.getCause() != null && e.getMessage().toLowerCase().contains("connection refused");
             }
             //can not happen (client does not work)
             return false;
