306efec286a55ffbe45e9a66cb26970c77e2cf58
Gaëlle Fournier <gaelle.fournier.work@gmail.com>
2026-05-20 08:21:11 +0200

    chore: remove unused build step on sftp/jsch

--- extensions/ftp/deployment/src/main/java/org/apache/camel/quarkus/component/ftp/deployment/FtpProcessor.java
+++ extensions/ftp/deployment/src/main/java/org/apache/camel/quarkus/component/ftp/deployment/FtpProcessor.java
@@ -18,7 +18,6 @@ package org.apache.camel.quarkus.component.ftp.deployment;
 
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.builditem.FeatureBuildItem;
-import io.quarkus.deployment.builditem.nativeimage.ReflectiveClassBuildItem;
 
 class FtpProcessor {
 
@@ -27,24 +26,5 @@ class FtpProcessor {
     @BuildStep
     FeatureBuildItem feature() {
         return new FeatureBuildItem(FEATURE);
-    }
-
-    @BuildStep
-    ReflectiveClassBuildItem registerJSchCertificateClasses() {
-        // JSch OpenSSH certificate support classes for @cert-authority parsing in known_hosts
-        // The quarkus-jsch reflection config is missing several classes that JSch loads dynamically.
-        return ReflectiveClassBuildItem.builder(
-                "com.jcraft.jsch.KeyPairRSA",
-                "com.jcraft.jsch.KeyPairECDSA",
-                "com.jcraft.jsch.KeyPairEd25519",
-                "com.jcraft.jsch.KeyPairEd448",
-                "com.jcraft.jsch.KeyPairDSA",
-                "com.jcraft.jsch.KeyPairEdDSA",
-                "com.jcraft.jsch.KeyPairPKCS8",
-                "com.jcraft.jsch.jce.SignatureEd25519",
-                "com.jcraft.jsch.IdentityFile",
-                "com.jcraft.jsch.LocalIdentityRepository",
-                "com.jcraft.jsch.jce.KeyPairGenEdDSA")
-                .build();
     }
 }
