c44d90a5085349b4481fcf16b2fd719900a04630
Ramu <kramu.k7@gmail.com>
2026-09-02 11:56:56 +0530

    Add AWS2 STS integration test group
    Fixes #5541
    
    * CAMEL-5541: Add AWS2 STS integration test group
    
    * Address review comments: remove redundant compliance setting and add credentials null guard

---
+++ integration-test-groups/aws2/aws2-sts/src/main/java/org/apache/camel/quarkus/component/aws2/sts/it/Aws2StsResource.java
@@ -0,0 +1,62 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.quarkus.component.aws2.sts.it;
+
+import java.util.Map;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.component.aws2.sts.STS2Operations;
+import software.amazon.awssdk.services.sts.model.Credentials;
+import software.amazon.awssdk.services.sts.model.GetSessionTokenResponse;
+
+@Path("/aws2-sts")
+@ApplicationScoped
+public class Aws2StsResource {
+
+    @Inject
+    ProducerTemplate producerTemplate;
+
+    @Path("/session-token")
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getSessionToken() {
+        GetSessionTokenResponse response = producerTemplate.requestBody(
+                componentUri(STS2Operations.getSessionToken),
+                null,
+                GetSessionTokenResponse.class);
+
+        if (response == null || response.credentials() == null) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+
+        Credentials credentials = response.credentials();
+        return Response.ok(Map.of(
+                "accessKeyId", credentials.accessKeyId(),
+                "sessionToken", credentials.sessionToken())).build();
+    }
+
+    private String componentUri(STS2Operations operation) {
+        return "aws2-sts://myaccount?operation=" + operation;
+    }
+}
